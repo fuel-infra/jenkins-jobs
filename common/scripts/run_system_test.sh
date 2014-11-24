236f3d6fd1f92eab55131597824f06372dce7e3b
Aleksandra Fedorova <afedorova@mirantis.com>
2014-11-24 21:44:31 +0300

    Example for fully templated system test run
    Use tox -e tests to see the result
    
    Change-Id: I6faa0641fd6efd8df1350f2ff256433d5b779997

---
+++ common/scripts/run_system_test.sh
@@ -0,0 +1,6 @@
+rm -rf logs/*
+
+ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER
+ISO_PATH=`seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}"`
+
+sh -x "utils/jenkins/system_tests.sh" -t test -w "$WORKSPACE" -e "$ENV_NAME" -o --group="$TEST_GROUP" -i "$ISO_PATH"
