82e55a3f912915ca600c1c1976e781efb6e52963
Aleksandra Fedorova <afedorova@mirantis.com>
2015-01-02 16:08:47 +0300

    Implement system tests (swarm)
    - add EnvInject Jenkins plugin to handle additional environment variables,
    - add default-properties and 'properties' variable to system-test
      template,
    - add build description,
    - fix basedir parameter for main-git macros,
    - create swarm job group,
    - add 6.1 swarm project,
    - use 6.0 branch as a base for upgrade tests.
    
    Remaining tasks
    - plugins jobs,
    - runner,
    - timer,
    - reporting,
    - prepare environment
    will be implemented in next set of patches.
    
    Change-Id: Iab18896b44cfdddfa7052e2430f6e4e58586cd93

--- common/scripts/run_system_test.sh
+++ common/scripts/run_system_test.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
+set -ex
+
 rm -rf logs/*
 
-ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER
+ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER.$BUILD_ID
 ISO_PATH=`seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}"`
+
+VERSION_STRING=`basename $ISO_PATH | cut -d '-' -f 2-3`
+echo "Description string: $TEST_GROUP on $VERSION_STRING"
 
 sh -x "utils/jenkins/system_tests.sh" -t test -w "$WORKSPACE" -e "$ENV_NAME" -o --group="$TEST_GROUP" -i "$ISO_PATH"
