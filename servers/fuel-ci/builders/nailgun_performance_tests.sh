4c97bbf63de113a1ec5267f41d7b0194a13520bc
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2015-01-04 22:03:26 +0100

    Do not use nosetests for nailgun_performance_tests
    - use 'run_test.sh' instead of 'nosetests' (deprecated)
    - to make sure all requirements are installed
    
    Change-Id: I24e07e95237e06eec3add6470f7a7c7a9c3f926d

--- servers/fuel-ci/builders/nailgun_performance_tests.sh
+++ servers/fuel-ci/builders/nailgun_performance_tests.sh
@@ -9,11 +9,12 @@ source ${VENV}/bin/activate
 
 export TEST_NAILGUN_DB="nailgun"
 
-cd $WORKSPACE/nailgun/nailgun
+cd ${WORKSPACE}/nailgun/nailgun
 
 sed -i 's#PERFORMANCE_PROFILING_TESTS:.*#PERFORMANCE_PROFILING_TESTS: 1#' settings.yaml
 sed -i 's#load_tests_base:.*#load_tests_base: "'${WORKSPACE}'/results/tests/"#' settings.yaml
 sed -i 's#last_performance_test:.*#last_performance_test: "'${WORKSPACE}'/results/last/"#' settings.yaml
 sed -i 's#load_tests_results:.*#load_tests_results: "'${WORKSPACE}'/results/results/"#' settings.yaml
 
-nosetests test/performance
+cd ${WORKSPACE}/
+./run_tests.sh -t nailgun/nailgun/test/performance
