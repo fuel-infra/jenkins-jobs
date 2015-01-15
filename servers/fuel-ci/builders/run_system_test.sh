bfa272457c54dd4b823ae8c2229f165e019d08ca
Sergey Kulanov <skulanov@mirantis.com>
2015-01-15 17:59:22 +0200

    Implement bvt test jobs for community iso
    Change-Id: I4e981e93a802cf1a7855080273c34aa3ffd6ade1

---
+++ servers/fuel-ci/builders/run_system_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+export VENV_PATH=/home/jenkins/venv-nailgun-tests
+rm -rf logs/*
+
+ISO_PATH=`seedclient.py -d -m "${ISO_TORRENT}" -v --force-set-symlink -o "${WORKSPACE}"`
+
+VERSION_STRING=`readlink ${ISO_PATH} | cut -d '-' -f 3-4`
+echo "Description string: ${TEST_GROUP} on ${VERSION_STRING}"
+
+sh -x "utils/jenkins/system_tests.sh" -t test -w "${WORKSPACE}" -V "${VENV_PATH}" -j "${JOB_NAME}" -o --group="${TEST_GROUP}" -i "${ISO_PATH}"
