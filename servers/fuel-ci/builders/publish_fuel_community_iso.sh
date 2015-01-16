5a78195c166e922423995d7753858dd10e9c9bda
Sergey Kulanov <skulanov@mirantis.com>
2015-01-16 13:57:38 +0200

    Implement publish jobs on fuel-ci
    - Implement jobs that responsible for publishing iso, img, upgd
      artifacts on torrent and status page:
        * publish_fuel_community_iso
        * fuel_communiy_build_reports
    - Fix misprint in job name
    
    Change-Id: I57170b9334106c75744cd16db702fce8c034b1b1

---
+++ servers/fuel-ci/builders/publish_fuel_community_iso.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+ISO_NAME=fuel-community-${ISO_ID}
+TARBALL_NAME=fuel-community-${UPGRADE_ID}
+
+seedclient.py -pvf "${ARTIFACTS_DIR}/${ISO_NAME}.iso" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+
+seedclient.py -pvf "${ARTIFACTS_DIR}/${ISO_NAME}.img" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+
+seedclient.py -pvf "${ARTIFACTS_DIR}/${TARBALL_NAME}.tar.lrz" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+
+echo "META: ${ISO_ID}: <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.iso.torrent>ISO</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.img.torrent>IMG</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${TARBALL_NAME}.tar.lrz.torrent>UPGD</a>"
