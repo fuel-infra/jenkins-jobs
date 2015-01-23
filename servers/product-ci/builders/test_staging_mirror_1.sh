1992f6a97eb16ada0bee6af418393bf4f418e43e
Pavel Boldin <pboldin@mirantis.com>
2015-01-23 17:30:52 +0200

    Verify Fuel against Ubuntu upstream repo
    The `6.1.ubuntu_upstream` job group is responsible for checking and verifying
    Fuel build against the latest Ubuntu upstream repository. On success this
    repository is to be linked as `stable` Ubuntu upstream repo.
    
    Change-Id: I548751eae07ec022cf8bb01f0abde725030af2ec
    Partial-Bug: #1414217

--- servers/product-ci/builders/test_staging_mirror_1.sh
+++ servers/product-ci/builders/test_staging_mirror_1.sh
@@ -1,15 +1,29 @@
 export MIRROR_DETECT=$(echo $FUEL_MAIN_BRANCH | grep '^stable\>/\<' | sed -e 's|^stable/||')
-[ -z "$MIRROR_DETECT" ]  && [ -z "$MIRROR" ] && exit 1:w
+[ -z "$MIRROR_DETECT" ]  && [ -z "$MIRROR" ] && exit 1
 
 export MIRROR=${MIRROR_DETECT:-$MIRROR}
 export MIRROR_HOST=osci-mirror-srt.srt.mirantis.net
 export MIRROR_VERSION=${MIRROR_VERSION:-$(rsync -l rsync://${MIRROR_HOST}/mirror/fwm/files/$MIRROR-staging | awk '/^l/ {print $NF}')}
 
 
-echo "MIRROR = ${MIRROR}" > $WORKSPACE/mirror_staging.txt
-echo "MIRROR_VERSION = ${MIRROR_VERSION}" >> $WORKSPACE/mirror_staging.txt
-echo "STABLE_VERSION = ${MIRROR_VERSION}" >> $WORKSPACE/mirror_staging.txt
-echo "MIRROR_BASE = http://${MIRROR_HOST}/fwm/files/${MIRROR_VERSION}" >> $WORKSPACE/mirror_staging.txt
-echo "fuelmain_gerrit_commit = ${extra_commits}" >> $WORKSPACE/mirror_staging.txt
+PARAM_FILE=$WORKSPACE/mirror_staging.txt
+rm -f $PARAM_FILE
+echo "MIRROR = ${MIRROR}" >>$PARAM_FILE
+echo "MIRROR_VERSION = ${MIRROR_VERSION}" >>$PARAM_FILE
+echo "STABLE_VERSION = ${MIRROR_VERSION}" >>$PARAM_FILE
+MIRROR_BASE=http://${MIRROR_HOST}/fwm/files/${MIRROR_VERSION}
+echo "MIRROR_BASE = $MIRROR_BASE" >>$PARAM_FILE
+echo "fuelmain_gerrit_commit = ${extra_commits}" >>$PARAM_FILE
+if [ -n "$MIRROR_UBUNTU" ]; then
+    echo "MIRROR_UBUNTU = $MIRROR_UBUNTU" >>$PARAM_FILE
+fi
+if [ "$MIRROR_UBUNTU_SECURITY" = "MIRROR_UBUNTU" ]; then
+    echo "MIRROR_UBUNTU_SECURITY = $MIRROR_UBUNTU" >>$PARAM_FILE
+elif [ -n "$MIRROR_UBUNTU_SECURITY" ]; then
+    echo "MIRROR_UBUNTU_SECURITY = $MIRROR_UBUNTU_SECURITY" >>$PARAM_FILE
+fi
+if [ -n "$USE_MIRROR" ]; then
+    echo "USE_MIRROR = $USE_MIRROR" >>$PARAM_FILE
+fi
 
 rm -f build_description.*
