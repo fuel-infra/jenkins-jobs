80a4e18f7d9d026499a4fef58cb914c4ffb89c34
Igor Belikov <ibelikov@mirantis.com>
2015-01-23 12:39:55 +0300

    Add $BUILD_ID to smoke/bvt env names
    Change-Id: I42eb7bcbd4797dea1b9e5a6549f8264994c34376

--- servers/product-ci/builders/run_system_test.sh
+++ servers/product-ci/builders/run_system_test.sh
@@ -1,6 +1,6 @@
 rm -rf logs/*
 export VENV_PATH=/home/jenkins/venv-nailgun-tests
-ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER
+ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER.$BUILD_ID
 ISO_PATH=`seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}"`
 
 sh -x "utils/jenkins/system_tests.sh" -t test -w "$WORKSPACE" -e "$ENV_NAME" -o --group="$TEST_GROUP" -i "$ISO_PATH"
