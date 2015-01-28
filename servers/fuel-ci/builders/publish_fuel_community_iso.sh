4b69ed04a33fc538383a501c12c76c20b7434ae8
Igor Shishkin <ishishkin@mirantis.com>
2015-01-29 00:41:26 +0300

    from GET parameter added for downlaods stats purposes
    Change-Id: I29f6bcbf942b8cc1bb5bc25198151df13334c569
    Signed-off-by: Igor Shishkin <ishishkin@mirantis.com>

--- servers/fuel-ci/builders/publish_fuel_community_iso.sh
+++ servers/fuel-ci/builders/publish_fuel_community_iso.sh
@@ -11,4 +11,4 @@ seedclient.py -pvf "${ARTIFACTS_DIR}/${ISO_NAME}.img" --tracker-url=http://retra
 
 seedclient.py -pvf "${ARTIFACTS_DIR}/${TARBALL_NAME}.tar.lrz" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
 
-echo "META: ${ISO_ID}: <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.iso.torrent>ISO</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.img.torrent>IMG</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${TARBALL_NAME}.tar.lrz.torrent>UPGD</a>"
+echo "META: ${ISO_ID}: <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.iso.torrent?from=jenkins>ISO</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.img.torrent?from=jenkins>IMG</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${TARBALL_NAME}.tar.lrz.torrent?from=jenkins>UPGD</a>"
