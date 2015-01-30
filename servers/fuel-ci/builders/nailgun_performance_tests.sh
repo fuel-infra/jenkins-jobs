7f48307b95ce190e5038e011f5c334a225a9eb4e
Maciej Kwiek <mkwiek@mirantis.com>
2015-01-30 12:44:31 +0100

    Nailgun perf test job archives and reuses csv file
    This change is needed for performance data visualizer to be able to
    get data about historic test runs to visualize performance trends in
    nailgun.
    
    Change-Id: I25008d06848ad2287686f82ef63b10af3612125e

--- servers/fuel-ci/builders/nailgun_performance_tests.sh
+++ servers/fuel-ci/builders/nailgun_performance_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -x
-set -e
+set -ex
 
 VENV="${WORKSPACE}_VENV"
 virtualenv -p python2.6 "${VENV}"
@@ -11,10 +10,13 @@ export TEST_NAILGUN_DB="nailgun"
 
 cd ${WORKSPACE}/nailgun/nailgun
 
-sed -i 's#PERFORMANCE_PROFILING_TESTS:.*#PERFORMANCE_PROFILING_TESTS: 1#' settings.yaml
+# modify paths since by default /tmp/ is used
 sed -i 's#load_tests_base:.*#load_tests_base: "'${WORKSPACE}'/results/tests/"#' settings.yaml
 sed -i 's#last_performance_test:.*#last_performance_test: "'${WORKSPACE}'/results/last/"#' settings.yaml
 sed -i 's#load_tests_results:.*#load_tests_results: "'${WORKSPACE}'/results/results/"#' settings.yaml
+sed -i 's#last_performance_test_run:.*#last_performance_test_run: "'${WORKSPACE}'/results/last/run/"#' settings.yaml
+sed -i 's#load_previous_tests_results:.*#load_previous_tests_results: "'${WORKSPACE}'/results/previous_results.json"#' settings.yaml
 
 cd ${WORKSPACE}/
-./run_tests.sh -t nailgun/nailgun/test/performance
+mkdir -p ${WORKSPACE}/results
+./run_tests.sh -x
