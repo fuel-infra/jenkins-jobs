fe5023471fa1218a85dc2cd87bf2f02b858b1e2c
Sergey Kulanov <skulanov@mirantis.com>
2015-02-02 11:28:12 +0200

    Fix UPGRADE_FUEL_FROM variable in upgrade tests
    Change-Id: Ied9a4878fa9877813c9ad506fdec51323c24d102
    Closes-bug: #1417013

--- common/scripts/run_upgrade_system_test.sh
+++ common/scripts/run_upgrade_system_test.sh
@@ -30,7 +30,7 @@ export TARBALL_PATH=`seedclient-wrapper -d -m "${UPGRADE_TARBALL_MAGNET_LINK}" -
 VERSION_STRING=`basename $TARBALL_PATH | cut -d '-' -f 2-4`
 echo "Description string: $VERSION_STRING"
 
-export UPGRADE_FUEL_FROM=`basename $ISO_PATH | cut -d '-' -f 2`
+export UPGRADE_FUEL_FROM=`basename $ISO_PATH | cut -d '-' -f 2 | sed s/.iso//g`
 export UPGRADE_FUEL_TO=`basename $TARBALL_PATH | cut -d '-' -f 2`
 
 # Use -k to reuse environment
