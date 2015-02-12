61ffcd58649f7671d0b8e5db146eae532ff34f61
Igor Belikov <ibelikov@mirantis.com>
2015-02-12 17:21:54 +0300

    Use venv in erase_env sctipts
    Since python-devops package is no longer present on slaves
    we need to start using virtualenv for dos.py related operations
    
    Change-Id: Ia90f48f1c46ff2df1c8561857efb6034f6f7296e

--- common/scripts/erase_env.sh
+++ common/scripts/erase_env.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+source /home/jenkins/venv-nailgun-tests/bin/activate
+
 ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER.$BUILD_ID
 ENV_NAME=${ENV_NAME:0:68}
 
--- servers/product-ci/builders/erase_env.sh
+++ servers/product-ci/builders/erase_env.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -ex
+
+source /home/jenkins/venv-nailgun-tests/bin/activate
+
 ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER.$BUILD_ID
 ENV_NAME=${ENV_NAME:0:68}
 dos.py erase $ENV_NAME
