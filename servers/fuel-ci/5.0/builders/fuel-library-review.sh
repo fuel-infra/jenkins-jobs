23b91196246747f34ddf5c336c4ec966a40bcd65
Igor Belikov <ibelikov@mirantis.com>
2015-03-05 17:23:54 +0300

    Fuel-library and community builds job refactorig
    Getting rid of templates and moving to separate directories
    for each release.
    
    Change-Id: I31bff48263f6a7011bd02b521915f8e70ce8acf2

---
+++ servers/fuel-ci/5.0/builders/fuel-library-review.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ex
+
+echo "${GERRIT_CHANGE_COMMIT_MESSAGE}" | base64 -d > gerrit_commit_message.txt 2>/dev/null || true
+
+if grep -q -iE "Fuel-CI:\s+disable" gerrit_commit_message.txt; then
+  echo "Fuel CI check disabled"
+  exit -1
+fi
+
+#common params
+
+export LOGS_DIR=/home/jenkins/workspace/${JOB_NAME}/logs/${BUILD_NUMBER}
+export UPLOAD_MANIFESTS=true
+export UPLOAD_MANIFESTS_PATH=/home/jenkins/workspace/${JOB_NAME}/deployment/puppet/
+
+BRANCH_ID=`echo ${BRANCH##*/} | sed 's:\.:_:g'`
+
+export WORKSPACE="/home/jenkins/workspace/fuel-main/env_${DISTRIBUTION}-default/${BRANCH}"
+export SYSTEM_TESTS="${WORKSPACE}/utils/jenkins/system_tests.sh"
+export ENV_NAME="env_${DISTRIBUTION}-${BRANCH_ID}"
+export ISO_PATH="/home/jenkins/workspace/iso/fuel_${BRANCH_ID}.iso"
+
+VERSION_STRING=`readlink ${ISO_PATH} | cut -d '-' -f 2-3`
+echo "Description string: ${VERSION_STRING}"
+
+sh -x "${SYSTEM_TESTS}" -V "${VENV_PATH}" -i "${ISO_PATH}" -t test -e "${ENV_NAME}" -k -o --group=${TEST_GROUP}
---
+++ servers/fuel-ci/6.0/builders/community.all.sh
@@ -0,0 +1,96 @@
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:$PATH
+
+export FEATURE_GROUPS=experimental
+
+PROD_VER=`grep 'PRODUCT_VERSION:=' config.mk | cut -d= -f2`
+export ISO_NAME=fuel-community-$PROD_VER-$BUILD_NUMBER-$BUILD_ID
+export UPGRADE_TARBALL_NAME=fuel-community-$PROD_VER-upgrade-$BUILD_NUMBER-$BUILD_ID
+export ARTIFACT_NAME=fuel-community-$PROD_VER-artifacts-$BUILD_NUMBER-$BUILD_ID
+export ARTIFACT_DIFF_NAME=fuel-community-$PROD_VER-diff-$BUILD_NUMBER-$BUILD_ID
+
+export USE_MIRROR=ext
+
+export BUILD_DIR=${WORKSPACE}/../tmp/${JOB_NAME}/build
+export LOCAL_MIRROR=${WORKSPACE}/../tmp/${JOB_NAME}/local_mirror
+
+export ARTS_DIR=${WORKSPACE}/artifacts
+rm -rf ${ARTS_DIR}
+
+export DEPS_DIR=${BUILD_DIR}/deps
+rm -rf "${DEPS_DIR}"
+
+#########################################
+
+test "$deep_clean" = "true" && make deep_clean
+
+echo "STEP 0. PROD_VER=${PROD_VER} BASE_VERSION=${BASE_VERSION} UPGRADE_VERSIONS=${PROD_VER}:${BASE_VERSION}"
+
+#########################################
+
+echo "Check version.yaml for Fuel commit ids (if present)"
+
+if [ -n "${FUEL_COMMITS}" ] && [ -f "/home/jenkins/workspace/fuel_commits/${FUEL_COMMITS}" ]; then
+  export FUEL_COMMITS=/home/jenkins/workspace/fuel_commits/${FUEL_COMMITS}
+  export ASTUTE_COMMIT=$(fgrep astute_sha ${FUEL_COMMITS}|cut -d\" -f2)
+  export FUELLIB_COMMIT=$(fgrep fuellib_sha ${FUEL_COMMITS}|cut -d\" -f2)
+  export FUELMAIN_COMMIT=$(fgrep fuelmain_sha ${FUEL_COMMITS}|cut -d\" -f2)
+  export NAILGUN_COMMIT=$(fgrep nailgun_sha ${FUEL_COMMITS}|cut -d\" -f2)
+  export OSTF_COMMIT=$(fgrep ostf_sha ${FUEL_COMMITS}|cut -d\" -f2)
+  export PYTHON_FUELCLIENT_COMMIT=$(fgrep python-fuelclient_sha ${FUEL_COMMITS}|cut -d\" -f2)
+fi
+
+#########################################
+
+echo "STEP 1. Get artifacts from ${BASE_VERSION}"
+export DEPS_DATA_DIR="$DEPS_DIR/${BASE_VERSION}"
+mkdir -p "${DEPS_DATA_DIR}"
+
+DATA_URL="https://fuel-jenkins.mirantis.com/job/${BASE_VERSION}-community.all"
+DATA_BUILD_NUMBER=`curl -s "${DATA_URL}/lastSuccessfulBuild/buildNumber"`
+echo "$DATA_URL/$DATA_BUILD_NUMBER" > $WORKSPACE/data_build_url.txt
+if [ -z "${DATA_MAGNET_LINK}" ]; then
+  export DATA_MAGNET_LINK=`curl -s "${DATA_URL}/${DATA_BUILD_NUMBER}/artifact/artifacts_torrent_link.txt" | sed 's~.*MAGNET_LINK=~~'`
+fi
+DATA_FILE=`seedclient-wrapper -dvm "${DATA_MAGNET_LINK}" --force-set-symlink -o "${DEPS_DATA_DIR}"`
+tar xvf "${DATA_FILE}" -C "${DEPS_DATA_DIR}"
+
+#########################################
+
+echo "STEP 2. Make everything"
+make UPGRADE_VERSIONS="${PROD_VER}:${BASE_VERSION}" BASE_VERSION=${BASE_VERSION} iso img upgrade-lrzip bootstrap docker centos-repo ubuntu-repo centos-diff-repo ubuntu-diff-repo version-yaml openstack-yaml
+
+#########################################
+
+echo "STEP 3. Pack artifacts"
+cd ${ARTS_DIR}
+tar cvf "${ARTIFACT_NAME}.tar" bootstrap.tar.gz centos-repo.tar ubuntu-repo.tar puppet.tgz openstack.yaml version.yaml fuel-images.tar.*
+
+#########################################
+
+echo "STEP 4. Pack diffs"
+cd ${ARTS_DIR}
+tar cvf "${ARTIFACT_DIFF_NAME}.tar" puppet.tgz version.yaml openstack.yaml diff*
+
+#########################################
+
+echo "STEP 5. Publish everything"
+
+cd ${WORKSPACE}
+
+cp $LOCAL_MIRROR/*changelog ${ARTS_DIR}/ || true
+cp ${BUILD_DIR}/iso/isoroot/version.yaml ${WORKSPACE}/version.yaml.txt || true
+(cd ${BUILD_DIR}/iso/isoroot && find . | sed -s 's/\.\///') > ${WORKSPACE}/listing.txt || true
+
+##5.1) artifacts
+seedclient.py -pvf "${ARTS_DIR}/${ARTIFACT_NAME}.tar" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+
+# Preparing description + torrent link for upstream jobs
+echo http://seed.fuel-infra.org/fuelweb-iso/${ARTIFACT_NAME}.tar.torrent > ${WORKSPACE}/artifacts_torrent_link.txt
+echo "<a href=http://seed.fuel-infra.org/fuelweb-iso/${ARTIFACT_NAME}.tar.torrent>${ARTIFACT_NAME}</a>"
+
+##5.2) diff artifacts
+seedclient.py -pvf "${ARTS_DIR}/${ARTIFACT_DIFF_NAME}.tar" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+
+# Preparing description + torrent link for upstream jobs
+echo http://seed.fuel-infra.org/fuelweb-iso/${ARTIFACT_DIFF_NAME}.tar.torrent > ${WORKSPACE}/diff_torrent_link.txt
+echo "<a href=http://seed.fuel-infra.org/fuelweb-iso/${ARTIFACT_DIFF_NAME}.tar.torrent>${ARTIFACT_DIFF_NAME}</a>"
