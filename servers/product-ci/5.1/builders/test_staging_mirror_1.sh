1290c0049dac08c1497de4cbcb1f56e6a574f78b
Aleksandra Fedorova <afedorova@mirantis.com>
2015-03-06 19:34:19 +0300

    Fix staging mirror script
    Remove unnecessary logic from staging mirror script:
     all variables should be predefined.
    
    Change-Id: Ice4a08fdc3fd27ab569e930e2c22c31cc9c9114a

--- servers/product-ci/5.1/builders/test_staging_mirror_1.sh
+++ servers/product-ci/5.1/builders/test_staging_mirror_1.sh
@@ -1,19 +1,31 @@
-export MIRROR_DETECT=$(echo $FUEL_MAIN_BRANCH | grep '^stable\>/\<' | sed -e 's|^stable/||')
-[ -z "$MIRROR_DETECT" ]  && [ -z "$MIRROR" ] && exit 1
+#!/bin/bash
 
-export MIRROR=${MIRROR_DETECT:-$MIRROR}
-export MIRROR_HOST=osci-mirror-srt.srt.mirantis.net
-export MIRROR_VERSION=${MIRROR_VERSION:-$(rsync -l rsync://${MIRROR_HOST}/mirror/fwm/files/$MIRROR-staging | awk '/^l/ {print $NF}')}
+set -ex
 
+if [ -z "$MIRROR" ]; then
+    echo "No MIRROR"
+    exit -1
+fi
+
+if [ -z "$MIRROR_VERSION" ]; then
+    echo "No MIRROR_VERSION"
+    exit -1
+fi
+
+if [ -z "$MIRROR_BASE" ]; then
+    echo "No MIRROR_BASE"
+    exit -1
+fi
 
 PARAM_FILE=$WORKSPACE/mirror_staging.txt
 rm -f $PARAM_FILE
+
 echo "MIRROR = ${MIRROR}" >>$PARAM_FILE
 echo "MIRROR_VERSION = ${MIRROR_VERSION}" >>$PARAM_FILE
 echo "STABLE_VERSION = ${MIRROR_VERSION}" >>$PARAM_FILE
-MIRROR_BASE=http://${MIRROR_HOST}/fwm/files/${MIRROR_VERSION}
 echo "MIRROR_BASE = $MIRROR_BASE" >>$PARAM_FILE
 echo "fuelmain_gerrit_commit = ${extra_commits}" >>$PARAM_FILE
+
 if [ -n "$MIRROR_UBUNTU" ]; then
     echo "MIRROR_UBUNTU = $MIRROR_UBUNTU" >>$PARAM_FILE
 fi
