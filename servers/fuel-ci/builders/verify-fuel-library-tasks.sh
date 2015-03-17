f8641785c9b4247b8350b661d46b4594cacacc04
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-03-17 09:59:52 +0100

    Add gate-/verify-fuel-library-tasks jobs
    This job is going to replace 'fuellib_tasks_graph_check' which
    uses old way to validate deployment graph for granular deployemnt.
    Now 'tasks-validator' command is used.
    
    New label: verify-fuel-library-tasks
    Depends on: https://review.openstack.org/#/c/162163/
    
    Change-Id: Ib952a92ad1164f20058a6176b6a5f43aa7857cd6

---
+++ servers/fuel-ci/builders/verify-fuel-library-tasks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+set -ex
+
+VENV=${WORKSPACE}_VENV
+virtualenv -p python2.6 --clear ${VENV}
+source ${VENV}/bin/activate || exit 1
+
+pip install -r "${WORKSPACE}/utils/jenkins/tasks-test-requirements.txt"
+
+/bin/bash ${WORKSPACE}/utils/jenkins/tasks_run_tests.sh
