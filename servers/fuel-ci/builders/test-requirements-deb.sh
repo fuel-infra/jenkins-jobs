18db0358f31068e432be2af15bd71c56f72c5537
Vitaly Parakhin <vparakhin@mirantis.com>
2015-03-18 18:07:42 +0200

    Extend requirements tests with Trusty support and sorting check
    1) Since 6.1 we have different structure of MOS mirror for deb
    packages. Added conditional behavior for test-requirements-deb.
    2) Additional check for alphabetical sorting in requirements*.txt
    
    Change-Id: If2b5fd38df5f89c794fcc8641b4e33f092a9ff4c

--- servers/fuel-ci/builders/test-requirements-deb.sh
+++ servers/fuel-ci/builders/test-requirements-deb.sh
@@ -1,39 +1,65 @@
 #!/bin/bash
 
-set -ex
+set -x
 
-DIST=precise
+DIST=`awk -F '[:=?]' '/^UBUNTU_RELEASE\>/ {print $NF}' config.mk`
 OVERALL_STATUS=0
 
-REPO_NAME=ubuntu-fuel-`awk -F '[:=?]' '/^PRODUCT_VERSION\>/ {print $NF}' config.mk`-stable
-PACKAGES=`git diff --word-diff=plain HEAD^ HEAD requirements-deb.txt | egrep '^{+' | cut -d"+" -f2 | sed ':a;N;$!ba;s/\n/ /g'`
+PRODUCT_VERSION=`awk -F '[:=?]' '/^PRODUCT_VERSION\>/ {print $NF}' config.mk`
+REPO_NAME=ubuntu-fuel-$PRODUCT_VERSION-stable
+PACKAGES=`git diff --word-diff=plain HEAD~ requirements-deb.txt | egrep '^{+' | cut -d"+" -f2 | sed ':a;N;$!ba;s/\n/ /g'`
 
 if [ X"${PACKAGES}" = X"" ]; then
 	echo "MARK: no difference found, all requested packages exist in OBS and upstream repos."
-else
-	echo "MARK: checking OBS and upstream repos..."
-	TEMPDIR=$(/bin/mktemp -d /tmp/test-requirements-deb-XXXXXXXX)
-	echo "MARK: workspace directory is ${TEMPDIR}"
-	chdist --data-dir ${TEMPDIR} create ${REPO_NAME} http://mirror.fuel-infra.org/osci/${REPO_NAME}/ubuntu/ /
+	exit 0
+fi
 
-	cat >> ${TEMPDIR}/${REPO_NAME}/etc/apt/sources.list <<EOT
+#check if requirements-deb.txt is sorted alphabetically
+if git diff --name-only HEAD~ requirements-deb.txt >/dev/null; then
+        sort -u requirements-deb.txt| diff requirements-deb.txt -;
+        if [ $? -ne 0 ]; then
+                echo "MARK: FAILURE. requirements-deb.txt is not sorted. Please sort it with sort -u";
+                exit 1;
+        fi
+fi
+
+echo "MARK: checking OBS and upstream repos..."
+TEMPDIR=$(/bin/mktemp -d /tmp/test-requirements-deb-XXXXXXXX)
+echo "MARK: workspace directory is ${TEMPDIR}"
+
+if [ "${DIST}" = "precise" ]; then
+    chdist --data-dir ${TEMPDIR} create ${REPO_NAME} http://mirror.fuel-infra.org/osci/${REPO_NAME}/ubuntu/ /
+
+    cat >> ${TEMPDIR}/${REPO_NAME}/etc/apt/sources.list <<EOT
 deb http://mirror.yandex.ru/ubuntu ${DIST} main universe multiverse restricted
 deb http://mirror.yandex.ru/ubuntu ${DIST}-updates main universe multiverse restricted
 deb http://mirror.yandex.ru/ubuntu ${DIST}-security main universe multiverse restricted
 EOT
+else
+        chdist --data-dir ${TEMPDIR} create ${REPO_NAME} http://obs-1.mirantis.com/mos/ubuntu/ mos${PRODUCT_VERSION} main
 
-	echo 'APT::Get::AllowUnauthenticated 1;' > ${TEMPDIR}/${REPO_NAME}/etc/apt/apt.conf.d/02unauthenticated
-
-	chdist --data-dir ${TEMPDIR} apt-get ${REPO_NAME} update
-	chdist --data-dir ${TEMPDIR} apt-get ${REPO_NAME} install --dry-run ${PACKAGES}
-	RES=${?}
-	if [ ${RES} -eq 0 ]; then
-		echo "MARK: SUCCESS. The following packages are available in upstream repos: ${PACKAGES}"
-	else
-		echo "MARK: FAILURE. Requested packages were not found in both OBS and upstream repos"
-		OVERALL_STATUS=1
-	fi
-	rm -rf ${TEMPDIR}
+        cat > ${TEMPDIR}/${REPO_NAME}/etc/apt/sources.list <<EOT
+deb [arch=amd64] http://obs-1.mirantis.com/mos/ubuntu/ mos${PRODUCT_VERSION} main
+deb [arch=amd64] http://obs-1.mirantis.com/mos/ubuntu/ mos${PRODUCT_VERSION}-updates main
+deb [arch=amd64] http://obs-1.mirantis.com/mos/ubuntu/ mos${PRODUCT_VERSION}-security main
+deb http://mirror.fuel-infra.org/pkgs/ubuntu/ ${DIST} main universe multiverse restricted
+deb http://mirror.fuel-infra.org/pkgs/ubuntu/ ${DIST}-security main universe multiverse restricted
+deb http://mirror.fuel-infra.org/pkgs/ubuntu/ ${DIST}-updates main universe multiverse restricted
+EOT
 fi
+
+echo 'APT::Get::AllowUnauthenticated 1;' > ${TEMPDIR}/${REPO_NAME}/etc/apt/apt.conf.d/02unauthenticated
+
+chdist --data-dir ${TEMPDIR} apt-get ${REPO_NAME} update
+chdist --data-dir ${TEMPDIR} apt-get ${REPO_NAME} install --dry-run ${PACKAGES}
+RES=${?}
+if [ ${RES} -eq 0 ]; then
+	echo "MARK: SUCCESS. The following packages are available in upstream repos: ${PACKAGES}"
+else
+	echo "MARK: FAILURE. Requested packages were not found in both OBS and upstream repos"
+	OVERALL_STATUS=1
+fi
+
+rm -rf ${TEMPDIR}
 
 exit ${OVERALL_STATUS}
--- servers/fuel-ci/builders/test-requirements-rpm.sh
+++ servers/fuel-ci/builders/test-requirements-rpm.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-set -ex
+set -x
 
 OVERALL_STATUS=0
 
 REPO_NAME=centos-fuel-`awk -F '[:=?]' '/^PRODUCT_VERSION\>/ {print $NF}' config.mk`-stable
-PACKAGES="`git diff --word-diff=plain HEAD^ HEAD requirements-rpm.txt | egrep '^{+' | cut -d"+" -f2 | sed ':a;N;$!ba;s/\n/ /g'`"
+PACKAGES=`git diff --word-diff=plain HEAD~ requirements-rpm.txt | egrep '^{+' | cut -d"+" -f2 | sed ':a;N;$!ba;s/\n/ /g'`
 
 if [ X"${PACKAGES}" = X"" ]; then
 	echo "MARK: no difference found, all requested packages exist in OBS and upstream repos."
+	exit 0
+fi
+
+#check if requirements-rpm.txt is sorted alphabetically
+if git diff --name-only HEAD~ requirements-rpm.txt >/dev/null; then
+        sort -u requirements-rpm.txt| diff requirements-rpm.txt -;
+        if [ $? -ne 0 ]; then
+                echo "MARK: FAILURE. requirements-rpm.txt is not sorted. Please sort it with sort -u";
+                exit 1;
+        fi
+fi
+
+echo "MARK: checking OBS and upstream repos..."
+rm -rf /var/tmp/yum-${USER}*/*
+RES=`repoquery --repofrompath=upstream,http://mirror.centos.org/centos/6/os/x86_64/ --repofrompath==obs,http://mirror.fuel-infra.org/osci/${REPO_NAME}/centos/ --nvr -q ${PACKAGES}`
+if [ X"${RES}" = X"" ]; then
+	echo "MARK: FAILURE. Requested packages were not found in both OBS and upstream repos."
+	OVERALL_STATUS=1
 else
-	echo "MARK: checking OBS and upstream repos..."
-	rm -rf /var/tmp/yum-${USER}*/*
-	RES=`repoquery --repofrompath=upstream,http://mirror.centos.org/centos/6/os/x86_64/ --repofrompath==obs,http://mirror.fuel-infra.org/osci/${REPO_NAME}/centos/ --nvr -q ${PACKAGES}`
-	if [ X"${RES}" = X"" ]; then
-		echo "MARK: FAILURE. Requested packages were not found in both OBS and upstream repos."
-		OVERALL_STATUS=1
-	else
-		for PACKAGE in ${PACKAGES}; do
-			if echo ${RES} | grep -q ${PACKAGE}; then
-				echo "MARK: ${PACKAGE} found in upstream repo»"
-			else
-				echo "MARK: FAILURE: ${PACKAGE} not found in upstream repos"
-				OVERALL_STATUS=1
-			fi
-		done
-	fi
+	for PACKAGE in ${PACKAGES}; do
+		if echo ${RES} | grep -q ${PACKAGE}; then
+			echo "MARK: ${PACKAGE} found in upstream repo»"
+		else
+			echo "MARK: FAILURE: ${PACKAGE} not found in upstream repos"
+			OVERALL_STATUS=1
+		fi
+	done
 fi
 
 exit ${OVERALL_STATUS}
