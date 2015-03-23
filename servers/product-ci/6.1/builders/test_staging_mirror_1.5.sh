da01320ec7fe5b6019b2dcc0bc5988fe6c5e848c
Aleksandra Fedorova <afedorova@mirantis.com>
2015-03-23 19:26:55 +0300

    Add MIRROR_UBUNTU to staging bvt tests
    Similarly to main builds, record current available external ubuntu
    mirror and pass it to BVT tests.
    
    Add recorded variables as artifacts.
    
    Change-Id: I196b793cadd531d4688a46de1b789a0a35c4dd0d
    Related-Bug: #1434182

---
+++ servers/product-ci/6.1/builders/test_staging_mirror_1.5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+# Get the latest mirror and set the mirror id
+UBUNTU_MIRROR_URL=$(curl "http://mirror-pkgs.vm.mirantis.net/ubuntu-latest.htm")
+UBUNTU_MIRROR_ID=$(expr "${UBUNTU_MIRROR_URL}" : '.*/\(ubuntu-.*\)')
+
+# Create artifacts
+
+echo "UBUNTU_MIRROR_ID=${UBUNTU_MIRROR_ID}" > ubuntu_mirror_id.txt
+
+# FIXME: this is hardcoded url passed to all downstream tests. In
+# future we should pass only the mirror id and full url should be
+# determined according to server location.
+echo "MIRROR_UBUNTU=deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse" > mirror_ubuntu.txt
+
+# Create properties file for downstream jobs
+# artifacts/magnet_link.txt is already available from previous build step
+
+cat ubuntu_mirror_id.txt mirror_ubuntu.txt artifacts/magnet_link.txt > properties_file.txt
