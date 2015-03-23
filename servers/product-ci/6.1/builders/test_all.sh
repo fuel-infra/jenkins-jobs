7b0614d848db7d69ae9727d3750bc9671e0d097e
Aleksandra Fedorova <afedorova@mirantis.com>
2015-03-23 17:51:35 +0300

    Add logic for MIRROR_UBUNTU parameter
    6.1.test_all job will get the latest available Ubuntu mirror
    and record its timestamp and url into build artifacts.
    
    All downstream tests (for example, swarm) will use this data, so
    we will get unified test results.
    
    FIXME: Currently we hardcode the full Ubuntu mirror URL into job
    parameters. Later we need to add some logic for mirror to be defined
    for each slave according to slave location.
    
    Change-Id: I4b22ce161b0c5ec73ebbb82f709e111d8297c9cb
    Related-Bug: #1434182

--- servers/product-ci/6.1/builders/test_all.sh
+++ servers/product-ci/6.1/builders/test_all.sh
@@ -2,6 +2,18 @@
 
 set -ex
 
+# Get Ubuntu mirror url and id
+
+if [ "${UBUNTU_MIRROR_ID}" == 'latest' ]
+then
+    # Get the latest mirror and set the mirror id
+    UBUNTU_MIRROR_URL=$(curl "http://mirror-pkgs.vm.mirantis.net/ubuntu-latest.htm")
+    UBUNTU_MIRROR_ID=$(expr "${UBUNTU_MIRROR_URL}" : '.*/\(ubuntu-.*\)')
+else
+    # Don't change mirror id, set mirror url only
+    UBUNTU_MIRROR_URL="http://mirror-pkgs.vm.mirantis.net/${UBUNTU_MIRROR_ID}"
+fi
+
 # Get the real build number from the lastSuccessfulBuild link
 
 REAL_UPSTREAM_BUILD_NUMBER=$(curl "${UPSTREAM_JOB_URL}${UPSTREAM_BUILD_NUMBER}/buildNumber")
@@ -11,10 +23,18 @@ UPSTREAM_BUILD_URL="${UPSTREAM_JOB_URL}${REAL_UPSTREAM_BUILD_NUMBER}/"
 
 echo "Description string: <a href=\"${UPSTREAM_BUILD_URL}\">ISO ${REAL_UPSTREAM_BUILD_NUMBER}</a>"
 
-# Create artifact
+# Create artifacts
 
 echo "ISO_BUILD_URL=${UPSTREAM_BUILD_URL}" > iso_build_url.txt
+echo "UBUNTU_MIRROR_ID=${UBUNTU_MIRROR_ID}" > ubuntu_mirror_id.txt
 
-# Fetch MAGNET_LINK
+# FIXME: this is hardcoded url passed to all downstream tests. In
+# future we should pass only the mirror id and full url should be
+# determined according to server location.
+echo "MIRROR_UBUNTU=deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse" > mirror_ubuntu.txt
 
 curl "${UPSTREAM_BUILD_URL}artifact/magnet_link.txt" -o magnet_link.txt
+
+# Create properties file for downstream jobs
+
+cat ubuntu_mirror_id.txt mirror_ubuntu.txt magnet_link.txt > properties_file.txt
