078e9053e7fb681e2bca3e8520dac2cebf26e638
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-04-08 14:59:02 +0300

    Move voting to separate stage for deployment tests
    Change-Id: Ife2dc5ac3a31c2e512137ad5e2edb9f1a7c7703e

--- servers/openstack-ci/6.1/builders/mos.deploy.sh
+++ servers/openstack-ci/6.1/builders/mos.deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
+START_TS=$(date +%s)
+export TEST_JOB=true
+
 case "${GERRIT_PROJECT##*/}" in
     murano|murano-build)
         export SLAVE_NODE_MEMORY=6144
@@ -9,9 +12,12 @@ esac
 
 if bash -ex package-testing; then
   echo FAILED=false >> ci_status_params
-  EXITCODE=0
+  RESULT=0
 else
   echo FAILED=true >> ci_status_params
-  EXITCODE=1
+  RESULT=1
 fi
-exit $EXITCODE
+
+TIME_ELAPSED=$(( $(date +%s) - START_TS ))
+echo "RESULT=$RESULT" > setenvfile
+echo "TIME_ELAPSED='`date -u -d @$TIME_ELAPSED +'%Hh %Mm %Ss' | sed 's|^00h ||; s|^00m ||'`'" >> setenvfile
