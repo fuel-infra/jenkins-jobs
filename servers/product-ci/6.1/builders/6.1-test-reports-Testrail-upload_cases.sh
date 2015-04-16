dec6caf9646e73208fe824225b43bb2436f98a10
ss-yuriy <ishamrai@mirantis.com>
2015-04-16 12:06:10 +0300

    Upload tests cases to TestRail before publishing tests results
    Changes:
     - Added shell step into the job
     - Added upload_cases script
     - Removed TEMPORARY section (ISO number)
    
    Closes-Bug: #1444705
    Change-Id: I9fecbd15b768208a613f06949827f8a1d4c455a9

---
+++ servers/product-ci/6.1/builders/6.1-test-reports-Testrail-upload_cases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# TestRail
+
+set -ex
+
+export TESTRAIL_USER=${TESTRAIL_USER}
+export TESTRAIL_PASSWORD=${TESTRAIL_PASSWORD}
+
+# Prepare venv
+source /home/jenkins/venv-nailgun-tests-2.9/bin/activate
+
+python fuelweb_test/testrail/upload_cases_description.py -v -j ${TESTS_RUNNER}
