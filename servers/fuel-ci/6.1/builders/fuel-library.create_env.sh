8b86b833d0d71d50f0cc03c845c9dc361dd956c0
Aleksandra Fedorova <afedorova@mirantis.com>
2015-04-22 17:27:43 +0300

    Job to create env with new fuel-qa/ code
    Change-Id: If157a13d1d7e3a8b578419d4f0693a37f56c7175

---
+++ servers/fuel-ci/6.1/builders/fuel-library.create_env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -ex
+
+# stable/4.1 -> 4_1
+# master -> master
+BRANCH_ID=$(echo "${BRANCH##*/}" | sed 's:\.:_:g')
+
+if [ "${ISO_ID}" == "default" ]; then
+  ISO_ID="${BRANCH_ID}"
+  ENV_POSTFIX="${ISO_ID}"
+else
+  ENV_POSTFIX="${ISO_ID}-${BRANCH_ID}"
+fi
+
+ISO_PATH="/home/jenkins/workspace/iso/fuel_${ISO_ID}.iso"
+
+if [ ! -L "${ISO_PATH}" ]; then
+  exit -1
+fi
+
+VERSION_STRING=$(readlink "${ISO_PATH}" | cut -d '-' -f 2-3)
+echo "Description string: ${NODE} - ${VERSION_STRING}<br>${ENV_PREFIXES}<br>${SHA}"
+
+# Create copies of fuel-qa repo for each prefix
+
+source "${VENV_PATH}/bin/activate"
+
+for ENV_PREFIX in ${ENV_PREFIXES}; do
+    cp -r fuel-qa/ "workspace/fuel-main/${ENV_PREFIX}-${ISO_ID}/${BRANCH}"
+    dos.py erase "${ENV_PREFIX}-${ENV_POSTFIX}" || echo "Nothing to erase"
+done
+
+
