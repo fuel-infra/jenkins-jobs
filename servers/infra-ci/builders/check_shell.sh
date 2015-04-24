b7b5d31c84a339e328efe9e4fd8676bc4bf74682
Sergey Kulanov <skulanov@mirantis.com>
2015-04-24 21:52:04 +0300

    Implement job for checking shell scripts in jenkins-job project
    * Create jenkins-jobs.check_shell job
    (Currently in non-voting mode)
    * Fix jjb_compare_xml.sh according to shellcheck requirements
    * Rename jjb_compare_xml to jenkins-jobs.compare_xml
    
    Change-Id: Id89df6bf83e778aa65d5507f772606f29dec0889

---
+++ servers/infra-ci/builders/check_shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+ARGS="-e SC2013"
+
+if [ "${GERRIT_REFSPEC}" = "refs/heads/master" ]; then
+  find "${WORKSPACE}" -name "*.sh" -type f -print0 | xargs -0 shellcheck "${ARGS}"
+else
+  git diff HEAD~1 --name-only | grep ".sh$" | xargs --no-run-if-empty shellcheck "${ARGS}"
+fi
--- servers/infra-ci/builders/jjb_compare_xml.sh
+++ servers/infra-ci/builders/jjb_compare_xml.sh
@@ -5,40 +5,40 @@ set -ex
 GITHEAD=$(git rev-parse HEAD)
 OUT_DIR=${WORKSPACE}/output
 LOGFILE=${OUT_DIR}/jobs-diff.log
-BLOCKED=[blocked]\<br\>
-CHANGED=[changed]\<br\>
-ADDED=[added]\<br\>
-REMOVED=[removed]\<br\>
+BLOCKED="[blocked]\<br\>"
+CHANGED="[changed]\<br\>"
+ADDED="[added]\<br\>"
+REMOVED="[removed]\<br\>"
 RESULT=''
 BLOCKLIST=blocklist
 STAT=0
 
 # First generate output from BASE_COMMIT vars value
-git checkout ${BASE_COMMIT}
+git checkout "${BASE_COMMIT}"
 
-for ENV in $(ls ${WORKSPACE}/servers); do
-  tox -e compare-xml-old -- ${ENV}
+for ENV in ${WORKSPACE}/servers/*; do
+  tox -e compare-xml-old -- "${ENV##*/}"
 done
 
 # Then use that as a reference to compare against HEAD
-git checkout ${GITHEAD}
+git checkout "${GITHEAD}"
 
-for ENV in $(ls ${WORKSPACE}/servers); do
-  tox -e compare-xml-new -- ${ENV}
+for ENV in ${WORKSPACE}/servers/*; do
+  tox -e compare-xml-new -- "${ENV##*/}"
 done
 
-DIFF=$(diff -q -r -u ${OUT_DIR}/old ${OUT_DIR}/new &>${LOGFILE}; echo ${?})
+DIFF=$(diff -q -r -u "${OUT_DIR}/old" "${OUT_DIR}/new" &>"${LOGFILE}"; echo "${?}")
 # Any changed job discovered? If exit code was 1, then there is a difference
 if [[ ${DIFF} -eq 1 ]]; then
   # Loop through all changed jobs and compare them with a blocklist
-  for JOB in $(awk '/Files/ {print $2}' ${LOGFILE}); do
+  for JOB in $(awk '/Files/ {print $2}' "${LOGFILE}"); do
     # Extract job's name
-    JOB_NAME=$(basename ${JOB})
+    JOB_NAME=$(basename "${JOB}")
     # Extract job's ENV name (server/${ENV} to make sure, that we are comparing ENV/JOB_NAME with right ENV/BLOCKLIST.
-    JOB_ENV=$(echo ${JOB} | awk -F "/" '{print $(NF?NF-1:0)}')
+    JOB_ENV=$(echo "${JOB}" | awk -F "/" '{print $(NF?NF-1:0)}')
     for BL in ${WORKSPACE}/servers/${JOB_ENV}/${BLOCKLIST}; do
       # Do exact job name match when checking with blocklist.
-      GREP=$(grep -Fxq ${JOB_NAME} ${BL}; echo ${?})
+      GREP=$(grep -Fxq "${JOB_NAME}" "${BL}"; echo "${?}")
       if [[ ${GREP} -eq 0 ]]; then
         BLOCKED+=${JOB_NAME}\<br\>
         STAT=1
@@ -58,9 +58,9 @@ if [[ ${DIFF} -eq 1 ]]; then
     RESULT+=${CHANGED}
   fi
   # Now find added/removed Jobs...
-  for JOB in $(awk '/Only in/ {print $3$4}' ${LOGFILE}); do
-    ON=$(echo  ${JOB}|awk -F/ '{print $7}')
-    JOB=$(echo  ${JOB}|awk -F: '{print $2}')
+  for JOB in $(awk '/Only in/ {print $3$4}' "${LOGFILE}"); do
+    ON=$(echo  "${JOB}"|awk -F/ '{print $7}')
+    JOB=$(echo  "${JOB}"|awk -F: '{print $2}')
     if [[ ${ON} = 'old' ]]; then
       REMOVE=1
       REMOVED+=${JOB}\<br\>
@@ -77,5 +77,5 @@ if [[ ${DIFF} -eq 1 ]]; then
     RESULT+=${ADDED}
   fi
 fi
-echo ${RESULT}
+echo "${RESULT}"
 exit ${STAT}
