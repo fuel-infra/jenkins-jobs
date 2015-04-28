f11821b1633b7a36f996b9afe76219deb2153103
Sergey Kulanov <skulanov@mirantis.com>
2015-04-28 11:49:03 +0300

    Fix img publishing for community iso
    Since we don't have img artifact for > 6.1 version we need to skip
    it from publishing
    
    Change-Id: I17a1a4052c0a1412a18c883694504cba5ce9be98

--- servers/fuel-ci/builders/publish_fuel_community_iso.sh
+++ servers/fuel-ci/builders/publish_fuel_community_iso.sh
@@ -7,8 +7,15 @@ TARBALL_NAME=fuel-community-${UPGRADE_ID}
 
 seedclient.py -pvf "${ARTIFACTS_DIR}/${ISO_NAME}.iso" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
 
-seedclient.py -pvf "${ARTIFACTS_DIR}/${ISO_NAME}.img" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+# we don't have img artifact for >6.1
+if [ "${RELEASE}" != "6.1" ]; then
+  seedclient.py -pvf "${ARTIFACTS_DIR}/${ISO_NAME}.img" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+fi
 
 seedclient.py -pvf "${ARTIFACTS_DIR}/${TARBALL_NAME}.tar.lrz" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
 
-echo "META: ${ISO_ID}: <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.iso.torrent?from=jenkins>ISO</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.img.torrent?from=jenkins>IMG</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${TARBALL_NAME}.tar.lrz.torrent?from=jenkins>UPGD</a>"
+if [ "${RELEASE}" != "6.1" ]; then
+  echo "META: ${ISO_ID}: <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.iso.torrent?from=jenkins>ISO</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.img.torrent?from=jenkins>IMG</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${TARBALL_NAME}.tar.lrz.torrent?from=jenkins>UPGD</a>"
+else
+  echo "META: ${ISO_ID}: <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.iso.torrent?from=jenkins>ISO</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${TARBALL_NAME}.tar.lrz.torrent?from=jenkins>UPGD</a>"
+fi
