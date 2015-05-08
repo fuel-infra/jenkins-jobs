2df1e833c5a214a5e1c5155d35a85de78c3e4e75
Aleksandra Fedorova <afedorova@mirantis.com>
2015-05-08 14:56:48 +0300

    Create 7.0 jobs on product-ci
    - jobs use master branch
    
    - job configuration created by replacing 6.1 with 7.0 with two
      exceptional cases:
    
       - swarm.upgrades.yaml needs 6.1 branch as baserepo and data
         from 6.1 iso
    
       - in test_staging_mirror.yaml there is additional variable
         6_1_STAGING_ISO_BUILD_NUMBER
    
    - timer triggers disabled
    
    Change-Id: Icb59c814b48f968a5c9d328b39dd221881b113d7

---
+++ servers/product-ci/7.0/builders/build_staging_mirror.sh
@@ -0,0 +1,2 @@
+osci-mirrors/fuel_master_mirror_vc.sh
+echo "USE_STABLE_MOS_FOR_STAGING = $USE_STABLE_MOS_FOR_STAGING" >> ${WORKSPACE:-"."}/mirror_staging.txt
---
+++ servers/product-ci/7.0/builders/test_staging_mirror_1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -ex
+
+if [ -z "$MIRROR" ]; then
+    echo "No MIRROR"
+    exit -1
+fi
+
+if [ -z "$MIRROR_VERSION" ]; then
+    echo "No MIRROR_VERSION"
+    exit -1
+fi
+
+if [ -z "$MIRROR_BASE" ]; then
+    echo "No MIRROR_BASE"
+    exit -1
+fi
+
+PARAM_FILE=$WORKSPACE/mirror_staging.txt
+rm -f $PARAM_FILE
+
+echo "MIRROR = ${MIRROR}" >>$PARAM_FILE
+echo "MIRROR_VERSION = ${MIRROR_VERSION}" >>$PARAM_FILE
+echo "STABLE_VERSION = ${MIRROR_VERSION}" >>$PARAM_FILE
+echo "MIRROR_BASE = $MIRROR_BASE" >>$PARAM_FILE
+echo "fuelmain_gerrit_commit = ${extra_commits}" >>$PARAM_FILE
+echo "BUILD_MIRROR_URL = ${BUILD_MIRROR_URL}" >> $PARAM_FILE
+echo "USE_STABLE_MOS_FOR_STAGING = ${USE_STABLE_MOS_FOR_STAGING}" >> $PARAM_FILE
+
+if [ -n "$MIRROR_UBUNTU" ]; then
+    echo "MIRROR_UBUNTU = $MIRROR_UBUNTU" >>$PARAM_FILE
+fi
+if [ "$MIRROR_UBUNTU_SECURITY" = "MIRROR_UBUNTU" ]; then
+    echo "MIRROR_UBUNTU_SECURITY = $MIRROR_UBUNTU" >>$PARAM_FILE
+elif [ -n "$MIRROR_UBUNTU_SECURITY" ]; then
+    echo "MIRROR_UBUNTU_SECURITY = $MIRROR_UBUNTU_SECURITY" >>$PARAM_FILE
+fi
+if [ -n "$USE_MIRROR" ]; then
+    echo "USE_MIRROR = $USE_MIRROR" >>$PARAM_FILE
+fi
+
+rm -f build_description.*
