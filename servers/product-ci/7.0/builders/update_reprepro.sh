075e4261d2c37420729efa89345253af63736454
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-05-12 18:21:51 +0300

    Fix OBS url for 7.0 release
    Change-Id: Ib49efa1ca29739fb313c4d122e2368d9c1d5c3ee

--- servers/product-ci/7.0/builders/update_reprepro.sh
+++ servers/product-ci/7.0/builders/update_reprepro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-export OBSURL=https://osci-obs.vm.mirantis.net
+export OBSURL=https://obs-1.mirantis.com
 for REPONAME in $REPOS; do
      ssh jenkins@${OBSURL##*/} "/home/jenkins/update-reprepro.sh $REPONAME"
 done
