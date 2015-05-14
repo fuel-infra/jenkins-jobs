f1e080c18932c29d4897cea113bfbf753d6a0466
Aleksandra Fedorova <afedorova@mirantis.com>
2015-05-14 17:43:39 +0300

    Determine mirror from location in bvt/smoke
    - set test_all job so it doesn't define MIRROR_UBUNTU parameter
    - set test_staging_mirror so it doesn't define MIRROR_UBUNTU parameter
    - set system test script so if MIRROR_UBUNTU is undefined, it takes data
      from UBUNTU_MIRROR_ID and adds MIRROR_HOST from location.
    
    Related-Bug: #1452657
    
    Change-Id: I3ef9a6d60c0b10af4a47fa966e68956fc8c4c6dc

--- servers/product-ci/builders/run_system_test.sh
+++ servers/product-ci/builders/run_system_test.sh
@@ -1,3 +1,53 @@
+#!/bin/bash
+
+set -ex
+
+###################### Get MIRROR HOST ###############
+
+LOCATION_FACT=$(facter location)
+LOCATION=${LOCATION_FACT:-bud}
+
+case "${LOCATION}" in
+    srt)
+        MIRROR_HOST="http://osci-mirror-srt.srt.mirantis.net/pkgs/"
+        ;;
+    msk)
+        MIRROR_HOST="http://osci-mirror-msk.msk.mirantis.net/pkgs/"
+        ;;
+    kha)
+        MIRROR_HOST="http://osci-mirror-kha.kha.mirantis.net/pkgs/"
+        ;;
+    poz)
+        MIRROR_HOST="http://osci-mirror-poz.poz.mirantis.net/pkgs/"
+        ;;
+    bud)
+        MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org/pkgs/"
+        ;;
+    bud-ext)
+        MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org/pkgs/"
+        ;;
+    mnv)
+        MIRROR_HOST="http://mirror.seed-us1.fuel-infra.org/pkgs/"
+        ;;
+    *)
+        MIRROR_HOST="http://mirror.fuel-infra.org/pkgs/"
+esac
+
+###################### Get MIRROR_UBUNTU ###############
+
+if [[ ! "${MIRROR_UBUNTU}" ]]; then
+
+    case "${UBUNTU_MIRROR_ID}" in
+        latest)
+            UBUNTU_MIRROR_URL="$(curl ${MIRROR_HOST}ubuntu-latest.htm)"
+            ;;
+        *)
+            UBUNTU_MIRROR_URL="${MIRROR_HOST}${UBUNTU_MIRROR_ID}/"
+    esac
+
+    export MIRROR_UBUNTU="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+fi
+
 rm -rf logs/*
 
 export VENV_PATH=${VENV_PATH:-/home/jenkins/venv-nailgun-tests}
