cf8547210c65f77b447659c011e7e1d0bbe8de77
Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
2015-05-25 18:22:34 +0300

    Add report from patching to errata
    Change-Id: I4a55f0493f391703f02c6491ba18dd1e23ed8e9c

---
+++ servers/openstack-ci/6.1/builders/mos.patching.report.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -ex
+function retry {
+    local count=$1; shift
+    local ec
+    while [[ "$count" -gt 0 && "$ec" != 0 ]]
+    do
+        "$@" && true
+        ec=$?
+        sleep 5
+        ((count--))
+    done
+    return "$ec"
+}
+HOST="https://errata-tst.infra.mirantis.net"
+APPNAME="${ERRATA_USER}"
+ACCESSTOKEN="${ERRATA_PASSWORD}"
+if echo "${GERRIT_BRANCH}" | fgrep fuel ; then
+    RELEASE="$(echo "${GERRIT_BRANCH}" | egrep -o 'fuel-[0-9.]*' | egrep -o '[0-9.]*' | cat)"
+else RELEASE="${GERRIT_BRANCH}"
+fi
+
+if [ "${UPDATES}" ] && [ -f "project.envfile" ] ; then
+    source project.envfile
+    if echo "${JOB_NAME}" | fgrep deb ; then
+        DISTR="Ubuntu"
+    elif echo "${JOB_NAME}" | fgrep rpm ; then
+        DISTR="Centos"
+    fi
+    retry 3 curl -X POST -d "launchpad=${LP_BUG}&release=${RELEASE}&distro=${DISTR}&package=${PACKAGENAME}&version=${PACKAGEVERSION}" "${HOST}/api/package/update.json?application=${APPNAME}&token=${ACCESSTOKEN}"
+fi
