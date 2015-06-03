e0e89713b6c7ea22424f253d557ed71ea58193c5
Dmitry Kaigarodtsev <dkaiharodsev@mirantis.com>
2015-06-03 14:50:00 +0300

    Fix in 'compare_repos' job on 'product-ci':
    - used 'dpkg --compare-versions' instaed of native bash compare
    
    Closes-Bug:#1461843
    Change-Id: Ia196d695b5e71d795a2b2d27e4b31ae1ce21168e

--- servers/product-ci/builders/compare_repos.sh
+++ servers/product-ci/builders/compare_repos.sh
@@ -10,6 +10,7 @@ set -ex
 WGET=$(/usr/bin/which wget)
 JOIN=$(/usr/bin/which join)
 REPODIFF=$(/usr/bin/which repodiff)
+DPKG=$(/usr/bin/which dpkg)
 
 if [[ $TYPE == "DEB" ]]; then
 
@@ -26,12 +27,43 @@ if [[ $TYPE == "DEB" ]]; then
   $JOIN --nocheck-order old_list new_list > joined.list
 
   # making report
-  awk '{
-    if ($2 == $3) print $1" "$2" "$3  " no change";
-    else if ($2 < $3) print $1" "$2" "$3  " upgraded";
-    else if ($2 > $3) print $1" "$2" "$3  " downgraded";
-    else print $1" "$2" "$3  " new_package"
-  }' joined.list  > raw_report.list
+
+  INPUT_FILE="joined.list"
+
+  # read line by line
+  while read line
+
+  do
+    # definition of variables from each line
+    PKG_NAME=$(echo "$line" | awk '{print $1}')
+    OLD_VER=$(echo "$line" | awk '{print $2}')
+    NEW_VER=$(echo "$line" | awk '{print $3}')
+
+    # exit code for check 'does old version higther than new?'
+    EX_CODE_GT=$("$DPKG" --compare-versions "$OLD_VER" gt "$NEW_VER"; echo $?)
+
+    # exit code for check 'does old version lower than new?'
+    EX_CODE_LT=$("$DPKG" --compare-versions "$OLD_VER" lt "$NEW_VER"; echo $?)
+
+    # exit code for check 'does old version the same as new?'
+    EX_CODE_EQ=$("$DPKG" --compare-versions "$OLD_VER" eq "$NEW_VER"; echo $?)
+
+    # if exit code 0 - than old version is higher than new one - so that package is downgraded
+    if [[ "$EX_CODE_GT" -eq 0 ]]; then
+      echo "$PKG_NAME $OLD_VER > $NEW_VER downgraded"
+    fi
+
+    # if exit code 0 - than old version is lower than new one - so package is upgraded
+    if [[ "$EX_CODE_LT" -eq 0 ]]; then
+      echo "$PKG_NAME $OLD_VER $NEW_VER upgraded"
+    fi
+
+    # if exit code 0 - than old version is same as new one - so package is same
+    if [[ "$EX_CODE_EQ" -eq 0 ]]; then
+      echo "$PKG_NAME $OLD_VER > $NEW_VER same"
+    fi
+
+  done < $INPUT_FILE > raw_report.list
 
   # print report in a same format as 'repodiff' do
   echo -e "New packages:\n"
