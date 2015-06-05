f73c2b73bcc56c797c0aaaf6201d3f3db3c89b37
Aleksandra Fedorova <afedorova@mirantis.com>
2015-06-05 20:38:08 +0300

    Add parameters for Ubuntu repo and limit ENV_NAME
    Change-Id: I34592ce662b620be78b612e2b4c6070e0eb31873

--- servers/product-ci/7.0/builders/custom_system_test.sh
+++ servers/product-ci/7.0/builders/custom_system_test.sh
@@ -2,6 +2,58 @@
 
 set -ex
 
+TEST_ISO_JOB_URL="${JENKINS_URL}job/7.0.test_all/"
+
+###################### Get MIRROR HOST ###############
+
+LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location)
+LOCATION=${LOCATION_FACT:-bud}
+
+case "${LOCATION}" in
+    srt)
+        MIRROR_HOST="http://osci-mirror-srt.srt.mirantis.net/pkgs/"
+        ;;
+    msk)
+        MIRROR_HOST="http://osci-mirror-msk.msk.mirantis.net/pkgs/"
+        ;;
+    kha)
+        MIRROR_HOST="http://osci-mirror-kha.kha.mirantis.net/pkgs/"
+        ;;
+    poz)
+        MIRROR_HOST="http://osci-mirror-poz.poz.mirantis.net/pkgs/"
+        ;;
+    bud)
+        MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org/pkgs/"
+        ;;
+    bud-ext)
+        MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org/pkgs/"
+        ;;
+    mnv)
+        MIRROR_HOST="http://mirror.seed-us1.fuel-infra.org/pkgs/"
+        ;;
+    *)
+        MIRROR_HOST="http://mirror.fuel-infra.org/pkgs/"
+esac
+
+###################### Get MIRROR_UBUNTU ###############
+
+if [[ ! "${MIRROR_UBUNTU}" ]]; then
+
+    case "${UBUNTU_MIRROR_ID}" in
+        latest-stable)
+            UBUNTU_MIRROR_ID="$(curl -fsS "${TEST_ISO_JOB_URL}lastSuccessfulBuild/artifact/ubuntu_mirror_id.txt" | awk -F '[ =]' '{print $NF}')"
+            UBUNTU_MIRROR_URL="${MIRROR_HOST}${UBUNTU_MIRROR_ID}/"
+            ;;
+        latest)
+            UBUNTU_MIRROR_URL="$(curl ${MIRROR_HOST}ubuntu-latest.htm)"
+            ;;
+        *)
+            UBUNTU_MIRROR_URL="${MIRROR_HOST}${UBUNTU_MIRROR_ID}/"
+    esac
+
+    export MIRROR_UBUNTU="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+fi
+
 # Checking gerrit commits for fuel-qa
 if [[ "${fuel_qa_gerrit_commit}" != "none" ]] ; then
   for commit in ${fuel_qa_gerrit_commit} ; do
@@ -26,8 +78,10 @@ if [[ "${TEST_GROUP}" == "ha_neutron_destructive" ]]; then
   export NEUTRON_ENABLE="true"
 fi
 
-export ENV_NAME="6.1.custom.system_test.${TEST_GROUP}.${BUILD_NUMBER}"
-export OPENSTACK_RELASE="${OPENSTACK_RELEASE}"
+ENV_NAME=${ENV_PREFIX}.${BUILD_NUMBER}.${BUILD_ID}
+export ENV_NAME=${ENV_NAME:0:68}
+
+export OPENSTACK_RELEASE="${OPENSTACK_RELEASE}"
 
 echo "Description string: ${TEST_GROUP} on ${NODE_NAME}: ${ENV_NAME}"
 
