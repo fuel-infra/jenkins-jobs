ca19a742507bbf7907ee312b27325dc0866a0348
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2015-07-13 14:56:03 +0200

    For 7.0 swarm upgrades jobs construct different ENV_NAME var value
    The ENV_NAME variable, in 7.0 upgrade jobs, should not contain
    the {testgroup} and should have the timestamp right after the builds
    number.
    
    Change-Id: I7c48505fe09f8a1c045713ac0c144e7bea156d11
    Closes-Bug: #1473389

--- servers/product-ci/6.1/builders/run_upgrade_system_test.sh
+++ servers/product-ci/6.1/builders/run_upgrade_system_test.sh
@@ -4,39 +4,43 @@ set -ex
 
 # Set proper Openstack Release
 if [[ ${OPENSTACK_RELEASE} == 'centos' ]]; then
-	export OPENSTACK_RELEASE="CentOS"
+	export OPENSTACK_RELEASE=CentOS
 elif [[ ${OPENSTACK_RELEASE} == 'ubuntu' ]]; then
-	export OPENSTACK_RELEASE="Ubuntu"
+	export OPENSTACK_RELEASE=Ubuntu
 fi
 
-export VENV_PATH="/home/jenkins/venv-nailgun-tests-2.9"
-export CONNECTION_STRING="qemu+tcp://127.0.0.1:16509/system"
-export TIMESTAMP=$(date +%y%m%d%H%M)
-export ENV_NAME="${ENV_PREFIX}.${BUILD_NUMBER}.${TIMESTAMP}"
-export ENV_NAME="${ENV_NAME:0:68}"
-export FUEL_STATS_ENABLED="false"
+export CONNECTION_STRING='qemu+tcp://127.0.0.1:16509/system'
+export ENV_NAME=${ENV_PREFIX}.${BUILD_NUMBER}
+export ENV_NAME=${ENV_NAME:0:68}
+export FUEL_STATS_ENABLED=false
 
 rm -rf logs/*
 
 ISO_PATH=$(seedclient-wrapper -d -m "${BASE_ISO_MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
 
-export MAKE_SNAPSHOT="true"
+export MAKE_SNAPSHOT=true
 
 sh -x "BASE/utils/jenkins/system_tests.sh" -t test -w "${WORKSPACE}/BASE" -e "${ENV_NAME}" -o --group="${TEST_GROUP}" -i "${ISO_PATH}"
 
 echo "Description string: ${TEST_GROUP} on ${VERSION_STRING}"
 
-export MAKE_SNAPSHOT="false"
+export MAKE_SNAPSHOT=false
 
 export TARBALL_PATH=$(seedclient-wrapper -d -m "${UPGRADE_TARBALL_MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
 
-VERSION_STRING=$(basename "${TARBALL_PATH}" | cut -d '-' -f 2-4)
+VERSION_STRING=$(basename ${TARBALL_PATH} | cut -d '-' -f 2-4)
 echo "Description string: ${VERSION_STRING}"
 
-export UPGRADE_FUEL_FROM=$(basename "${ISO_PATH}" | cut -d '-' -f 2 | sed s/.iso//g)
-export UPGRADE_FUEL_TO=$(basename "${TARBALL_PATH}" | cut -d '-' -f 2)
+export UPGRADE_FUEL_FROM=$(basename ${ISO_PATH} | cut -d '-' -f 2 | sed s/.iso//g)
+export UPGRADE_FUEL_TO=$(basename ${TARBALL_PATH} | cut -d '-' -f 2)
 
-export VENV_PATH="/home/jenkins/venv-nailgun-tests-2.9"
+if [[ ${OLD_DEVOPS_DB} == 'true' ]]; then
+  export DEVOPS_DB_NAME='devops'
+  export DEVOPS_DB_USER='devops'
+  export DEVOPS_DB_PASSWORD='devops'
+fi
+
+export VENV_PATH=/home/jenkins/venv-nailgun-tests-2.9
 
 # Use -k to reuse environment
 sh -x "UPGRADE/utils/jenkins/system_tests.sh" -k -t test -w "${WORKSPACE}/UPGRADE" -e "${ENV_NAME}" -o --group="${UPGRADE_TEST_GROUP}" -i "${ISO_PATH}"
