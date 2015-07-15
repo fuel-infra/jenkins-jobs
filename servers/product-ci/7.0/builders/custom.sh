ac8834e1cc192939ac1e3cec58d8c161ce284805
Sergey Kulanov <skulanov@mirantis.com>
2015-07-15 14:11:11 +0300

    Add fuel-agent related parameters to custom/staging 7.0 ISO build
    Since we've moved fuel-agent to separate repo, we need to be able
    to build ISO with cherry-picked commits from fuel-agent repo
    
    * Update custom_7.0_iso job
    * 7.0.staging.iso
    
    Change-Id: I71090a45365fe1c3cabcb9f1c4a4a56972e06d68
    Related-change: https://review.openstack.org/#/c/199178/

--- servers/product-ci/7.0/builders/custom.sh
+++ servers/product-ci/7.0/builders/custom.sh
@@ -2,32 +2,31 @@
 
 set -ex
 
-export LANG="C"
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:${PATH}
 
-PROD_VER=`grep 'PRODUCT_VERSION:=' config.mk | cut -d= -f2`
+PROD_VER=$(grep 'PRODUCT_VERSION:=' config.mk | cut -d= -f2)
 
-export ISO_NAME=fuel-gerrit-${PROD_VER}-${BUILD_NUMBER}-${BUILD_ID}
-export UPGRADE_TARBALL_NAME=fuel-gerrit-${PROD_VER}-upgrade-${BUILD_NUMBER}-${BUILD_ID}
+export ISO_NAME="fuel-gerrit-${PROD_VER}-${BUILD_NUMBER}-${BUILD_ID}"
+export UPGRADE_TARBALL_NAME="fuel-gerrit-${PROD_VER}-upgrade-${BUILD_NUMBER}-${BUILD_ID}"
 
-export BUILD_DIR=${WORKSPACE}/../tmp/${JOB_NAME}/build
-export LOCAL_MIRROR=${WORKSPACE}/../tmp/${JOB_NAME}/local_mirror
+export BUILD_DIR="${WORKSPACE}/../tmp/${JOB_NAME}/build"
+export LOCAL_MIRROR="${WORKSPACE}/../tmp/${JOB_NAME}/local_mirror"
 
-export ARTS_DIR=${WORKSPACE}/artifacts
-rm -rf ${ARTS_DIR}
+export ARTS_DIR="${WORKSPACE}/artifacts"
+rm -rf "${ARTS_DIR}"
 
-export DEPS_DIR=${BUILD_DIR}/deps
+export DEPS_DIR="${BUILD_DIR}/deps"
 rm -rf "${DEPS_DIR}"
 
 # Checking gerrit commits for fuel-main
 if [ "${FUELMAIN_COMMIT}" != "master" ] ; then
-    git checkout ${FUELMAIN_COMMIT}
+    git checkout "${FUELMAIN_COMMIT}"
 fi
 
 # Checking gerrit commits for fuel-main
 if [ "${fuelmain_gerrit_commit}" != "none" ] ; then
   for commit in ${fuelmain_gerrit_commit} ; do
-    git fetch https://review.openstack.org/stackforge/fuel-main ${commit} && git cherry-pick FETCH_HEAD || false
+    git fetch https://review.openstack.org/stackforge/fuel-main "${commit}" && git cherry-pick FETCH_HEAD || false
   done
 fi
 
@@ -36,6 +35,7 @@ export ASTUTE_GERRIT_COMMIT="${astute_gerrit_commit}"
 export OSTF_GERRIT_COMMIT="${ostf_gerrit_commit}"
 export FUELLIB_GERRIT_COMMIT="${fuellib_gerrit_commit}"
 export PYTHON_FUELCLIENT_GERRIT_COMMIT="${python_fuelclient_gerrit_commit}"
+export FUEL_AGENT_GERRIT_COMMIT="${fuel_agent_gerrit_commit}"
 
 ######## Get node location to choose closer mirror ###############
 
@@ -94,33 +94,34 @@ make $make_args iso version-yaml
 
 echo "STEP 2. Publish everything"
 
-cd ${ARTS_DIR}
-for artifact in `ls fuel-*`
+cd "${ARTS_DIR}"
+for artifact in $(ls fuel-*)
 do
- ${WORKSPACE}/utils/jenkins/process_artifacts.sh ${artifact}
+  "${WORKSPACE}/utils/jenkins/process_artifacts.sh" "${artifact}"
 done
 
-cd ${WORKSPACE}
+cd "${WORKSPACE}"
 
-echo FUELMAIN_GERRIT_COMMIT="${fuelmain_gerrit_commit}" > ${ARTS_DIR}/gerrit_commits.txt
-echo NAILGUN_GERRIT_COMMIT="${nailgun_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
-echo ASTUTE_GERRIT_COMMIT="${astute_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
-echo OSTF_GERRIT_COMMIT="${ostf_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
-echo FUELLIB_GERRIT_COMMIT="${fuellib_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
-echo PYTHON_FUELCLIENT_GERRIT_COMMIT="${python_fuelclient_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
+echo FUELMAIN_GERRIT_COMMIT="${fuelmain_gerrit_commit}" > "${ARTS_DIR}/gerrit_commits.txt"
+echo NAILGUN_GERRIT_COMMIT="${nailgun_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo ASTUTE_GERRIT_COMMIT="${astute_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo OSTF_GERRIT_COMMIT="${ostf_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo FUELLIB_GERRIT_COMMIT="${fuellib_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo PYTHON_FUELCLIENT_GERRIT_COMMIT="${python_fuelclient_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo FUEL_AGENT_GERRIT_COMMIT="${fuel_agent_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
 
-cp ${LOCAL_MIRROR}/*changelog ${ARTS_DIR}/ || true
-cp ${BUILD_DIR}/iso/isoroot/version.yaml ${ARTS_DIR}/version.yaml.txt || true
-(cd ${BUILD_DIR}/iso/isoroot && find . | sed -s 's/\.\///') > ${ARTS_DIR}/listing.txt || true
+cp "${LOCAL_MIRROR}/*changelog" "${ARTS_DIR}/" || true
+cp "${BUILD_DIR}/iso/isoroot/version.yaml" "${ARTS_DIR}/version.yaml.txt" || true
+(cd "${BUILD_DIR}/iso/isoroot" && find . | sed -s 's/\.\///') > "${ARTS_DIR}/listing.txt" || true
 
-grep MAGNET_LINK ${ARTS_DIR}/*iso.data.txt > ${ARTS_DIR}/magnet_link.txt
+grep MAGNET_LINK "${ARTS_DIR}/*iso.data.txt" > "${ARTS_DIR}/magnet_link.txt"
 
 # Generate build description
-ISO_MAGNET_LINK=`grep MAGNET_LINK ${ARTS_DIR}/*iso.data.txt | sed 's/MAGNET_LINK=//'`
-ISO_HTTP_LINK=`grep HTTP_LINK ${ARTS_DIR}/*iso.data.txt | sed 's/HTTP_LINK=//'`
-ISO_HTTP_TORRENT=`grep HTTP_TORRENT ${ARTS_DIR}/*iso.data.txt | sed 's/HTTP_TORRENT=//'`
+ISO_MAGNET_LINK=$(grep MAGNET_LINK "${ARTS_DIR}/*iso.data.txt" | sed 's/MAGNET_LINK=//')
+ISO_HTTP_LINK=$(grep HTTP_LINK "${ARTS_DIR}/*iso.data.txt" | sed 's/HTTP_LINK=//')
+ISO_HTTP_TORRENT=$(grep HTTP_TORRENT "${ARTS_DIR}/*iso.data.txt" | sed 's/HTTP_TORRENT=//')
 
-echo "<a href="${ISO_HTTP_LINK}">ISO download link</a> <a href="${ISO_HTTP_TORRENT}">ISO torrent link</a><br>${ISO_MAGNET_LINK}<br>"
+echo "<a href=${ISO_HTTP_LINK}>ISO download link</a> <a href=${ISO_HTTP_TORRENT}>ISO torrent link</a><br>${ISO_MAGNET_LINK}<br>"
 
 #########################################
 
--- servers/product-ci/7.0/builders/staging.iso.sh
+++ servers/product-ci/7.0/builders/staging.iso.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-echo STARTED_TIME="`date -u +'%Y-%m-%dT%H:%M:%S'`" > ci_status_params.txt
+echo STARTED_TIME="$(date -u +'%Y-%m-%dT%H:%M:%S')" > ci_status_params.txt
 
 # Checking gerrit commits for fuel-main
 if [ "${FUELMAIN_COMMIT}" != "master" ] ; then
@@ -11,19 +11,18 @@ fi
 
 PROD_VER=$(grep 'PRODUCT_VERSION:=' config.mk | cut -d= -f2)
 
-export LANG="C"
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:${PATH}
 
-export ISO_NAME=fuel-staging-${PROD_VER}-${BUILD_NUMBER}-${BUILD_ID}
-export UPGRADE_TARBALL_NAME=fuel-staging-${PROD_VER}-upgrade-${BUILD_NUMBER}-${BUILD_ID}
+export ISO_NAME="fuel-staging-${PROD_VER}-${BUILD_NUMBER}-${BUILD_ID}"
+export UPGRADE_TARBALL_NAME="fuel-staging-${PROD_VER}-upgrade-${BUILD_NUMBER}-${BUILD_ID}"
 
-export BUILD_DIR=${WORKSPACE}/../tmp/${JOB_NAME}/build
-export LOCAL_MIRROR=${WORKSPACE}/../tmp/${JOB_NAME}/local_mirror
+export BUILD_DIR="${WORKSPACE}/../tmp/${JOB_NAME}/build"
+export LOCAL_MIRROR="${WORKSPACE}/../tmp/${JOB_NAME}/local_mirror"
 
-export ARTS_DIR=${WORKSPACE}/artifacts
-rm -rf ${ARTS_DIR}
+export ARTS_DIR="${WORKSPACE}/artifacts"
+rm -rf "${ARTS_DIR}"
 
-rm -f ${WORKSPACE}/version.yaml
+rm -f "${WORKSPACE}/version.yaml"
 
 if [ "${USE_STABLE_MOS_FOR_STAGING}" = "true" ]; then
 
@@ -66,20 +65,21 @@ print(sorted(iso_urls)[-1])
 ")
 
     # geting of last stable iso commits
-    curl -fsS ${JENKINS_STABLE_ISO_BUILD_URL}artifact/version.yaml.txt > ${WORKSPACE:-.}/version.yaml
-    export VERSIONS=$(cat ${WORKSPACE:-.}/version.yaml)
-    export NAILGUN_COMMIT=$(echo -e "$VERSIONS" | awk '/nailgun_sha:/ {print $NF}')
-    export PYTHON_FUELCLIENT_COMMIT=$(echo -e "$VERSIONS" | awk '/python-fuelclient_sha:/ {print $NF}')
-    export ASTUTE_COMMIT=$(echo -e "$VERSIONS" | awk '/astute_sha:/ {print $NF}')
-    export FUELLIB_COMMIT=$(echo -e "$VERSIONS" | awk '/fuel-library_sha:/ {print $NF}')
-    export OSTF_COMMIT=$(echo -e "$VERSIONS" | awk '/ostf_sha:/ {print $NF}')
-    export FUELMAIN_COMMIT=${FUELMAIN_COMMIT:-$(echo -e "$VERSIONS" | awk '/fuelmain_sha:/ {print $NF}')}
+    curl -fsS "${JENKINS_STABLE_ISO_BUILD_URL}artifact/version.yaml.txt" > "${WORKSPACE:-.}/version.yaml"
+    export VERSIONS=$(cat "${WORKSPACE:-.}/version.yaml")
+    export NAILGUN_COMMIT=$(echo -e "${VERSIONS}" | awk '/nailgun_sha:/ {print $NF}')
+    export PYTHON_FUELCLIENT_COMMIT=$(echo -e "${VERSIONS}" | awk '/python-fuelclient_sha:/ {print $NF}')
+    export ASTUTE_COMMIT=$(echo -e "${VERSIONS}" | awk '/astute_sha:/ {print $NF}')
+    export FUELLIB_COMMIT=$(echo -e "${VERSIONS}" | awk '/fuel-library_sha:/ {print $NF}')
+    export OSTF_COMMIT=$(echo -e "${VERSIONS}" | awk '/ostf_sha:/ {print $NF}')
+    export FUEL_AGENT_COMMIT=$(echo -e "${VERSIONS}" | awk '/fuel-agent_sha:/ {print $NF}')
+    export FUELMAIN_COMMIT=${FUELMAIN_COMMIT:-$(echo -e "${VERSIONS}" | awk '/fuelmain_sha:/ {print $NF}')}
 fi
 
 # Checking gerrit commits for fuel-main
 if [ "${fuelmain_gerrit_commit}" != "none" ] ; then
   for commit in ${fuelmain_gerrit_commit} ; do
-    git fetch https://review.openstack.org/stackforge/fuel-main ${commit} && git cherry-pick FETCH_HEAD || false
+    git fetch https://review.openstack.org/stackforge/fuel-main "${commit}" && git cherry-pick FETCH_HEAD || false
   done
 fi
 
@@ -88,6 +88,7 @@ export ASTUTE_GERRIT_COMMIT="${astute_gerrit_commit}"
 export OSTF_GERRIT_COMMIT="${ostf_gerrit_commit}"
 export FUELLIB_GERRIT_COMMIT="${fuellib_gerrit_commit}"
 export PYTHON_FUELCLIENT_GERRIT_COMMIT="${python_fuelclient_gerrit_commit}"
+export FUEL_AGENT_GERRIT_COMMIT="${fuel_agent_gerrit_commit}"
 
 #########################################
 
@@ -105,40 +106,41 @@ make ${make_args} iso version-yaml ${add_make_target}
 
 echo "STEP 2. Publish everything"
 
-cd ${ARTS_DIR}
+cd "${ARTS_DIR}"
 
 for artifact in $(ls fuel-*)
 do
-  ${WORKSPACE}/utils/jenkins/process_artifacts.sh ${artifact}
+  "${WORKSPACE}/utils/jenkins/process_artifacts.sh" "${artifact}"
 done
 
-cd ${WORKSPACE}
+cd "${WORKSPACE}"
 
-echo FUELMAIN_GERRIT_COMMIT="${fuelmain_gerrit_commit}" > ${ARTS_DIR}/gerrit_commits.txt
-echo NAILGUN_GERRIT_COMMIT="${nailgun_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
-echo ASTUTE_GERRIT_COMMIT="${astute_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
-echo OSTF_GERRIT_COMMIT="${ostf_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
-echo FUELLIB_GERRIT_COMMIT="${fuellib_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
-echo PYTHON_FUELCLIENT_GERRIT_COMMIT="${python_fuelclient_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
+echo FUELMAIN_GERRIT_COMMIT="${fuelmain_gerrit_commit}" > "${ARTS_DIR}/gerrit_commits.txt"
+echo NAILGUN_GERRIT_COMMIT="${nailgun_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo ASTUTE_GERRIT_COMMIT="${astute_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo OSTF_GERRIT_COMMIT="${ostf_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo FUELLIB_GERRIT_COMMIT="${fuellib_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo PYTHON_FUELCLIENT_GERRIT_COMMIT="${python_fuelclient_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo FUEL_AGENT_GERRIT_COMMIT="${fuel_agent_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
 
-cp ${LOCAL_MIRROR}/*changelog ${ARTS_DIR}/ || true
-cp ${BUILD_DIR}/iso/isoroot/version.yaml ${ARTS_DIR}/version.yaml.txt || true
-(cd ${BUILD_DIR}/iso/isoroot && find . | sed -s 's/\.\///') > ${ARTS_DIR}/listing.txt || true
+cp "${LOCAL_MIRROR}/*changelog" "${ARTS_DIR}/" || true
+cp "${BUILD_DIR}/iso/isoroot/version.yaml" "${ARTS_DIR}/version.yaml.txt" || true
+(cd "${BUILD_DIR}/iso/isoroot" && find . | sed -s 's/\.\///') > "${ARTS_DIR}/listing.txt" || true
 
-grep MAGNET_LINK ${ARTS_DIR}/*iso.data.txt > ${ARTS_DIR}/magnet_link.txt
+grep MAGNET_LINK "${ARTS_DIR}/*iso.data.txt" > "${ARTS_DIR}/magnet_link.txt"
 
 # Generate build description
-ISO_MAGNET_LINK=$(grep MAGNET_LINK ${ARTS_DIR}/*iso.data.txt | sed 's/MAGNET_LINK=//')
-ISO_HTTP_LINK=$(grep HTTP_LINK ${ARTS_DIR}/*iso.data.txt | sed 's/HTTP_LINK=//')
-ISO_HTTP_TORRENT=$(grep HTTP_TORRENT ${ARTS_DIR}/*iso.data.txt | sed 's/HTTP_TORRENT=//')
+ISO_MAGNET_LINK=$(grep MAGNET_LINK "${ARTS_DIR}/*iso.data.txt" | sed 's/MAGNET_LINK=//')
+ISO_HTTP_LINK=$(grep HTTP_LINK "${ARTS_DIR}/*iso.data.txt" | sed 's/HTTP_LINK=//')
+ISO_HTTP_TORRENT=$(grep HTTP_TORRENT "${ARTS_DIR}/*iso.data.txt" | sed 's/HTTP_TORRENT=//')
 
-echo "DESCRIPTION=<a href=${ISO_HTTP_TORRENT}>ISO</a>" > ${ARTS_DIR}/status_description.txt
+echo "DESCRIPTION=<a href=${ISO_HTTP_TORRENT}>ISO</a>" > "${ARTS_DIR}/status_description.txt"
 
-echo "<a href="${ISO_HTTP_LINK}">ISO download link</a> <a href="${ISO_HTTP_TORRENT}">ISO torrent link</a><br>${ISO_MAGNET_LINK}<br>"
+echo "<a href=${ISO_HTTP_LINK}>ISO download link</a> <a href=${ISO_HTTP_TORRENT}>ISO torrent link</a><br>${ISO_MAGNET_LINK}<br>"
 
 #########################################
 
 echo "STEP 3. Clean after build"
 make deep_clean
 
-echo FINISHED_TIME="`date -u +'%Y-%m-%dT%H:%M:%S'`" >> ci_status_params.txt
+echo FINISHED_TIME="$(date -u +'%Y-%m-%dT%H:%M:%S')" >> ci_status_params.txt
