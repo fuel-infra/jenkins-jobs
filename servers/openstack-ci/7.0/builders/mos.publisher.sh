630ce0a78c04caf8f63a59c300cedd79636f0c0c
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-07-21 13:31:22 +0300

    [7.0] [publisher] Implement new repository scheme
        Implement new repository scheme in parallel with old one
    
    Change-Id: Ia35e8985327734fd36fd7c7bbe1c7e09c9e9ed65

--- servers/openstack-ci/7.0/builders/mos.publisher.sh
+++ servers/openstack-ci/7.0/builders/mos.publisher.sh
@@ -7,3 +7,24 @@ case "${REPO_TYPE}" in
     rpm) bash publisher/publish-rpm-binaries.sh ;;
       *) bash publisher/publish-deb-binaries.sh ;;
 esac
+
+export REPO_REQUEST_PATH_PREFIX=review/v4/
+
+export RPM_OS_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/os/x86_64"
+export RPM_PROPOSED_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/proposed/x86_64"
+export RPM_UPDATES_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/updates/x86_64"
+export RPM_SECURITY_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/security/x86_64"
+export RPM_HOLDBACK_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/holdback/x86_64"
+
+export DEB_REPO_PATH="mos-repos/ubuntu"
+export DEB_DIST_NAME="${PROJECT_NAME}${PROJECT_VERSION}"
+export DEB_PROPOSED_DIST_NAME="${DEB_DIST_NAME}-proposed"
+export DEB_UPDATES_DIST_NAME="${DEB_DIST_NAME}-updates"
+export DEB_SECURITY_DIST_NAME="${DEB_DIST_NAME}-security"
+export DEB_HOLDBACK_DIST_NAME="${DEB_DIST_NAME}-holdback"
+
+case "${REPO_TYPE}" in
+        rpm) bash -ex publisher.v4/publish-rpm-binaries.sh ;;
+        deb) bash -ex publisher.v4/publish-deb-binaries.sh ;;
+         * ) echo "Unsupported repository type" ; exit 1 ;;
+esac
