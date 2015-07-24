b61602c9f7c524d5ffd6a63bf71300b86e874646
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-07-24 18:20:11 +0300

    [7.0] Switch to new repository schema
    Change-Id: Id832d3f24f04b04b0d21aedff58e0fc9288831c5

--- servers/openstack-ci/7.0/builders/mos.publisher.sh
+++ servers/openstack-ci/7.0/builders/mos.publisher.sh
@@ -4,27 +4,18 @@ set -o xtrace
 set -o errexit
 
 case "${REPO_TYPE}" in
-    rpm) bash publisher/publish-rpm-binaries.sh ;;
-      *) bash publisher/publish-deb-binaries.sh ;;
-esac
-
-export REPO_REQUEST_PATH_PREFIX=review/
-
-export RPM_OS_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/os"
-export RPM_PROPOSED_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/cr"
-export RPM_UPDATES_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/updates"
-export RPM_SECURITY_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/security"
-export RPM_HOLDBACK_REPO_PATH="mos-repos/centos/${PROJECT_NAME}${PROJECT_VERSION}-centos6-fuel/holdback"
-
-export DEB_REPO_PATH="mos-repos/ubuntu"
-export DEB_DIST_NAME="${PROJECT_NAME}${PROJECT_VERSION}"
-export DEB_PROPOSED_DIST_NAME="${DEB_DIST_NAME}-proposed"
-export DEB_UPDATES_DIST_NAME="${DEB_DIST_NAME}-updates"
-export DEB_SECURITY_DIST_NAME="${DEB_DIST_NAME}-security"
-export DEB_HOLDBACK_DIST_NAME="${DEB_DIST_NAME}-holdback"
-
-case "${REPO_TYPE}" in
         rpm) bash -ex publisher.v4/publish-rpm-binaries.sh ;;
         deb) bash -ex publisher.v4/publish-deb-binaries.sh ;;
          * ) echo "Unsupported repository type" ; exit 1 ;;
+esac
+
+export REPO_PATH_PREFIX=osci/
+export REPO_REQUEST_PATH_PREFIX=osci/review/
+export BASE_PATH=base/
+export UPDATES_PATH=updates/
+export SECURITY_PATH=security/
+
+case "${REPO_TYPE}" in
+    rpm) bash publisher/publish-rpm-binaries.sh ;;
+      *) bash publisher/publish-deb-binaries.sh ;;
 esac
