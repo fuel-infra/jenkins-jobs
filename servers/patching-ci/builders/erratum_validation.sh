4dc0a04f28ac67edb018a8bff78c2206df4a5631
vrovachev <vrovachev@mirantis.com>
2015-07-27 16:01:38 +0400

    Add virtualenv for erratumvalidation job
    Added and activated python virtualenv for
    validation arratum schema job
    
    Change-Id: I76e55e2a62c8b40fd956ecf8d8e47eac3d1a4fea
    Closes-bug: #1478496

--- servers/patching-ci/builders/erratum_validation.sh
+++ servers/patching-ci/builders/erratum_validation.sh
@@ -2,4 +2,11 @@
 
 git clone https://review.fuel-infra.org/tools/sustaining
 
+VENV="${WORKSPACE}_VENV"
+
+virtualenv "${VENV}"
+source "${VENV}"/bin/activate || exit 1
+
+pip install -r sustaining/scripts/erratumvalidation_requirements.txt
+
 git diff HEAD~1 --name-only --diff-filter=AM | grep ".yaml$" | xargs --no-run-if-empty python sustaining/scripts/erratumvalidation.py
