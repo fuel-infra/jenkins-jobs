2b4b716e3f936ba89d58bbc4fe5cc3ed574cb54f
Sergey Kulanov <skulanov@mirantis.com>
2015-07-31 11:26:30 +0300

    Remove YUM_DOWNLOAD_SRC flag from build staging mirror job
    * Partial revert New perestroika mirror structure for build staging mirror
      job only (83b00faedce9ecc28fbb87f5c3316256244105ed)
    * remove YUM_DOWNLOAD_SRC from 7.0_build_staging_mirror job
    
    Change-Id: I6fb685e03c8a254d316837584c10ce90dd783af1

--- servers/product-ci/7.0/builders/build_staging_mirror_1.sh
+++ servers/product-ci/7.0/builders/build_staging_mirror_1.sh
@@ -7,11 +7,8 @@ echo STARTED_TIME="$(date -u +'%Y-%m-%dT%H:%M:%S')" > ci_status_params.txt
 LATEST_MIRROR_ID=$(curl -s http://perestroika-repo-tst.infra.mirantis.net/osci/mos/7.0.target.txt | head -1)
 # re-define MIRROR_UBUNTU_ROOT value from fuel-main make system
 export MIRROR_UBUNTU_ROOT="/osci/mos/${LATEST_MIRROR_ID}/cluster/base/trusty/"
-LATEST_SUITE=$(curl -s http://perestroika-repo-tst.infra.mirantis.net/mos-repos/ubuntu/dists/mos7.0.target.txt | head -1)
-export MIRROR_MOS_UBUNTU_SUITE="${LATEST_SUITE}"
 # re-define MIRROR_FUEL value from fuel-main make system
-LATEST_CENTOS=$(curl -s http://perestroika-repo-tst.infra.mirantis.net/mos-repos/centos/mos7.0-centos6-fuel/os.target.txt | head -1)
-export MIRROR_FUEL="http://perestroika-repo-tst.infra.mirantis.net/mos-repos/centos/mos7.0-centos6-fuel/${LATEST_CENTOS}/x86_64"
+export MIRROR_FUEL="http://perestroika-repo-tst.infra.mirantis.net/osci/mos/${LATEST_MIRROR_ID}/fuel/base/centos6/"
 osci-mirrors/fuel_master_mirror_vc.sh
 echo "USE_STABLE_MOS_FOR_STAGING = ${USE_STABLE_MOS_FOR_STAGING}" >> ${WORKSPACE:-"."}/mirror_staging.txt
 echo FINISHED_TIME="$(date -u +'%Y-%m-%dT%H:%M:%S')" >> ci_status_params.txt
