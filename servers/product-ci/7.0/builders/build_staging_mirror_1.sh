5bd0152b980051f03a1e306be66ccf9fc16bc5c9
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-08-03 16:13:47 +0300

    Export MIRROR_MOS_UBUNTU_ROOT
    This change relates this fuel-main change
    https://review.openstack.org/#/c/206605
    
    1. Perestroika repository structure is going to
    be like this (see below). We need to download
    /mos-repos/ubuntu/7.0.target.txt to get the
    latest snapshot:
      ${LATEST_UBUNTU_SNAPSHOT}=snapshots/7.0-2015-08-06-084056
     Then use it to define snapshot root
    /mos-repos/ubuntu/${LATEST_UBUNTU_SNAPSHOT}
    
    └── ubuntu
       └── dists
           ├── mos7.0
               mos7.0-holdback
               mos7.0-proposed
               mos7.0-security
               mos7.0-updates
    
    2. Completly remove parameter MIRROR_BASE from all 7.0.* jobs
    since it's deprecated and we need to use MIRROR_CENTOS instead.
    
    Change-Id: I3bba81528d784a1ade567584bf7d3627d98d6def
    Related-Bug: #1479761

--- servers/product-ci/7.0/builders/build_staging_mirror_1.sh
+++ servers/product-ci/7.0/builders/build_staging_mirror_1.sh
@@ -4,11 +4,12 @@ set -ex
 
 echo STARTED_TIME="$(date -u +'%Y-%m-%dT%H:%M:%S')" > ci_status_params.txt
 # get stable ubuntu mirror
-LATEST_MIRROR_ID=$(curl -s http://perestroika-repo-tst.infra.mirantis.net/osci/mos/7.0.target.txt | head -1)
-# re-define MIRROR_UBUNTU_ROOT value from fuel-main make system
-export MIRROR_UBUNTU_ROOT="/osci/mos/${LATEST_MIRROR_ID}/cluster/base/trusty/"
+LATEST_UBUNTU_SNAPSHOT=$(curl -s http://perestroika-repo-tst.infra.mirantis.net/mos-repos/ubuntu/7.0.target.txt | head -1)
+# re-define MIRROR_MOS_UBUNTU_ROOT value from fuel-main make system
+export MIRROR_MOS_UBUNTU_ROOT="/mos-repos/ubuntu/${LATEST_UBUNTU_SNAPSHOT}"
 # re-define MIRROR_FUEL value from fuel-main make system
-export MIRROR_FUEL="http://perestroika-repo-tst.infra.mirantis.net/osci/mos/${LATEST_MIRROR_ID}/fuel/base/centos6/"
+LATEST_CENTOS_SNAPSHOT=$(curl -s http://perestroika-repo-tst.infra.mirantis.net/mos-repos/centos/mos7.0-centos6-fuel/os.target.txt | head -1)
+export MIRROR_FUEL="http://perestroika-repo-tst.infra.mirantis.net/mos-repos/centos/mos7.0-centos6-fuel/${LATEST_CENTOS_SNAPSHOT}/x86_64"
 osci-mirrors/fuel_master_mirror_vc.sh
 echo "USE_STABLE_MOS_FOR_STAGING = ${USE_STABLE_MOS_FOR_STAGING}" >> ${WORKSPACE:-"."}/mirror_staging.txt
 echo FINISHED_TIME="$(date -u +'%Y-%m-%dT%H:%M:%S')" >> ci_status_params.txt
--- servers/product-ci/7.0/builders/custom.sh
+++ servers/product-ci/7.0/builders/custom.sh
@@ -77,13 +77,11 @@ if [ "${USE_MIRROR}" != "none" ]; then
           LATEST_MIRROR_ID_URL=http://osci-mirror-msk.msk.mirantis.net
   esac
 
-  LATEST_MIRROR_ID=$(curl -s "${LATEST_MIRROR_ID_URL}/mos-repos/7.0.target.txt" | head -1)
-  export MIRROR_UBUNTU_ROOT="/mos-repos/${LATEST_MIRROR_ID}/cluster/base/trusty"
-  LATEST_SUITE=$(curl -s "${LATEST_MIRROR_ID_URL}/mos-repos/ubuntu/dists/mos7.0.target.txt" | head -1)
-  export MIRROR_MOS_UBUNTU_SUITE="${LATEST_SUITE}"
+  LATEST_TARGET=$(curl -s "${LATEST_MIRROR_ID_URL}/mos-repos/ubuntu/7.0.target.txt" | head -1)
+  export MIRROR_MOS_UBUNTU_ROOT="/mos-repos/ubuntu/${LATEST_TARGET}"
 fi
 
-echo "Using mirror: ${USE_MIRROR} with ${MIRROR_UBUNTU_ROOT}"
+echo "Using mirror: ${USE_MIRROR} with ${MIRROR_MOS_UBUNTU_ROOT}"
 
 #########################################
 
