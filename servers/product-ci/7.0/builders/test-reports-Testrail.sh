f4a755e897869a7e9b79703b803451c7d3ba57e7
ss-yuriy <ishamrai@mirantis.com>
2015-08-03 16:54:49 +0300

    Updated command-line options for Testrail script.
    Changes:
     - Added '7.0.swarm.statistic' job.
    
    Change-Id: Ia1f2f3492cd9ad2be8cbbdd1c6822d11cfb206b4
    Related-Bug: #1474791
    Related-Bug: #1476239

--- servers/product-ci/7.0/builders/test-reports-Testrail.sh
+++ servers/product-ci/7.0/builders/test-reports-Testrail.sh
@@ -18,4 +18,15 @@ export TestrailCMDPath="fuelweb_test/testrail/report.py"
 export TESTRAIL_TEST_SUITE='Swarm 7.0'
 export TESTRAIL_TEST_subSUITE='7.0.swarm.runner'
 
-python ${TestrailCMDPath} -v -l -j "${TESTRAIL_TEST_subSUITE}"
+if [ "${TEST_JOB_NAME_ENABLE}" == "true" ]; then
+ TEST_JOB_NAME=$(curl "${JENKINS_URL}job/7.0.swarm.Testrail/lastBuild/api/json?tree=actions\[causes\[upstreamProject\]\]&pretty=true" |grep upstreamProject |cut -d'"' -f4)
+fi
+
+if [ "${BUG_STATISTIC}" == "true" ]; then
+ RUNNER_BUILD_NUMBER=$(curl ${JENKINS_URL}view/7.0_swarm/job/7.0.swarm.runner/lastBuild/buildNumber)
+ OPTIONS="-s -N ${RUNNER_BUILD_NUMBER}"
+elif [ "${TEST_JOB_NAME}" != "" ]; then
+ OPTIONS="-o ${TEST_JOB_NAME}"
+fi
+
+python ${TestrailCMDPath} -v -l -j "${TESTRAIL_TEST_subSUITE}" ${OPTIONS}
