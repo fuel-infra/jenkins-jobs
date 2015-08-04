f105c80e8903ad4382a124170f23236a28534ce5
Vitaly Parakhin <vparakhin@mirantis.com>
2015-08-04 15:39:53 +0300

    Switch MOS repository URLs to Perestroika v4
    Repository format in Perestroika v4 has changed, so reflecting this
    for the test-requirements jobs on Fuel CI.
    
    Change-Id: I97567bfaf47f1fd1b18fa25e043d90c1c71b79cb
    Closes-Bug: #1474349

--- servers/fuel-ci/builders/test-requirements-deb.sh
+++ servers/fuel-ci/builders/test-requirements-deb.sh
@@ -6,11 +6,10 @@ DIST=`awk -F '[:=?]' '/^UBUNTU_RELEASE\>/ {print $NF}' config.mk`
 OVERALL_STATUS=0
 
 PRODUCT_VERSION=`awk -F '[:=?]' '/^PRODUCT_VERSION\>/ {print $NF}' config.mk`
-REPO_NAME=ubuntu-fuel-$PRODUCT_VERSION-stable
 PACKAGES=`git diff --word-diff=plain HEAD~ requirements-deb.txt | egrep '{+' | cut -d"+" -f2 | sed ':a;N;$!ba;s/\n/ /g'`
 
 if [ X"${PACKAGES}" = X"" ]; then
-	echo "MARK: no difference found, all requested packages exist in OBS and upstream repos."
+	echo "MARK: no difference found, all requested packages exist in Perestroika and upstream repos."
 	exit 0
 fi
 
@@ -23,7 +22,7 @@ if git diff --name-only HEAD~ requirements-deb.txt >/dev/null; then
         fi
 fi
 
-echo "MARK: checking OBS and upstream repos..."
+echo "MARK: checking Perestroika and upstream repos..."
 TEMPDIR=$(/bin/mktemp -d /tmp/test-requirements-deb-XXXXXXXX)
 echo "MARK: workspace directory is ${TEMPDIR}"
 
@@ -36,15 +35,12 @@ deb http://mirror.yandex.ru/ubuntu ${DIST}-updates main universe multiverse rest
 deb http://mirror.yandex.ru/ubuntu ${DIST}-security main universe multiverse restricted
 EOT
 else
-        chdist --data-dir ${TEMPDIR} create ${REPO_NAME} http://obs-1.mirantis.com/mos/ubuntu/ mos${PRODUCT_VERSION} main
+        chdist --data-dir ${TEMPDIR} create ${REPO_NAME} http://mirror.fuel-infra.org/mos-repos/ubuntu/ mos${PRODUCT_VERSION} main restricted
 
         cat > ${TEMPDIR}/${REPO_NAME}/etc/apt/sources.list <<EOT
-# FIXME(vparakhin): fix Perestroika mirror as soon as repo format for 7.0+ is defined
-deb http://mirror.fuel-infra.org/mos-repos/7.0/cluster/base/trusty/ trusty main
-
-#deb [arch=amd64] http://obs-1.mirantis.com/mos/ubuntu/ mos${PRODUCT_VERSION} main
-#deb [arch=amd64] http://obs-1.mirantis.com/mos/ubuntu/ mos${PRODUCT_VERSION}-updates main
-#deb [arch=amd64] http://obs-1.mirantis.com/mos/ubuntu/ mos${PRODUCT_VERSION}-security main
+deb http://mirror.fuel-infra.org/mos-repos/ubuntu/ mos${PRODUCT_VERSION} main restricted
+deb http://mirror.fuel-infra.org/mos-repos/ubuntu/ mos${PRODUCT_VERSION}-updates main restricted
+deb http://mirror.fuel-infra.org/mos-repos/ubuntu/ mos${PRODUCT_VERSION}-security main restricted
 deb http://mirror.fuel-infra.org/pkgs/ubuntu/ ${DIST} main universe multiverse restricted
 deb http://mirror.fuel-infra.org/pkgs/ubuntu/ ${DIST}-security main universe multiverse restricted
 deb http://mirror.fuel-infra.org/pkgs/ubuntu/ ${DIST}-updates main universe multiverse restricted
@@ -57,9 +53,9 @@ chdist --data-dir ${TEMPDIR} apt-get ${REPO_NAME} update
 chdist --data-dir ${TEMPDIR} apt-get ${REPO_NAME} install --dry-run ${PACKAGES}
 RES=${?}
 if [ ${RES} -eq 0 ]; then
-	echo "MARK: SUCCESS. The following packages are available in upstream repos: ${PACKAGES}"
+	echo "MARK: SUCCESS. The following packages are available in Perestroika or upstream repos: ${PACKAGES}"
 else
-	echo "MARK: FAILURE. Requested packages were not found in both OBS and upstream repos"
+	echo "MARK: FAILURE. Requested packages were not found in both Perestroika and upstream repos"
 	OVERALL_STATUS=1
 fi
 
--- servers/fuel-ci/builders/test-requirements-rpm.sh
+++ servers/fuel-ci/builders/test-requirements-rpm.sh
@@ -4,11 +4,13 @@ set -xe
 
 OVERALL_STATUS=0
 
-REPO_NAME=centos-fuel-$(awk -F '[:=?]' '/^PRODUCT_VERSION\>/ {print $NF}' config.mk)-stable
+PRODUCT_VERSION=`awk -F '[:=?]' '/^PRODUCT_VERSION\>/ {print $NF}' config.mk`
+CENTOS_MAJOR=`awk -F '[:=?]' '/^CENTOS_MAJOR\>/ {print $NF}' config.mk`
+CENTOS_ARCH=`awk -F '[:=?]' '/^CENTOS_ARCH\>/ {print $NF}' config.mk`
 PACKAGES=$(git diff --word-diff=plain HEAD~ requirements-rpm.txt | egrep '{+' | egrep -v "@Base|@Core" | cut -d"+" -f2 | sed ':a;N;$!ba;s/\n/ /g')
 
 if [ X"${PACKAGES}" = X"" ]; then
-	echo "MARK: no difference found, all requested packages exist in OBS and upstream repos."
+	echo "MARK: no difference found, all requested packages exist in Perestroika and upstream repos."
 	exit 0
 fi
 
@@ -17,21 +19,25 @@ if git diff --name-only HEAD~ requirements-rpm.txt >/dev/null; then
         ( sort -u requirements-rpm.txt | diff requirements-rpm.txt - ) || ( echo "MARK: FAILURE. requirements-rpm.txt is not sorted. Please sort it with sort -u"; exit 1 )
 fi
 
-echo "MARK: checking OBS and upstream repos..."
+echo "MARK: checking Perestroika and upstream repos..."
 rm -rf /var/tmp/yum-"${USER}"*/*
 
-# FIXME(vparakhin): fix Perestroika mirror as soon as repo format for 7.0+ is defined
+RES=$(repoquery --repofrompath=upstream-base,http://mirror.centos.org/centos/${CENTOS_MAJOR}/os/${CENTOS_ARCH}/ \
+--repofrompath=upstream-updates,http://mirror.centos.org/centos/${CENTOS_MAJOR}/updates/${CENTOS_ARCH}/ \
+--repofrompath==perestroika,http://mirror.fuel-infra.org/mos-repos/centos/mos${PRODUCT_VERSION}-centos${CENTOS_MAJOR}-fuel/os/${CENTOS_ARCH}/ \
+--repofrompath==perestroika-security,http://mirror.fuel-infra.org/mos-repos/centos/mos${PRODUCT_VERSION}-centos${CENTOS_MAJOR}-fuel/security/${CENTOS_ARCH}/ \
+--repofrompath==perestroika-updates,http://mirror.fuel-infra.org/mos-repos/centos/mos${PRODUCT_VERSION}-centos${CENTOS_MAJOR}-fuel/updates/${CENTOS_ARCH}/ \
+--whatprovides --nvr -q ${PACKAGES})
 
-RES=$(repoquery --repofrompath=upstream,http://mirror.centos.org/centos/6/os/x86_64/ --repofrompath==perestroika,http://mirror.fuel-infra.org/mos-repos/7.0/fuel/base/centos6/ --whatprovides --nvr -q ${PACKAGES})
 if [ X"${RES}" = X"" ]; then
-	echo "MARK: FAILURE. Requested packages were not found in both OBS and upstream repos."
+	echo "MARK: FAILURE. Requested packages were not found in both Perestroika and upstream repos."
 	OVERALL_STATUS=1
 else
 	for PACKAGE in ${PACKAGES}; do
 		if echo "${RES}" | grep -q "${PACKAGE}"; then
-			echo "MARK: ${PACKAGE} found in upstream repo»"
+			echo "MARK: ${PACKAGE} found in Perestroika or upstream repos"
 		else
-			echo "MARK: FAILURE: ${PACKAGE} not found in upstream repos"
+			echo "MARK: FAILURE: ${PACKAGE} not found in both Perestroika and upstream repos"
 			OVERALL_STATUS=1
 		fi
 	done
