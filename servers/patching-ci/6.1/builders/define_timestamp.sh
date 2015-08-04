a95153510bde2303c7e5b60760f34c8c0494c31d
Max Rasskazov <mrasskazov@mirantis.com>
2015-08-04 12:52:45 +0300

    Added create_snapshot jobs
    - define <timestamp> which used everywhere later
    - create snapshot w/o 2nd level artifacts: <distro>-noartifacts-<timestamp>
    - if DISTRO==centos-6
        - trigger downstream job which builds second-level artifacts
        - place artifacts into snapshot <distro>-<timestamp> based on
          snapshot created on step 1 and 2nd level artifacts
        - rebuild proposed repo (update repo metadata)
    - sync updated snapshot to all of the LOCATIONS as new one
    
    Change-Id: I7c1ee1dc6cc42704e1c1d1e48969072b4bcec802

---
+++ servers/patching-ci/6.1/builders/define_timestamp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+WORKSPACE="${WORKSPACE:-.}"
+TIMESTAMP_ARTIFACT="${WORKSPACE}/timestamp.txt"
+rm -f "$TIMESTAMP_ARTIFACT"
+
+
+TIMESTAMP_REGEXP='[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}'
+
+export SAVE_LATEST_DAYS=${SAVE_LATEST_DAYS:-61}
+export WARN_DATE=$(date --utc "+%Y%m%d" -d "$SAVE_LATEST_DAYS days ago")
+
+if [ -z "${TIMESTAMP}" ]; then
+    export TIMESTAMP=$(date --utc "+%Y-%m-%d-%H%M%S")
+else
+    # check that TIMESTAMP variable matches regexp
+    echo "${TIMESTAMP}" | grep -E "^${TIMESTAMP_REGEXP}$"
+fi
+
+echo "${TIMESTAMP}" > "${TIMESTAMP_ARTIFACT}"
