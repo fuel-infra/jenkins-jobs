5e1cf235fbb8b117c631724178efe49ec17994fd
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-08-06 10:40:07 +0300

    [7.0][osci] Switch build CI to new debian repo schema
    Change-Id: I859001e813414dc92b472451a8a5e7963b567256

--- servers/openstack-ci/7.0/builders/mos.publisher.sh
+++ servers/openstack-ci/7.0/builders/mos.publisher.sh
@@ -4,8 +4,8 @@ set -o xtrace
 set -o errexit
 
 case "${REPO_TYPE}" in
-        rpm) bash -ex publisher.v4/publish-rpm-binaries.sh ;;
-        deb) bash -ex publisher.v4/publish-deb-binaries.sh ;;
+        rpm) bash -ex publisher.v5/publish-rpm-binaries.sh ;;
+        deb) bash -ex publisher.v5/publish-deb-binaries.sh ;;
          * ) echo "Unsupported repository type" ; exit 1 ;;
 esac
 
