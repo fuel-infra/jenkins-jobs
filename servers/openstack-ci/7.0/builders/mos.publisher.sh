eb1272e2f0a5a1ac033d4028877d18930086f27e
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-08-12 13:00:26 +0300

    [7.0] Remove old repository schema from all jobs
    Change-Id: Iadaeddffa7aeb5d9c7afdb1e9080e64d59a75a11

--- servers/openstack-ci/7.0/builders/mos.publisher.sh
+++ servers/openstack-ci/7.0/builders/mos.publisher.sh
@@ -8,14 +8,3 @@ case "${REPO_TYPE}" in
         deb) bash -ex publisher.v5/publish-deb-binaries.sh ;;
          * ) echo "Unsupported repository type" ; exit 1 ;;
 esac
-
-export REPO_PATH_PREFIX=osci/
-export REPO_REQUEST_PATH_PREFIX=osci/review/
-export BASE_PATH=base/
-export UPDATES_PATH=updates/
-export SECURITY_PATH=security/
-
-case "${REPO_TYPE}" in
-    rpm) bash publisher/publish-rpm-binaries.sh ;;
-      *) bash publisher/publish-deb-binaries.sh ;;
-esac
