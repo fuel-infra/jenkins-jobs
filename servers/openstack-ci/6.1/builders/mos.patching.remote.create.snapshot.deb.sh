f8ce506e8ba660d24ba49677361da9be1a733b2c
Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
2015-08-17 14:33:03 +0300

    [6.1] [Patching] Add run remote jobs for create snapshot
    Change-Id: Ib910babd90520bd68ba89cbaf6a33b1320330b70

---
+++ servers/openstack-ci/6.1/builders/mos.patching.remote.create.snapshot.deb.sh
@@ -0,0 +1,54 @@
+function retry {
+    local count=3
+    local sleep=5
+    local optname
+    while getopts 'c:s:' optname
+    do
+        case $optname in
+            c) count=$OPTARG ;;
+            s) sleep=$OPTARG ;;
+            ?) return 1 ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+    local ec
+    while true
+    do
+       "$@" && true
+       ec=$?
+       (( count-- ))
+       if [[ $ec -eq 0 || $count -eq 0 ]]
+       then
+           break
+       else
+           sleep "$sleep"
+       fi
+    done
+    return "$ec"
+}
+
+REMOTE_URL="https://patching-ci.infra.mirantis.net"
+REMOTE_JOB="6.1.create_snapshot.ubuntu"
+JOBURL=${REMOTE_URL}/view/All/job/${REMOTE_JOB}
+RESULT=None
+
+retry curl -X POST ${JOBURL}/buildWithParameters --user "${PATCHING_USER}":"${PATCHING_PASSWORD}"
+sleep 10
+BUILDNUMBER=$(curl --silent ${JOBURL}/lastBuild/api/json | python -c \
+'import sys
+from yaml import load, Loader
+obj = load(sys.stdin, Loader=Loader)
+print obj["number"]')
+
+#TODO: Implement timeout (about 10 min) for this loop
+while [ "${RESULT}" = None ]; do
+    sleep 120
+    RESULT=$(curl --silent ${JOBURL}/"${BUILDNUMBER}"/api/json | python -c \
+'import sys
+from yaml import load, Loader
+obj = load(sys.stdin, Loader=Loader)
+print obj["result"]')
+done
+if [ "${RESULT}" = FAILURE ]; then
+    exit 1
+fi
