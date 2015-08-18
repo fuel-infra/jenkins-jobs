599a494866ee62aeadeddf8d57912760fa7e5572
Alexander Evseev <aevseev@mirantis.com>
2015-08-18 14:26:30 +0300

    Set more common job parameters by Zuul
    Zuul should set as much as possible job parameters to not use complex shell
    expressions in job scripts
    
    Change-Id: Ide975c0db6474c6a78f78e7e5db711f5f5bdd0e2

--- zuul/external_functions.py
+++ zuul/external_functions.py
@@ -15,11 +15,12 @@
 import re
 
 
-re_openstack = r'^openstack(-build)?/'
+re_proj_openstack = r'^openstack(?P<build>-build)?/(?P<name>.+)'
+re_branch_mos_version = r'^openstack-ci/fuel-(?P<version>[0-9.]+)(?P<update>-updates)?/'
 
 
-def pkg_build(item, job, params):
-    # Set Gerrit parameters
+def params_gerrit(item, job, params):
+    # Gerrit parameters
     params['GERRIT_PROJECT'] = params['ZUUL_PROJECT']
     params['GERRIT_BRANCH'] = params['ZUUL_BRANCH']
     params['GERRIT_CHANGE_NUMBER'] = params['ZUUL_CHANGE']
@@ -35,9 +36,32 @@ def pkg_build(item, job, params):
         params['GERRIT_CHANGE_OWNER_NAME'] = item.change.owner['name']
         params['GERRIT_CHANGE_OWNER_EMAIL'] = item.change.owner['email']
 
-    # Set specific parameters
-    if re.match(re_openstack, params['ZUUL_PROJECT']):
+
+def params_mos(item, job, params):
+    # MOS-specific parameters
+    mos_proj = re.match(re_proj_openstack, params['ZUUL_PROJECT'])
+    if mos_proj:
         params['IS_OPENSTACK'] = 'true'
+        params['MOS_PROJECT'] = re.sub(r'-build(/|$)', r'\1', mos_proj.group('name'))
+    else:
+        params['IS_OPENSTACK'] = 'false'
+
+    mos_ver = re.match(re_branch_mos_version, params['ZUUL_BRANCH'])
+    if mos_ver:
+        params['MOS_VERSION'] = str(mos_ver.group('version'))
+        if mos_ver.group('update'):
+            params['IS_UPDATES'] = 'true'
+        else:
+            params['IS_UPDATES'] = 'false'
+
+
+def pkg_build(item, job, params):
+    # Common params
+    params_gerrit(item, job, params)
+    params_mos(item, job, params)
+
+    # Package jobs specific parameters
+    if params['IS_OPENSTACK'] == 'true':
         params['SRC_PROJECT'] = re.sub(r'-build(/|$)', r'\1', params['ZUUL_PROJECT'])
         params['SPEC_PROJECT'] = re.sub(r'(/|$)', r'-build\1', params['ZUUL_PROJECT'])
         params['SPEC_BRANCH'] = params['ZUUL_BRANCH']
@@ -48,7 +72,6 @@ def pkg_build(item, job, params):
         params['COMPONENT_PATH'] = 'cluster/'
     else:
         params['SRC_PROJECT'] = params['ZUUL_PROJECT']
-        params['IS_OPENSTACK'] = 'false'
         params['COMPONENT_PATH'] = 'fuel/'
 
     if params['SRC_PROJECT'] == params['ZUUL_PROJECT']:
