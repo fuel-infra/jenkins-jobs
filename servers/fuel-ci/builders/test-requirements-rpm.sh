b5bcb64eef6e3d47ebf2078da07ad66e4719e207
Vitaly Parakhin <vparakhin@mirantis.com>
2015-08-21 17:12:30 +0300

    Switch from repoquery to fuel-main routine
    repoquery produces false positives on hard-coded versions in
    requirements-rpm.txt, so let's replace it with generic fuel-main
    "show-yum-urls-centos" routine
    
    Change-Id: I00526d46ae96af126e9da1e852d3a1f7342c529a

--- servers/fuel-ci/builders/test-requirements-rpm.sh
+++ servers/fuel-ci/builders/test-requirements-rpm.sh
@@ -2,14 +2,15 @@
 
 set -xe
 
-OVERALL_STATUS=0
-
 PRODUCT_VERSION=`awk -F '[:=?]' '/^PRODUCT_VERSION\>/ {print $NF}' config.mk`
 CENTOS_MAJOR=`awk -F '[:=?]' '/^CENTOS_MAJOR\>/ {print $NF}' config.mk`
 CENTOS_MINOR=`awk -F '[:=?]' '/^CENTOS_MINOR\>/ {print $NF}' config.mk`
 CENTOS_RELEASE=${CENTOS_MAJOR}.${CENTOS_MINOR}
 CENTOS_ARCH=`awk -F '[:=?]' '/^CENTOS_ARCH\>/ {print $NF}' config.mk`
 PACKAGES=$(git diff --word-diff=plain HEAD~ requirements-rpm.txt | egrep '{+' | egrep -v "@Base|@Core" | cut -d"+" -f2 | sed ':a;N;$!ba;s/\n/ /g')
+
+export MIRROR_CENTOS=http://vault.centos.org/${CENTOS_RELEASE}/
+export MIRROR_FUEL=http://mirror.fuel-infra.org/mos-repos/centos/mos${PRODUCT_VERSION}-centos${CENTOS_MAJOR}-fuel/os/${CENTOS_ARCH}/
 
 if [ X"${PACKAGES}" = X"" ]; then
 	echo "MARK: no difference found, all requested packages exist in Perestroika and upstream repos."
@@ -22,27 +23,5 @@ if git diff --name-only HEAD~ requirements-rpm.txt >/dev/null; then
 fi
 
 echo "MARK: checking Perestroika and upstream repos..."
-rm -rf /var/tmp/yum-"${USER}"*/*
 
-RES=$(repoquery --repofrompath=upstream-base,http://vault.centos.org/${CENTOS_RELEASE}/os/${CENTOS_ARCH}/ \
---repofrompath=upstream-updates,http://vault.centos.org/${CENTOS_RELEASE}/updates/${CENTOS_ARCH}/ \
---repofrompath==perestroika,http://mirror.fuel-infra.org/mos-repos/centos/mos${PRODUCT_VERSION}-centos${CENTOS_MAJOR}-fuel/os/${CENTOS_ARCH}/ \
---repofrompath==perestroika-security,http://mirror.fuel-infra.org/mos-repos/centos/mos${PRODUCT_VERSION}-centos${CENTOS_MAJOR}-fuel/security/${CENTOS_ARCH}/ \
---repofrompath==perestroika-updates,http://mirror.fuel-infra.org/mos-repos/centos/mos${PRODUCT_VERSION}-centos${CENTOS_MAJOR}-fuel/updates/${CENTOS_ARCH}/ \
---whatprovides --nvr -q ${PACKAGES})
-
-if [ X"${RES}" = X"" ]; then
-	echo "MARK: FAILURE. Requested packages were not found in both Perestroika and upstream repos."
-	OVERALL_STATUS=1
-else
-	for PACKAGE in ${PACKAGES}; do
-		if echo "${RES}" | grep -q "${PACKAGE}"; then
-			echo "MARK: ${PACKAGE} found in Perestroika or upstream repos"
-		else
-			echo "MARK: FAILURE: ${PACKAGE} not found in both Perestroika and upstream repos"
-			OVERALL_STATUS=1
-		fi
-	done
-fi
-
-exit ${OVERALL_STATUS}
+make show-yum-urls-centos USE_MIRROR=none
