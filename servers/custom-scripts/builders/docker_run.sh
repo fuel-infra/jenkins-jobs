5d0fa26d95d2eaad092af36361f4d8cf62243f42
Andrey Nikitin <anikitin@mirantis.com>
2015-08-24 14:42:42 +0300

    Updated the script to run any custom script in Docker
      * command 'apt-get update' was added to have a fresh list of
        packages inside Docker container.
    
      * '--rm': this option is needed to remove Docker container
        after running of script. Currently all existing containers
        are stored on Jenkins slave.
    
    Change-Id: I252558503f9427ebc5d5bdc0d827458cf7cf026b

--- servers/custom-scripts/builders/docker_run.sh
+++ servers/custom-scripts/builders/docker_run.sh
@@ -7,12 +7,12 @@ JOB_DOCKER_PATH="/opt/${JOB_NAME}"
 
 COMMAND='true'
 if [[ -f "${JOB_HOST_PATH}/requirements-deb.txt" ]] ; then
-  COMMAND="${COMMAND} ; apt-get install -y $(xargs < "${JOB_HOST_PATH}/requirements-deb.txt")"
+  COMMAND="${COMMAND} && apt-get update && apt-get install -y $(xargs < "${JOB_HOST_PATH}/requirements-deb.txt")"
 fi
 
 if [[ -f "${JOB_HOST_PATH}/requirements-pip.txt" ]] ; then
-  COMMAND="${COMMAND} ; pip install -r ${JOB_DOCKER_PATH}/requirements-pip.txt"
+  COMMAND="${COMMAND} && pip install -r ${JOB_DOCKER_PATH}/requirements-pip.txt"
 fi
 
-docker run -v "${JOB_HOST_PATH}:${JOB_DOCKER_PATH}" ${VOLUMES} \
+docker run --rm -v "${JOB_HOST_PATH}:${JOB_DOCKER_PATH}" ${VOLUMES} \
            -t "${DOCKER_IMAGE}" /bin/bash -exc "${COMMAND} ; /opt/${SCRIPT_PATH} ${MODE}"
