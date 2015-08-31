7189e52436462b3c32ff8b72f52f742bd55b8948
Alexey Elagin <aelagin@mirantis.com>
2015-08-31 18:00:03 +0300

    change LMA plugin job for tox use
    Change-Id: I3531eaa8278ccccc5268c7e5ace1ebe515a40665
    Closes-Bug: #1489938

---
+++ servers/plugins-ci/builders/build-zabbix-plugin.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -ex
+
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:$PATH
+
+VIRTUAL_ENV=/home/jenkins/venv-nailgun-tests-fpb3
+if [ -f $VIRTUAL_ENV/bin/activate ]; then
+  source $VIRTUAL_ENV/bin/activate
+  echo "Python virtual env exist"
+  git clone git://github.com/stackforge/fuel-plugins.git
+  pushd .
+  cd fuel-plugins
+  cd fuel_plugin_builder
+  pip install .
+  popd
+else
+  rm -rf $VIRTUAL_ENV
+  virtualenv --system-site-packages  $VIRTUAL_ENV
+  source $VIRTUAL_ENV/bin/activate
+  git clone git://github.com/stackforge/fuel-plugins.git
+  pushd .
+  cd fuel-plugins
+  cd fuel_plugin_builder
+  pip install . 
+  popd
+fi
+
+
+find deployment_scripts/ -name '*.erb' -print0 | xargs -0 -P1 -L1 -I '%' erb -P -x -T '-' % | ruby -c
+find deployment_scripts/ -name '*.pp' -print0 | xargs -0 -P1 -L1 puppet parser validate --verbose
+find deployment_scripts/ -name '*.pp' -print0 | xargs -0 -r -P1 -L1 puppet-lint \
+          --fail-on-warnings \
+          --with-context \
+          --with-filename \
+          --no-80chars-check \
+          --no-variable_scope-check \
+          --no-nested_classes_or_defines-check \
+          --no-autoloader_layout-check \
+          --no-class_inherits_from_params_class-check \
+          --no-documentation-check \
+          --no-arrow_alignment-check
+
+if [ -f deployment_tasks.yaml ]; then
+  echo "deployment_tasks.yaml exist"
+else
+  echo "creating deployment_tasks.yaml IT's dog-nail!!!"
+  echo "[]" > deployment_tasks.yaml
+fi
+
+fpb --check  ./
+fpb --build  ./
+
+deactivate
