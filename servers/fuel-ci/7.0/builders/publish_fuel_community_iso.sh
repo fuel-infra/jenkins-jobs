9bd521c2d8676b9bb3ce0cef22a08a6eee230889
Igor Belikov <ibelikov@mirantis.com>
2015-09-10 16:14:45 +0300

    [fuel-ci] versioned bvt jobs
    * Add separate publish_fuel_community_iso jobs for 7.0/8.0
    * Add separate bvt jobs for 7.0/8.0
    * Trigger only ubuntu bvt for 7.0
    * Change centos bvt group to bvt_2 for 8.0
    
    Closes-Bug: #1477324
    Change-Id: Ic538e890c902ae86eba186818b40e27de0ec4eca

---
+++ servers/fuel-ci/7.0/builders/publish_fuel_community_iso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+ISO_NAME=fuel-community-${ISO_ID}
+TARBALL_NAME=fuel-community-${UPGRADE_ID}
+
+seedclient.py -pvf "${ARTIFACTS_DIR}/${ISO_NAME}.iso" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+
+seedclient.py -pvf "${ARTIFACTS_DIR}/${TARBALL_NAME}.tar.lrz" --tracker-url=http://retracker.local:80/announce,http://seed-us1.fuel-infra.org:8080/announce,http://seed-cz1.fuel-infra.org:8080/announce --http-root=http://seed-cz1.fuel-infra.org/fuelweb-iso,http://seed-us1.fuel-infra.org/fuelweb-iso --seed-host=seed-us1.fuel-infra.org:17333,seed-cz1.fuel-infra.org:17333
+
+echo "META: ${ISO_ID}: <a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.iso.torrent?from=jenkins>ISO</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${TARBALL_NAME}.tar.lrz.torrent?from=jenkins>UPGD</a>"
+echo "DESCRIPTION=<a href=http://seed.fuel-infra.org/fuelweb-iso/${ISO_NAME}.iso.torrent?from=status>ISO</a> <a href=http://seed.fuel-infra.org/fuelweb-iso/${TARBALL_NAME}.tar.lrz.torrent?from=status>UPGD</a>" > description.txt
