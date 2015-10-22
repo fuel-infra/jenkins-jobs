b46369932912832ef0bf116fe9b12fb8bbcdd65f
Artem Panchenko <apanchenko@mirantis.com>
2015-10-22 19:30:22 +0300

    Explicitly pass test job name to TestRail reporter
    Closes-bug: #1508924
    Change-Id: I358715a283e9c9eebbb3929b5915812ff265dd3f

--- servers/new-product-ci/7.0/builders/test-reports-Testrail.sh
+++ servers/new-product-ci/7.0/builders/test-reports-Testrail.sh
@@ -12,14 +12,10 @@ export SWARM_RUNNER_JOB_NAME='7.0.swarm.runner'
 # Prepare venv
 source /home/jenkins/venv-nailgun-tests-2.9/bin/activate
 
-if [ "${TEST_JOB_NAME_ENABLE}" == "true" ]; then
- TEST_JOB_NAME=$(curl "${JENKINS_URL}/job/${JOB_NAME}/${BUILD_NUMBER}/api/json?tree=actions\[causes\[upstreamProject\]\]&pretty=true" |grep upstreamProject |cut -d'"' -f4)
-fi
-
 if [ "${BUG_STATISTIC}" == "true" ]; then
  RUNNER_BUILD_NUMBER=$(curl "${JENKINS_URL}/job/${SWARM_RUNNER_JOB_NAME}/lastBuild/buildNumber")
  OPTIONS="-s -N ${RUNNER_BUILD_NUMBER}"
-elif [ "${TEST_JOB_NAME}" != "" ]; then
+elif [ "${TEST_JOB_NAME}" != "none" ]; then
  OPTIONS="-o ${TEST_JOB_NAME}"
 fi
 
