146d08fd31ae76a14c6fdbb5d28434327645e965
Igor Belikov <ibelikov@mirantis.com>
2015-11-03 19:38:05 +0300

    Fix 7.0 swarm upgrade tests templates
    Use separate templates and scripts with the ability to use extra
    DEB and RPM repos for 7.0 swarm upgrade tests.
    
    Related-Bug: #1513405
    
    Change-Id: Id9f2e1eca936caae43eb67d8cf0cb12297d5a70a

--- common/7.0/scripts/run_upgrade_system_test.sh
+++ common/7.0/scripts/run_upgrade_system_test.sh
@@ -37,27 +37,29 @@ esac
 
 # Set proper Openstack Release
 if [[ ${OPENSTACK_RELEASE} == 'centos' ]]; then
-	export OPENSTACK_RELEASE=CentOS
+	export OPENSTACK_RELEASE="CentOS"
 elif [[ ${OPENSTACK_RELEASE} == 'ubuntu' ]]; then
-	export OPENSTACK_RELEASE=Ubuntu
+	export OPENSTACK_RELEASE="Ubuntu"
 fi
 
-export CONNECTION_STRING='qemu+tcp://127.0.0.1:16509/system'
-export ENV_NAME=${ENV_PREFIX}.${BUILD_NUMBER}
-export ENV_NAME=${ENV_NAME:0:68}
-export FUEL_STATS_ENABLED=false
+export VENV_PATH="/home/jenkins/venv-nailgun-tests-2.9"
+export CONNECTION_STRING="qemu+tcp://127.0.0.1:16509/system"
+export TIMESTAMP=$(date +%y%m%d%H%M)
+export ENV_NAME="${ENV_PREFIX}.${BUILD_NUMBER}.${TIMESTAMP}"
+export ENV_NAME="${ENV_NAME:0:68}"
+export FUEL_STATS_ENABLED="false"
 
 rm -rf logs/*
 
 ISO_PATH=$(seedclient-wrapper -d -m "${BASE_ISO_MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
 
-export MAKE_SNAPSHOT=true
+export MAKE_SNAPSHOT="true"
 
 sh -x "BASE/utils/jenkins/system_tests.sh" -t test -w "${WORKSPACE}/BASE" -e "${ENV_NAME}" -o --group="${TEST_GROUP}" -i "${ISO_PATH}"
 
 echo "Description string: ${TEST_GROUP} on ${VERSION_STRING}"
 
-export MAKE_SNAPSHOT=false
+export MAKE_SNAPSHOT="false"
 
 export TARBALL_PATH=$(seedclient-wrapper -d -m "${UPGRADE_TARBALL_MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
 
@@ -67,11 +69,7 @@ echo "Description string: ${VERSION_STRING}"
 export UPGRADE_FUEL_FROM=$(basename "${ISO_PATH}" | cut -d '-' -f 2 | sed s/.iso//g)
 export UPGRADE_FUEL_TO=$(basename "${TARBALL_PATH}" | cut -d '-' -f 2)
 
-export DEVOPS_DB_NAME='devops'
-export DEVOPS_DB_USER='devops'
-export DEVOPS_DB_PASSWORD='devops'
-
-export VENV_PATH=/home/jenkins/venv-nailgun-tests-2.9
+export VENV_PATH="/home/jenkins/venv-nailgun-tests-2.9"
 
 ###################### Get MIRROR_UBUNTU ###############
 
@@ -145,6 +143,7 @@ if [[ -n "${DEB_LATEST}" ]]; then
         EXTRA_DEB_REPOS+="${DEB_SECURITY}"
     fi
 fi
+
 
 # Use -k to reuse environment
 sh -x "UPGRADE/utils/jenkins/system_tests.sh" -k -t test -w "${WORKSPACE}/UPGRADE" -e "${ENV_NAME}" -o --group="${UPGRADE_TEST_GROUP}" -i "${ISO_PATH}"
