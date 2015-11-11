0aed9f92cb0fee178f3e2b8022528323cd996ec0
Dmitry Kaigarodtsev <dkaiharodsev@mirantis.com>
2015-11-11 12:01:39 +0200

    Added 'verify-slave-label' job and utills for it
    Change-Id: Ibc2c84593965f39e90c996b8b4f53e3aad264d06

---
+++ utils/label_compliance/generate_yaml.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# usage example: ./generate_yaml.py ../../output/patching-ci/
+# ../../output/patching-ci/ - path to generated xml configs
+
+import os
+import sys
+import yaml
+from lxml import etree
+
+label_template='label_template.yaml'
+label_file='labels.yaml'
+job_name_field='job_name'
+stream = open(label_template, 'r')
+data = yaml.load(stream)
+
+def show_labels(tox_output_dir):
+
+    tox_output_dir = os.path.abspath(tox_output_dir)
+    tox_output_files = os.listdir(tox_output_dir)
+
+    for filename in tox_output_files:
+
+        filepath = os.path.join(tox_output_dir, filename)
+        doc = etree.parse(filepath)
+        label = doc.find ('assignedNode')
+
+        if label is None:
+            data["NO_LABEL"][job_name_field]=filename
+        else:
+            data[label.text][job_name_field]=filename
+
+    with open(label_file, 'w') as yaml_file:
+        yaml_file.write(yaml.dump(data, default_flow_style=False))
+
+show_labels(sys.argv[1])
---
+++ utils/label_compliance/parse_yaml.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# usage example: ./parse_yaml.py docker-builder
+# docker-builder - name of the label
+
+import yaml
+import sys
+
+conf_file="labels.yaml"
+
+with open(conf_file, 'r') as config:
+
+    doc = yaml.load(config)
+    job = doc [sys.argv[1]] ["verify_tests"]
+
+    print "TRIGGERED_JOB_NAMES="+(job["name"])
+
+    if job.has_key('params'):
+
+        params=job["params"]
+
+        for param, param_data in params.items():
+            print '%s=%s' % (param, param_data)
