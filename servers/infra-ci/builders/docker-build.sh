665b9b612e6bf9ed450401613b8c1d31a8a7bf1b
Pawel Brzozowski <pbrzozowski@mirantis.com>
2015-11-23 15:14:49 +0100

    automated docker images building
    - added SCM triggers
    - passing variables via artifacts
    - detection of new/changed Dockerfiles
    - added copy/paste output for cloning images
    
    Change-Id: I5577802dd2ba933a3759c39a6857c0b60d422f9d

--- servers/infra-ci/builders/docker-build.sh
+++ servers/infra-ci/builders/docker-build.sh
@@ -2,16 +2,50 @@
 
 set -ex
 
-echo "Cleaning a previous image first:"
-docker rmi "${IMAGE_NAME}:${IMAGE_TAG}" 2>/dev/null || /bin/true
+OUTPUT="[urls]"
 
-echo "Clone repository with images:"
-DESTINATION=$(mktemp -d)
-git clone "${REPOSITORY}" "${DESTINATION}"
+# prepare prefix
+if [ "${GERRIT_EVENT_TYPE}" == 'change-merged' ]
+then
+  PREFIX='infra'
+else
+  PREFIX='jenkins'
+fi
 
-echo "Building a new image from the URL:"
-cd "${DESTINATION}/${IMAGE_NAME}/${IMAGE_TAG}"
-docker build -t "infra/${IMAGE_NAME}:${IMAGE_TAG}" .
+# prepare image list
+IMAGES=$(git diff --stat HEAD~1 \
+  | grep Dockerfile | cut -f 2 -d ' ' \
+  | sed 's/\/Dockerfile//g' \
+  | sed 's/\//:/g' \
+  | tr '\n' ' ')
 
-echo "Cleanup temporary files"
-rm -rf "${DESTINATION}"
+# build all images
+for IMAGE in ${IMAGES}
+do
+  IMAGE_NAME=$(echo "$IMAGE" | cut -f 1 -d :)
+  IMAGE_TAG=$(echo "$IMAGE" | cut -f 2 -d :)
+
+  echo "Cleaning a previous image first:"
+  docker rmi "${IMAGE_NAME}:${IMAGE_TAG}" 2>/dev/null || /bin/true
+
+  # check if directory still exists
+  if [ -f "${WORKSPACE}/${IMAGE_NAME}/${IMAGE_TAG}/Dockerfile" ]
+  then
+    # build image
+    echo "Building a new image from the URL:"
+    cd "${WORKSPACE}/${IMAGE_NAME}/${IMAGE_TAG}"
+    docker build -t "${PREFIX}/${IMAGE_NAME}:${IMAGE_TAG}" .
+    # add image to publish list
+    VIMAGES="${VIMAGES} ${IMAGE_NAME}:${IMAGE_TAG}"
+    OUTPUT+="${PREFIX}/${IMAGE_NAME}:${IMAGE_TAG}<br>"
+  fi
+done
+
+# remove trailing, leading spaces
+IMAGES=$(echo -e "${VIMAGES}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+# prepare artifacts for publisher
+echo "PREFIX='${PREFIX}'" > "${WORKSPACE}/to_publish.txt"
+echo "IMAGES='${IMAGES}'" >> "${WORKSPACE}/to_publish.txt"
+
+echo "${OUTPUT}"
