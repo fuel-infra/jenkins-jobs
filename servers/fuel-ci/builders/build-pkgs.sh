76c0422a8dba0de24eee26b0d118e09c3c92c954
Dmitry Kaigarodtsev <dkaiharodsev@mirantis.com>
2015-12-08 17:27:20 +0200

    Added support of rpm packages in 'build-pkgs.sh' script
    Closes-Bug: #1524242
    Change-Id: Iaf569b036a3a189399d8209fe5e89328f764c818

--- servers/fuel-ci/builders/build-pkgs.sh
+++ servers/fuel-ci/builders/build-pkgs.sh
@@ -7,6 +7,9 @@ if echo "${GERRIT_CHANGE_COMMIT_MESSAGE}" | grep -q -iE "Fuel-CI:\s+disable"; th
   exit -1
 fi
 
+## we MUST use external DNS
+echo "DNSPARAM=\"--dns 8.8.8.8\"" > "${WORKSPACE}"/fuel-mirror/perestroika/docker-builder/config
+
 # where we store sources, this dir will be mounted to docker env
 SOURCE_PATH="${WORKSPACE}/sources/"
 # where we store packages, that should be uploaded by test
@@ -23,11 +26,13 @@ DEB_RESULT_DIR="${WORKSPACE}/packages_deb"
 rm -rf "${RPM_RESULT_DIR}" "${DEB_RESULT_DIR}"
 mkdir -p "${RPM_RESULT_DIR}" "${DEB_RESULT_DIR}"
 
-
-## prepare fuel-library sources
+# prepare fuel-library sources
 pushd "${PROJECT_ROOT}" &>/dev/null
 
-PACKAGE_VERSION=$(dpkg-parsechangelog --show-field Version | cut -d '-' -f1)
+# taking version of package
+
+RPM_PACKAGE_VERSION=$(rpm -q --specfile "${PROJECT_ROOT}"/specs/"${PROJECT_PACKAGE}".spec --queryformat %{VERSION}"\n" | head -1 )
+
 if [ -z "${GERRIT_BRANCH}" ]; then
 # we start job from master branch (by timer)
   RELEASE="$(git -C "${PROJECT_ROOT}" rev-list --no-merges HEAD --count).1.git$(git -C "${PROJECT_ROOT}" rev-parse --short HEAD)"
@@ -40,22 +45,10 @@ export DEBEMAIL=$(git -C "${PROJECT_ROOT}" log -1 --pretty=format:%ae)
 DEBMSG=$(git -C "${PROJECT_ROOT}" log -1 --pretty=%s)
 
 # for rpm
-git archive --format=tar.gz --worktree-attributes HEAD --output="${SOURCE_PATH}/${PROJECT_PACKAGE}-${PACKAGE_VERSION}.tar.gz"
+git archive --format=tar.gz --worktree-attributes HEAD --output="${SOURCE_PATH}/${PROJECT_PACKAGE}-${RPM_PACKAGE_VERSION}.tar.gz"
 cp -v "${PROJECT_ROOT}/specs/${PROJECT_PACKAGE}.spec" "${SOURCE_PATH}"
 # update spec with proper version
 sed -i "s|Release:.*$|Release: ${RELEASE}|" "${SOURCE_PATH}/${PROJECT_PACKAGE}.spec"
-# for deb
-cp -v "${SOURCE_PATH}/${PROJECT_PACKAGE}-${PACKAGE_VERSION}.tar.gz" "${SOURCE_PATH}/${PROJECT_PACKAGE}_${PACKAGE_VERSION}.orig.tar.gz"
-mkdir -p "${SOURCE_PATH}/${PROJECT_PACKAGE}"
-cp -rv "${PROJECT_ROOT}/debian" "${SOURCE_PATH}/${PROJECT_PACKAGE}"
-dch -c "${SOURCE_PATH}/${PROJECT_PACKAGE}/debian/changelog" -D trusty -b --force-distribution -v "${PACKAGE_VERSION}-${RELEASE}" "${DEBMSG}"
-
-popd &>/dev/null
-
-
-## we MUST use external DNS
-echo "DNSPARAM=\"--dns 8.8.8.8\"" > "${WORKSPACE}"/fuel-mirror/perestroika/docker-builder/config
-
 ## build rpm
 "${WORKSPACE}"/fuel-mirror/perestroika/build-package.sh \
   --build-target centos7 \
@@ -63,13 +56,22 @@ echo "DNSPARAM=\"--dns 8.8.8.8\"" > "${WORKSPACE}"/fuel-mirror/perestroika/docke
   --source "${SOURCE_PATH}" \
   --output-dir "${RPM_RESULT_DIR}"
 
-## build deb
-"${WORKSPACE}"/fuel-mirror/perestroika/build-package.sh \
-  --build-target trusty \
-  --ext-repos "mos,http://mirror.seed-cz1.fuel-infra.org/mos-repos/ubuntu/${MOS} mos${MOS} main restricted" \
-  --source "${SOURCE_PATH}" \
-  --output-dir "${DEB_RESULT_DIR}"
+# for deb
+if [ -d "${PROJECT_ROOT}/debian" ]; then
+  DEB_PACKAGE_VERSION=$(dpkg-parsechangelog --show-field Version | cut -d '-' -f1)
+  cp -v "${SOURCE_PATH}/${PROJECT_PACKAGE}-${RPM_PACKAGE_VERSION}.tar.gz" "${SOURCE_PATH}/${PROJECT_PACKAGE}_${DEB_PACKAGE_VERSION}.orig.tar.gz"
+  mkdir -p "${SOURCE_PATH}/${PROJECT_PACKAGE}"
+  cp -rv "${PROJECT_ROOT}/debian" "${SOURCE_PATH}/${PROJECT_PACKAGE}"
+  dch -c "${SOURCE_PATH}/${PROJECT_PACKAGE}/debian/changelog" -D trusty -b --force-distribution -v "${DEB_PACKAGE_VERSION}-${RELEASE}" "${DEBMSG}"
+  ## build deb
+  "${WORKSPACE}"/fuel-mirror/perestroika/build-package.sh \
+    --build-target trusty \
+    --ext-repos "mos,http://mirror.seed-cz1.fuel-infra.org/mos-repos/ubuntu/${MOS} mos${MOS} main restricted" \
+    --source "${SOURCE_PATH}" \
+    --output-dir "${DEB_RESULT_DIR}"
+fi
 
+popd &>/dev/null
 
 # preparing artifacts
 echo "Copy packages to artifacts dir"
