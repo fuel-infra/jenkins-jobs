5f858394dd947c335bf920522f36fb68a9680128
Dmitry Kaigarodtsev <dkaiharodsev@mirantis.com>
2015-12-24 16:18:57 +0200

    Added cache cleanup for yum
    Change-Id: I09994faa25e9d8083a82a5a26e2718a4263b4bb8
    Closes-Bug:#1529073

--- servers/fuel-ci/9.0/builders/community.all.sh
+++ servers/fuel-ci/9.0/builders/community.all.sh
@@ -16,8 +16,10 @@ rm -rf "${ARTS_DIR}"
 #########################################
 
 test "$deep_clean" = "true" && make deep_clean
+rm -rf /var/tmp/yum-"${USER}"-*
 
 #########################################
+
 
 echo "Check version.yaml for Fuel commit ids (if present)"
 
