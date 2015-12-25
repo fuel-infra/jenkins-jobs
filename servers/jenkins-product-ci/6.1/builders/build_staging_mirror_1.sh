def50050b4b0b5cea1d869c1b6edd6927b54c195
Aleksandra Fedorova <afedorova@mirantis.com>
2015-12-25 22:23:19 +0300

    Remove 6.1 iso and swarm from jenkins-product
    6.1 staging and iso builds were moved to Old Stable CI.
    6.1 swarm moved to Product CI.
    
    We keep only custom tests and builds.
    
    Change-Id: I9105ce95d482b02ffed9f633d38853cfad3d6ccf

--- servers/jenkins-product-ci/6.1/builders/build_staging_mirror_1.sh
+++
@@ -1,5 +0,0 @@
-echo STARTED_TIME="`date -u +'%Y-%m-%dT%H:%M:%S'`" > ci_status_params.txt
-osci-mirrors/fuel_master_mirror_vc.sh
-echo "USE_STABLE_MOS_FOR_STAGING = $USE_STABLE_MOS_FOR_STAGING" >> ${WORKSPACE:-"."}/mirror_staging.txt
-echo FINISHED_TIME="`date -u +'%Y-%m-%dT%H:%M:%S'`" >> ci_status_params.txt
-
--- servers/jenkins-product-ci/6.1/builders/get_stable_iso_magnet_link.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-infile="${WORKSPACE:-.}/stable_iso_build_url.txt"
-outfile="${WORKSPACE:-.}/stable_iso_magnet_link.txt"
-
-rm -f ${outfile}
-curl -fsS -o ${outfile} $(cat ${infile})artifact/magnet_link.txt
-cat ${outfile}
