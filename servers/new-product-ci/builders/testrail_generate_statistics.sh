1dc8b3711938b5a965f8579c13e4bd1304bcefb0
Artem Panchenko <apanchenko@mirantis.com>
2016-01-28 01:00:59 +0200

    Create new job for TestRail bugs stats generation
    Create new job 'testrail.bugs.statistics' which can
    be run manually against any test plan in TestRail in
    order to collect attached bugs and create reports.
    
    Also use new separate builder for SWARM statistics
    generation.
    
    Change-Id: I0fe729bba5c76786e98a011851c8a1a566f1435a
    Partial-bug: #1533196

---
+++ servers/new-product-ci/builders/testrail_generate_statistics.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -ex
+
+# Prepare venv
+source "${VENV_PATH:-/home/jenkins/venv-nailgun-tests-2.9/bin/activate}"
+
+# Initialize variables
+export TESTRAIL_STATS_GENERATOR="fuelweb_test/testrail/generate_statistics.py"
+
+OPTIONS=" --verbose"
+
+if [ "${HANDLE_BLOCKED}" == "true" ]; then
+    OPTIONS+=" --handle-blocked"
+fi
+
+if [ "${PUBLISH}" == "true" ]; then
+    OPTIONS+=" --publish"
+fi
+
+if [ "${OUTPUT_FILE:-none}" != "none" ]; then
+    OPTIONS+=" --out-file ${OUTPUT_FILE}"
+fi
+
+if [ "${TESTRAIL_RUN_IDS:-none}" != "none" ]; then
+    OPTIONS+=" --run-ids ${TESTRAIL_RUN_IDS}"
+fi
+
+if [ "${TEST_RUNNER_JOB_NAME:-none}" != "none" ]; then
+    OPTIONS+=" --job-name ${TEST_RUNNER_JOB_NAME}"
+fi
+
+if [ "${BUILD_NUMBER:-none}" != "none" ]; then
+    OPTIONS+=" --build-number ${BUILD_NUMBER}"
+fi
+
+if [ "${SEPARATE_RUNS}" == "true" ]; then
+    OPTIONS+=" --separate-runs"
+fi
+
+if [ "${CREATE_HTML}" == "true" ]; then
+    OPTIONS+=" --html"
+fi
+
+if [ "${TEST_PLAN_ID:-none}" != "none" ]; then
+    OPTIONS+=" ${TEST_PLAN_ID}"
+fi
+
+python "${TESTRAIL_STATS_GENERATOR}" ${OPTIONS}
