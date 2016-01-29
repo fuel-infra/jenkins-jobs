f72f210dbb219a2a9f8f181f15fca493ee5fe71c
Aliaksei Cherniakou <acherniakou@mirantis.com>
2016-01-29 18:04:33 +0300

    Destroy VMs in case of time out for bvt and smoke
    This change enables VMs destroy functionality for bvt and smoke jobs
    if ones terminated due time out.
    
    Related-Bug: #1491820
    Change-Id: I8bfc8148d95534903809ba6b3e75e8d34604bf2a

--- servers/new-product-ci/builders/run_system_test.sh
+++ servers/new-product-ci/builders/run_system_test.sh
@@ -67,6 +67,8 @@ export VENV_PATH=${VENV_PATH:-/home/jenkins/venv-nailgun-tests}
 
 ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER.$BUILD_ID
 ENV_NAME=${ENV_NAME:0:68}
+echo "export ENV_NAME=\"${ENV_NAME}\"" > "${WORKSPACE}/${DOS_ENV_NAME_PROPS_FILE:=.dos_environment_name}"
+
 ISO_PATH=$(seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
 
 sh -x "utils/jenkins/system_tests.sh" -t test -w "$WORKSPACE" -e "$ENV_NAME" -o --group="$TEST_GROUP" -i "$ISO_PATH"
