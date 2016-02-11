2dfd6a7c4fc0be720da694505e202f348e834e70
vrovachev <vrovachev@mirantis.com>
2016-02-11 16:12:18 +0300

    Copied big swarm runner for 8.0 release
    Copied big swarm runner for 8.0 release
    from new-product-ci to patching-ci
    
    In swarm.tests.yaml we define full set of system tests,
    but create two separate runner jobs: small and full.
    
    8.0.small.runner triggers only a subset of jobs
    defined in swarm.tests.yaml
    
    Update 8.0 swarm tests template to use static magnet link for iso
    
    Remove latest-stable option from ubuntu mirrors, this change allow
    to execute jobs without dependency on product-ci
    
    Change-Id: Ie7a31b0b3a3a8d89493307fe08d9d7b482755cf9

--- common/8.0/scripts/run_plugin_example_system_test.sh
+++ common/8.0/scripts/run_plugin_example_system_test.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-TEST_ISO_JOB_URL="${JENKINS_URL}job/${ENV_PREFIX:0:3}.test_all/"
-
 ###################### Get MIRROR HOST ###############
 
 LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location)
@@ -41,10 +39,6 @@ esac
 if [[ ! "${MIRROR_UBUNTU}" ]]; then
 
     case "${UBUNTU_MIRROR_ID}" in
-        latest-stable)
-            UBUNTU_MIRROR_ID="$(curl -fsS "${TEST_ISO_JOB_URL}lastSuccessfulBuild/artifact/ubuntu_mirror_id.txt" | awk -F '[ =]' '{print $NF}')"
-            UBUNTU_MIRROR_URL="${MIRROR_HOST}${UBUNTU_MIRROR_ID}/"
-            ;;
         latest)
             UBUNTU_MIRROR_URL="$(curl ${MIRROR_HOST}ubuntu-latest.htm)"
             ;;
@@ -53,6 +47,61 @@ if [[ ! "${MIRROR_UBUNTU}" ]]; then
     esac
 
     export MIRROR_UBUNTU="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+fi
+
+###################### Set extra DEB and RPM repos ####
+
+if [[ -n "${RPM_LATEST}" ]]; then
+    RPM_MIRROR="${MIRROR_HOST}mos-repos/centos/mos8.0-centos7-fuel/snapshots/"
+    if [[ "${ENABLE_PROPOSED}" == "true" ]]; then
+        RPM_PROPOSED="mos-proposed,${RPM_MIRROR}proposed-${RPM_LATEST}/x86_64"
+        EXTRA_RPM_REPOS+="${RPM_PROPOSED}"
+        UPDATE_FUEL_MIRROR="${RPM_MIRROR}proposed-${RPM_LATEST}/x86_64"
+    fi
+    if [[ "${ENABLE_UPDATES}" == "true" ]]; then
+        RPM_UPDATES="mos-updates,${RPM_MIRROR}updates-${RPM_LATEST}/x86_64"
+        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
+            EXTRA_RPM_REPOS+="|"
+            UPDATE_FUEL_MIRROR+=" "
+        fi
+        EXTRA_RPM_REPOS+="${RPM_UPDATES}"
+        UPDATE_FUEL_MIRROR+="${RPM_MIRROR}updates-${RPM_LATEST}/x86_64"
+    fi
+    if [[ "${ENABLE_SECURITY}" == "true" ]]; then
+        RPM_SECURITY="mos-security,${RPM_MIRROR}security-${RPM_LATEST}/x86_64"
+        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
+            EXTRA_RPM_REPOS+="|"
+            UPDATE_FUEL_MIRROR+=" "
+        fi
+        EXTRA_RPM_REPOS+="${RPM_SECURITY}"
+        UPDATE_FUEL_MIRROR+="${RPM_MIRROR}security-${RPM_LATEST}/x86_64"
+    fi
+    export EXTRA_RPM_REPOS
+    export UPDATE_FUEL_MIRROR
+    export UPDATE_MASTER=true
+fi
+
+if [[ -n "${DEB_LATEST}" ]]; then
+    DEB_MIRROR="${MIRROR_HOST}mos-repos/ubuntu/snapshots/8.0-${DEB_LATEST}"
+    if [[ "${ENABLE_PROPOSED}" == "true" ]]; then
+        DEB_PROPOSED="mos-proposed,deb ${DEB_MIRROR} mos8.0-proposed main restricted"
+        EXTRA_DEB_REPOS+="${DEB_PROPOSED}"
+    fi
+    if [[ "${ENABLE_UPDATES}" == "true" ]]; then
+        DEB_UPDATES="mos-updates,deb ${DEB_MIRROR} mos8.0-updates main restricted"
+        if [[ -n "${EXTRA_DEB_REPOS}" ]]; then
+            EXTRA_DEB_REPOS+="|"
+        fi
+        EXTRA_DEB_REPOS+="${DEB_UPDATES}"
+    fi
+    if [[ "${ENABLE_SECURITY}" == "true" ]]; then
+        DEB_SECURITY="mos-security,deb ${DEB_MIRROR} mos8.0-security main restricted"
+        if [[ -n "${EXTRA_DEB_REPOS}" ]]; then
+            EXTRA_DEB_REPOS+="|"
+        fi
+        EXTRA_DEB_REPOS+="${DEB_SECURITY}"
+    fi
+    export EXTRA_DEB_REPOS
 fi
 
 rm -rf logs/*
@@ -65,7 +114,7 @@ ISO_PATH=$(seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "
 VERSION_STRING=$(basename "${ISO_PATH}" | cut -d '-' -f 2-3)
 echo "Description string: ${TEST_GROUP} on ${VERSION_STRING}"
 
-export MAKE_SNAPSHOT=false
+export MAKE_SNAPSHOT=true
 
 ################ PLUGINS  ###############
 
