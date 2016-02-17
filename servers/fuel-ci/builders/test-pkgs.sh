4725888d9be3be8fc9152a1c7a861d9a1ec87269
Sergey Kulanov <skulanov@mirantis.com>
2016-02-17 11:21:11 +0200

    Extend ISO name description for pkgs jobs
    For each job which deals with packages we have description
       9.0-liberty
    So let's make it
       9.0-liberty-{BUILD_ID}, for example 9.0-liberty-226
    This will make description more human friendly
    
    Change-Id: I02de09a0ff36a61c749a3970ab47002e1a3c6e77

--- servers/fuel-ci/builders/test-pkgs.sh
+++ servers/fuel-ci/builders/test-pkgs.sh
@@ -9,7 +9,7 @@ export LOGS_DIR=/home/jenkins/workspace/${JOB_NAME}/logs/${BUILD_NUMBER}
 
 #test params
 
-VERSION_STRING=`readlink ${ISO_PATH} | cut -d '-' -f 2-3`
+VERSION_STRING=`readlink ${ISO_PATH} | cut -d '-' -f 2-4`
 echo "Description string: ${VERSION_STRING}"
 
 sh -x "${SYSTEM_TESTS}" -w "${SYSTEST_ROOT}" -V "${VENV_PATH}" -i "${ISO_PATH}" -t test -e "${ENV_NAME}" -o --group=${TEST_GROUP}
