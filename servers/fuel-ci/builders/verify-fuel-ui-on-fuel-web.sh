a449902b58ac2da0194645c8ba5508c64432a5cd
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2016-03-03 16:19:38 +0300

    Add verify-fuel-ui-on-fuel-web job
    We have moved UI javascript code to a
    separate repository, but we still need to make
    sure that fuel-web patches are compatible with
    UI. This job runs UI functional tests with:
    
    CR fuel-web + HEAD fuel-ui
    
    Change-Id: Iffd234e31adbeb6aaf156f81acd38223145843b1
    Implements: blueprint separate-fuel-ui-repo

---
+++ servers/fuel-ci/builders/verify-fuel-ui-on-fuel-web.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+export DISPLAY=:99
+export PATH=$PATH:${NPM_CONFIG_PREFIX}/bin
+
+
+VENV="${WORKSPACE}_VENV"
+[ "${VENV_CLEANUP}" == "true" ] && rm -rf "${VENV}"
+virtualenv -p python2.7 "${VENV}"
+source "${VENV}/bin/activate"
+pip install --upgrade 'tox>=2.3.1'
+
+NODE_MODULES="${VENV}/node_modules"
+
+mkdir -p "${NODE_MODULES}"
+
+pushd "${WORKSPACE}"/fuel-ui
+[ -L node_modules ] || ln -s "${NODE_MODULES}" node_modules
+
+npm install
+export ARTIFACTS="${WORKSPACE}"/test_run/ui_func
+export FUEL_WEB_ROOT="${WORKSPACE}"
+export NAILGUN_DB_HOST=127.0.0.1
+export DB_ROOTPW=insecurepassword
+
+# Run UI tests
+npm test
+popd
+deactivate
