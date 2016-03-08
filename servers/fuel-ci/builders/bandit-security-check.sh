335b93ece5a94e6a1593975508dc610c691816bf
Egor Kotko <ykotko@mirantis.com>
2016-03-08 12:12:44 +0100

    Bandit security test job
    Add security code revision job
    with help of bandit tool
    
    Change-Id: I7dfa9947f2a94eb257c402bcc2b9cefcac57b69f
    Closes-bug: #1554159

---
+++ servers/fuel-ci/builders/bandit-security-check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+VENV="${WORKSPACE}_VENV"
+virtualenv -p python2.7 "${VENV}"
+source "${VENV}/bin/activate" || exit 1
+
+pip install "bandit==${BANDIT_PACKAGE_VERSION}"
+
+BANDIT_ARGS="-c ${VENV}/etc/bandit/bandit.yaml -r ${WORKSPACE}/${GERRIT_PROJECT} -n5"
+
+if [[ ! -z "${BANDIT_PLUGIN_EXCLUDE}" ]]; then
+  for i in "${BANDIT_PLUGIN_EXCLUDE[@]}"
+    do
+      sed -i -e "s/- ${i}/#- ${i}/g" "${VENV}/etc/bandit/bandit.yaml"
+  done
+  BANDIT_ARGS="${BANDIT_ARGS} -p All"
+fi
+bandit "${BANDIT_ARGS}"
