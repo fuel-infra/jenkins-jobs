d1cdb1894727fd3fe248782be53fb8f264fba06c
Anton Tcitlionok <atcitlionok@mirantis.com>
2016-03-09 13:42:42 +0300

    Add job to run Dockerized fuel webui tests for 8.0
    Change-Id: I9991653aca4b83001f41497e1e2621eaa5666313

---
+++ servers/fuel-ci/8.0/builders/run_docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+# Default mounts
+JOB_HOST_PATH="${WORKSPACE}/${JOB_NAME}"
+JOB_DOCKER_PATH="/opt/jenkins/${JOB_NAME}"
+
+# Path to runner script inside Docker image
+SCRIPT_PATH="/opt/jenkins/runner.sh"
+
+# Docker Registry to use
+REGISTRY="registry.fuel-infra.org/jenkins/fuel-ci-tests"
+
+# Container ID path
+CONTAINER_ID="${WORKSPACE}/container.id"
+
+CMDPARAM=;
+for i in ${VOLUMES}; do
+    CMDPARAM="${CMDPARAM} -v ${i}"
+done
+
+docker pull "${REGISTRY}:${DOCKER_IMAGE}"
+docker run -v "${JOB_HOST_PATH}:${JOB_DOCKER_PATH}" ${CMDPARAM} \
+           --cidfile="${CONTAINER_ID}" \
+           ${ENVVARS} -t "${REGISTRY}:${DOCKER_IMAGE}" \
+           /bin/bash -exc "${SCRIPT_PATH}"
+
+# Stop container after job finishes, preserving exit code
+exitcode=$?
+docker stop "$(cat "${CONTAINER_ID}")"
+exit $exitcode
