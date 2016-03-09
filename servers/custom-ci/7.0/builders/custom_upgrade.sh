427dd56db7447ddba0d36a93e563ef7ebc83f2bb
Artur Kaszuba <akaszuba@mirantis.com>
2016-03-09 20:52:06 +0100

    Initial jobs for custom-ci
    This is initial version of jobs for new jenkins, dedicated for custom jobs.
    It contains custom jobs from jenkins-product for ISO build, system_test
    and packages.
    
    Changes which was created to original job:
    - updates names in standard MOS_VERSION.custom.test_type
    - changed script name custom.sh to custom_iso.sh
    - added authorization property with permission to start jobs by authorized
      users
    - updated custom package jobs to use /var/www/packages directory
    
    Change-Id: I0bb9e705a20342deeb1fd67b3aa27427b4ac894d

---
+++ servers/custom-ci/7.0/builders/custom_upgrade.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+set -ex
+
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:${PATH}
+
+PROD_VER=$(grep 'PRODUCT_VERSION:=' config.mk | cut -d= -f2)
+
+export ISO_NAME="fuel-gerrit-${PROD_VER}-${BUILD_NUMBER}-${BUILD_ID}"
+export UPGRADE_TARBALL_NAME="fuel-gerrit-${PROD_VER}-upgrade-${BUILD_NUMBER}-${BUILD_ID}"
+
+export BUILD_DIR="${WORKSPACE}/../tmp/${JOB_NAME}/build"
+export LOCAL_MIRROR="${WORKSPACE}/../tmp/${JOB_NAME}/local_mirror"
+
+export ARTS_DIR="${WORKSPACE}/artifacts"
+rm -rf "${ARTS_DIR}"
+
+export DEPS_DIR="${BUILD_DIR}/deps"
+rm -rf "${DEPS_DIR}"
+
+# Checking gerrit commits for fuel-main
+if [ "${FUELMAIN_COMMIT}" != "stable/7.0" ] ; then
+    git checkout "${FUELMAIN_COMMIT}"
+fi
+
+# Checking gerrit commits for fuel-main
+if [ "${fuelmain_gerrit_commit}" != "none" ] ; then
+  for commit in ${fuelmain_gerrit_commit} ; do
+    git fetch https://review.openstack.org/openstack/fuel-main "${commit}" && git cherry-pick FETCH_HEAD || false
+  done
+fi
+
+export NAILGUN_GERRIT_COMMIT="${nailgun_gerrit_commit}"
+export ASTUTE_GERRIT_COMMIT="${astute_gerrit_commit}"
+export OSTF_GERRIT_COMMIT="${ostf_gerrit_commit}"
+export FUELLIB_GERRIT_COMMIT="${fuellib_gerrit_commit}"
+export PYTHON_FUELCLIENT_GERRIT_COMMIT="${python_fuelclient_gerrit_commit}"
+export FUEL_AGENT_GERRIT_COMMIT="${fuel_agent_gerrit_commit}"
+export FUEL_NAILGUN_AGENT_GERRIT_COMMIT="${fuel_nailgun_agent_gerrit_commit}"
+
+######## Get node location to choose closer mirror ###############
+# Do NOTHING if USE_MIRROR=none, else if user choose auto, we try to use
+# facter location to get closer mirror, if user provide the exact mirror we
+# use it
+if [ "${USE_MIRROR}" != "none" ]; then
+
+  if [ "${USE_MIRROR}" == "auto" ]; then
+    LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location)
+    LOCATION=${LOCATION_FACT:-msk}
+  else
+    LOCATION="${USE_MIRROR}"
+  fi
+
+  case "${LOCATION}" in
+      srt)
+          USE_MIRROR=srt
+          LATEST_MIRROR_ID_URL=http://osci-mirror-srt.srt.mirantis.net
+          ;;
+      msk)
+          USE_MIRROR=msk
+          LATEST_MIRROR_ID_URL=http://osci-mirror-msk.msk.mirantis.net
+          ;;
+      hrk)
+          USE_MIRROR=hrk
+          LATEST_MIRROR_ID_URL=http://osci-mirror-kha.kha.mirantis.net
+          ;;
+      poz|bud|bud-ext|cz)
+          USE_MIRROR=cz
+          LATEST_MIRROR_ID_URL=http://mirror.seed-cz1.fuel-infra.org
+          ;;
+      mnv|scc)
+          USE_MIRROR=usa
+          LATEST_MIRROR_ID_URL=http://mirror.seed-us1.fuel-infra.org
+          ;;
+      *)
+          USE_MIRROR=msk
+          LATEST_MIRROR_ID_URL=http://osci-mirror-msk.msk.mirantis.net
+  esac
+
+  LATEST_TARGET=$(curl -sSf "${LATEST_MIRROR_ID_URL}/mos-repos/ubuntu/7.0.target.txt" | head -1)
+  export MIRROR_MOS_UBUNTU_ROOT="/mos-repos/ubuntu/${LATEST_TARGET}"
+fi
+
+echo "Using mirror: ${USE_MIRROR} with ${MIRROR_MOS_UBUNTU_ROOT}"
+
+#########################################
+
+echo "STEP 0. Clean before start"
+make deep_clean
+
+#########################################
+
+echo "STEP 1. Make everything"
+make ${make_args} iso upgrade-lrzip version-yaml openstack-yaml
+
+#########################################
+
+echo "STEP 2. Publish everything"
+
+cd "${ARTS_DIR}"
+for artifact in $(ls fuel-*)
+do
+  "${WORKSPACE}/utils/jenkins/process_artifacts.sh" "${artifact}"
+done
+
+cd "${WORKSPACE}"
+
+echo FUELMAIN_GERRIT_COMMIT="${fuelmain_gerrit_commit}" > ${ARTS_DIR}/gerrit_commits.txt
+echo NAILGUN_GERRIT_COMMIT="${nailgun_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
+echo ASTUTE_GERRIT_COMMIT="${astute_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
+echo OSTF_GERRIT_COMMIT="${ostf_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
+echo FUELLIB_GERRIT_COMMIT="${fuellib_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
+echo PYTHON_FUELCLIENT_GERRIT_COMMIT="${python_fuelclient_gerrit_commit}" >> ${ARTS_DIR}/gerrit_commits.txt
+echo FUEL_AGENT_GERRIT_COMMIT="${fuel_agent_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+echo FUEL_NAILGUN_AGENT_GERRIT_COMMIT="${fuel_nailgun_agent_gerrit_commit}" >> "${ARTS_DIR}/gerrit_commits.txt"
+
+cp "${LOCAL_MIRROR}"/*changelog "${ARTS_DIR}/" || true
+cp "${BUILD_DIR}/iso/isoroot/version.yaml" "${WORKSPACE}/version.yaml.txt" || true
+(cd "${BUILD_DIR}/iso/isoroot" && find . | sed -s 's/\.\///') > "${WORKSPACE}/listing.txt" || true
+
+grep MAGNET_LINK "${ARTS_DIR}"/fuel-*.iso.data.txt > "${WORKSPACE}/magnet_link.txt"
+grep MAGNET_LINK "${ARTS_DIR}"/fuel-*upgrade-*.data.txt > "${WORKSPACE}/upgrade_magnet_link.txt"
+
+#########################################
+
+echo "STEP 3. Generate build description"
+
+UPGRADE_MAGNET_LINK=$(grep MAGNET_LINK "${ARTS_DIR}"/*upgrade-*.data.txt | sed 's/MAGNET_LINK=//')
+UPGRADE_HTTP_LINK=$(grep HTTP_LINK "${ARTS_DIR}"/*upgrade-*.data.txt | sed 's/HTTP_LINK=//')
+UPGRADE_HTTP_TORRENT=$(grep HTTP_TORRENT "${ARTS_DIR}"/*upgrade-*.data.txt | sed 's/HTTP_TORRENT=//')
+
+echo "<a href=${UPGRADE_HTTP_LINK}>UPGD download link</a> <a href=${UPGRADE_HTTP_TORRENT}>UPGD torrent link</a><br>${UPGRADE_MAGNET_LINK}<br>"
+
+echo "BUILD FINISHED."
---
+++ servers/custom-ci/8.0/builders/custom_system_test.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+set -ex
+
+TEST_ISO_JOB_URL="https://product-ci.infra.mirantis.net/job/8.0.test_all/"
+
+###################### Get MIRROR HOST ###############
+
+LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location)
+LOCATION=${LOCATION_FACT:-bud}
+
+case "${LOCATION}" in
+    srt)
+        MIRROR_HOST="http://osci-mirror-srt.srt.mirantis.net/pkgs/"
+        ;;
+    msk)
+        MIRROR_HOST="http://osci-mirror-msk.msk.mirantis.net/pkgs/"
+        ;;
+    kha)
+        MIRROR_HOST="http://osci-mirror-kha.kha.mirantis.net/pkgs/"
+        ;;
+    poz)
+        MIRROR_HOST="http://osci-mirror-poz.poz.mirantis.net/pkgs/"
+        ;;
+    bud)
+        MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org/pkgs/"
+        ;;
+    bud-ext)
+        MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org/pkgs/"
+        ;;
+    mnv|scc)
+        MIRROR_HOST="http://mirror.seed-us1.fuel-infra.org/pkgs/"
+        ;;
+    *)
+        MIRROR_HOST="http://mirror.fuel-infra.org/pkgs/"
+esac
+
+###################### Get MIRROR_UBUNTU ###############
+
+if [[ ! "${MIRROR_UBUNTU}" ]]; then
+
+    case "${UBUNTU_MIRROR_ID}" in
+        latest-stable)
+            UBUNTU_MIRROR_ID="$(curl -fsS "${TEST_ISO_JOB_URL}lastSuccessfulBuild/artifact/ubuntu_mirror_id.txt" | awk -F '[ =]' '{print $NF}')"
+            UBUNTU_MIRROR_URL="${MIRROR_HOST}${UBUNTU_MIRROR_ID}/"
+            ;;
+        latest)
+            UBUNTU_MIRROR_URL="$(curl ${MIRROR_HOST}ubuntu-latest.htm)"
+            ;;
+        *)
+            UBUNTU_MIRROR_URL="${MIRROR_HOST}${UBUNTU_MIRROR_ID}/"
+    esac
+
+    export MIRROR_UBUNTU="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+fi
+
+if [[ "${NETWORK_MODE}" == "Neutron/VLAN" ]]; then
+  export NEUTRON_ENABLE="true"
+  export NEUTRON_SEGMENT_TYPE="vlan"
+fi
+
+if [[ "${NETWORK_MODE}" == "Neutron/GRE" ]]; then
+  export NEUTRON_ENABLE="true"
+  export NEUTRON_SEGMENT_TYPE="gre"
+fi
+
+if [[ "${NETWORK_MODE}" == "Neutron/VXLAN" ]]; then
+  export NEUTRON_ENABLE="true"
+  export NEUTRON_SEGMENT_TYPE="tun"
+fi
+
+# Checking gerrit commits for fuel-qa
+if [[ "${fuel_qa_gerrit_commit}" != "none" ]] ; then
+  for commit in ${fuel_qa_gerrit_commit} ; do
+    git fetch https://review.openstack.org/openstack/fuel-qa "${commit}" && git cherry-pick FETCH_HEAD
+  done
+fi
+
+# Check if custom test group is specified
+if [[ ! -z "${CUSTOM_TEST_GROUP}" ]]; then
+  export TEST_GROUP="${CUSTOM_TEST_GROUP}"
+fi
+
+export VENV_PATH="/home/jenkins/venv-nailgun-tests-2.9"
+rm -rf logs/*
+
+export MAKE_SNAPSHOT=${MAKE_SNAPSHOT}
+ISO_PATH=$(seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
+echo "${ISO_PATH}"
+
+# Fix parameters for ha_neutron_destructive test
+if [[ "${TEST_GROUP}" == "ha_neutron_destructive" ]]; then
+  export NEUTRON_ENABLE="true"
+fi
+
+ENV_NAME=${ENV_PREFIX}.${BUILD_NUMBER}.${BUILD_ID}
+export ENV_NAME=${ENV_NAME:0:68}
+echo "export ENV_NAME=\"${ENV_NAME}\"" > "${WORKSPACE}/${DOS_ENV_NAME_PROPS_FILE:=.dos_environment_name}"
+
+export PATH_TO_CERT=${WORKSPACE}"/"${ENV_NAME}".crt"
+export PATH_TO_PEM=${WORKSPACE}"/"${ENV_NAME}".pem"
+
+export OPENSTACK_RELEASE="${OPENSTACK_RELEASE}"
+
+echo "Description string: ${TEST_GROUP} on ${NODE_NAME}: ${ENV_NAME}"
+
+sh -x "utils/jenkins/system_tests.sh" -t test -w "${WORKSPACE}" -V "${VENV_PATH}" -j "${JOB_NAME}" -o --group="${TEST_GROUP}" -i "${ISO_PATH}"
