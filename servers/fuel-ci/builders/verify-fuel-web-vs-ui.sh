8c16b1fd587f7b8c246a4fb6890be345bcd18597
Ivan Remizov <iremizov@mirantis.com>
2016-03-16 17:36:33 +0300

    Add nightly-test-fuel-ui-on-fuel-web job
    This patch set adds new job for nightly UI tests.
    It also adds some refactoring for existent jobs.
    
    Change-Id: Id48e654e249cbb56b294349ca0d2ca6d8baf9b66

--- servers/fuel-ci/builders/verify-fuel-web-vs-ui.sh
+++ servers/fuel-ci/builders/verify-fuel-web-vs-ui.sh
@@ -3,7 +3,7 @@
 set -ex
 
 export DISPLAY=:99
-export PATH=$PATH:${NPM_CONFIG_PREFIX}/bin
+export PATH="$PATH:${NPM_CONFIG_PREFIX}/bin"
 
 VENV="${WORKSPACE}_VENV"
 [ "${VENV_CLEANUP}" == "true" ] && rm -rf "${VENV}"
@@ -13,19 +13,19 @@ pip install --upgrade 'tox>=2.3.1'
 
 NODE_MODULES="${VENV}/node_modules"
 
+pushd "${WORKSPACE}"/fuel-ui
 mkdir -p "${NODE_MODULES}"
 
 [ -L node_modules ] || ln -s "${NODE_MODULES}" node_modules
 
 npm install
-export ARTS="${WORKSPACE}"/test_run/ui_func
-export ARTIFACTS="${WORKSPACE}"/test_run/ui_func
+export ARTS="${WORKSPACE}"/artifacts
+export ARTIFACTS="${ARTS}"
 export FUEL_WEB_ROOT="${WORKSPACE}/fuel-web"
 export NAILGUN_DB_HOST=127.0.0.1
 export DB_ROOTPW=insecurepassword
 
 # Run UI tests
-npm run lint
-npm test
-
+npm run "${UI_TEST_GROUP}"
+popd
 deactivate
