663136811b5a02dc2c8224283ce5f00c02f49728
Egor Kotko <ykotko@mirantis.com>
2016-03-23 16:14:42 +0100

    Quotes fix
    Fix the problem with quoting
    in bash executed by Jenkins
    
    Change-Id: I9eaad7302f5bf0a8fff5638202e4511942d2d066
    Closes-bug: #1554159

--- servers/fuel-ci/builders/bandit-security-check.sh
+++ servers/fuel-ci/builders/bandit-security-check.sh
@@ -8,13 +8,13 @@ source "${VENV}/bin/activate" || exit 1
 
 pip install "bandit==${BANDIT_PACKAGE_VERSION}"
 
-BANDIT_ARGS="-c ${VENV}/etc/bandit/bandit.yaml -r ${WORKSPACE}/${GERRIT_PROJECT} -n5"
+BANDIT_ARGS=(-c "${VENV}"/etc/bandit/bandit.yaml -r "${WORKSPACE}"/"${GERRIT_PROJECT}" -n5)
 
 if [[ ! -z "${BANDIT_PLUGIN_EXCLUDE}" ]]; then
   for i in "${BANDIT_PLUGIN_EXCLUDE[@]}"
     do
       sed -i -e "s/- ${i}/#- ${i}/g" "${VENV}/etc/bandit/bandit.yaml"
   done
-  BANDIT_ARGS="${BANDIT_ARGS} -p All"
+  BANDIT_ARGS+=(-p All)
 fi
-bandit "${BANDIT_ARGS}"
+bandit "${BANDIT_ARGS[@]}"
