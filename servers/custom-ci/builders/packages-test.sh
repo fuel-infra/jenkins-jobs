56e0855cc00933b77ca038f01bc443af49ecf3f8
Artur Kaszuba <akaszuba@mirantis.com>
2016-04-01 16:47:33 +0200

    Add custom package tests to custom-ci
    This change will move custom package build test from jenkins-product to
    custom-ci.
    
    Change-Id: I2eb8a52bf79b1e1c75c3a404830fe33c8f89df2a

---
+++ servers/custom-ci/builders/packages-test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+## needed variables:
+# MIRROR_ARTIFACT - aka PERESTROIKA mirror, example:
+# ${JENKINS_URL}job/${ENV_JOB}/lastSuccessfulBuild/artifact/mirror_ubuntu_data.txt
+export MIRROR_UBUNTU=$(curl -sSf "${MIRROR_ARTIFACT}")
+
+# Checking gerrit commits for fuel-qa
+if [[ "${FUEL_QA_GERRIT_COMMIT}" != "none" ]] ; then
+  cd ${SYSTEST_ROOT}
+  for commit in ${FUEL_QA_GERRIT_COMMIT} ; do
+    git fetch https://review.openstack.org/openstack/fuel-qa "${commit}" && git cherry-pick FETCH_HEAD
+  done
+fi
+
+export SYSTEM_TESTS="${SYSTEST_ROOT}/utils/jenkins/system_tests.sh"
+export LOGS_DIR=/home/jenkins/workspace/${JOB_NAME}/logs/${BUILD_NUMBER}
+
+# Check if custom test group is specified
+if [[ ! -z "${CUSTOM_TEST_GROUP}" ]]; then
+  export TEST_GROUP="${CUSTOM_TEST_GROUP}"
+fi
+
+#test params
+VERSION_STRING=$(readlink ${ISO_PATH} | cut -d '-' -f 2-3)
+echo "Description string: ${VERSION_STRING}"
+
+sh -x "${SYSTEM_TESTS}" \
+  -w "${SYSTEST_ROOT}/" \
+  -V "${VENV_PATH}" \
+  -i "${ISO_PATH}" \
+  -t test \
+  -e "${ENV_NAME}" \
+  -o --group="${TEST_GROUP}"
