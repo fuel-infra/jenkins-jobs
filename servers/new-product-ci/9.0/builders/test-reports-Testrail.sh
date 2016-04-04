e5737ff9620518058fe46d21dc7ec9e94716e8d4
Dmitry Kaigarodtsev <dkaiharodsev@mirantis.com>
2016-04-04 13:59:12 +0300

    Fixed '9.0.acceptance.test-reports' job:
    - 'test-reports-Testrail.sh' script same as in 8.0
    
    Change-Id: I3284da332f703944e7d445667b35a3000e58bd6b

--- servers/new-product-ci/9.0/builders/test-reports-Testrail.sh
+++ servers/new-product-ci/9.0/builders/test-reports-Testrail.sh
@@ -7,16 +7,13 @@ set -ex
 export TESTRAIL_USER=${TESTRAIL_USER}
 export TESTRAIL_PASSWORD=${TESTRAIL_PASSWORD}
 export TESTRAIL_REPORTER_PATH="fuelweb_test/testrail/report.py"
-export SWARM_RUNNER_JOB_NAME='9.0.swarm.runner'
-export TESTRAIL_PROJECT="Mirantis OpenStack"
-export TESTRAIL_URL="https://mirantis.testrail.com"
 
 # Prepare venv
 source /home/jenkins/venv-nailgun-tests-2.9/bin/activate
 export PYTHONPATH="$(pwd):$PYTHONPATH"
 
 if [ "${BUG_STATISTIC}" == "true" ]; then
- RUNNER_BUILD_NUMBER=$(curl "${JENKINS_URL}/job/${SWARM_RUNNER_JOB_NAME}/lastBuild/buildNumber")
+ RUNNER_BUILD_NUMBER=$(curl "${JENKINS_URL}/job/${TESTS_RUNNER}/lastBuild/buildNumber")
  OPTIONS="-s -N ${RUNNER_BUILD_NUMBER}"
 elif [ "${TEST_JOB_NAME}" != "none" ]; then
  OPTIONS="-o ${TEST_JOB_NAME}"
@@ -24,8 +21,6 @@ fi
 
 # Report tests results from swarm (Ubuntu)
 
-export TESTRAIL_TEST_SUITE='[9.0] Swarm'
 export USE_UBUNTU=true
 export USE_CENTOS=false
-python ${TESTRAIL_REPORTER_PATH} -v -l -j "${SWARM_RUNNER_JOB_NAME}" ${OPTIONS}
-
+python ${TESTRAIL_REPORTER_PATH} -v -l -j "${TESTS_RUNNER}" ${OPTIONS}
