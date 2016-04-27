6076c1b1f7fc42c16ac66cf46c8953b7bd162951
Ivan Remizov <iremizov@mirantis.com>
2016-04-27 17:09:36 +0300

    Add description/docs for build-late-artifacts.sh
    This patchset adds documentation for the script and
    fixes some defects.
    
    Related-Bug: #1559117
    Change-Id: Ie70eae0a0ae0107ec90bf78dbd1e8307b2141d13

--- servers/pkgs-ci/builders/build_late_artifacts.sh
+++ servers/pkgs-ci/builders/build_late_artifacts.sh
@@ -1,78 +1,128 @@
-#!/bin/bash -ex
-# Required parameters:
-# - FUELMAIN_COMMIT         commit for fuel-main
-# - fuelmain_gerrit_commit  (array) refspecs for commits in fuel-main gerrit
-# - NOARTIFACT_MIRROR       upstream mirror without late artifacts (??)
-# - EXTRA_RPM_REPOS         additional CentOS repos
-# - make_args               (array) additional parameters to make command
+#!/bin/bash
+#
+#   :mod:`build-late-artifacts` -- A Wrapper for building late artifacts
+#   ====================================================================
+#
+#   .. module:: build-late-artifacts
+#       :platform: Ubuntu 14.04
+#       :synopsis: building late artifacts
+#   .. vesionadded:: MOS-8.0 patching
+#   .. vesionchanged:: MOS-9.0 patching
+#   .. author:: Ivan Remizov <iremizov@mirantis.com>
+#
+#
+#   .. envvar::
+#       :var  FUELMAIN_COMMIT: commit for fuel-main
+#       :type FUELMAIN_COMMIT: refspec:str
+#       :var  fuelmain_gerrit_commit: refspecs for commits in ``fuel-main`` gerrit
+#       :type fuelmain_gerrit_commit: arr[refspec:str]
+#       :var  EXTRA_RPM_REPOS: list of rpm repos to use at build step
+#       :type EXTRA_RPM_REPOS: arr[str]
+#       :var  make_args: list of additional parameters to make command
+#       :type make_args: arr[str]
+#       :var  JOB_NAME: Name of Jenkins Job
+#       :type JOB_NAME: str
+#       :var  WORKSPACE: Location where build is started, defaults to ``.``
+#       :type WORKSPACE: path
+#
+#   .. requirements::
+#
+#       * sudo
+#       * vivid kernel
+#       * docker
+#
+#   .. class:: sample.envfile
+#       FIXME: Add description for artifact
+#
+#
+#   .. entrypoint:: main
+#
+#
+#   .. affects::
+#       :file buildresult.params: dumped env variables
+#                                 similar to zuul pipelines
+#
 
-export BUILD_DIR="${WORKSPACE}/../tmp/${JOB_NAME}/build"
-export LOCAL_MIRROR="${WORKSPACE}/../tmp/${JOB_NAME}/local_mirror"
+set -ex
+
+export BUILD_DIR="$(readlink -e "${WORKSPACE}/../tmp/${JOB_NAME}/build")"
+export LOCAL_MIRROR="$(readlink -e "${WORKSPACE}/../tmp/${JOB_NAME}/local_mirror")"
 export ARTS_DIR="${WORKSPACE}/artifacts"
 export DEPS_DIR="${BUILD_DIR}/deps"
-export DESTINATION_DIR="${WORKSPACE}/../tmp/${JOB_NAME}/late-artifacts"
+export DESTINATION_DIR="$(readlink -e "${WORKSPACE}/../tmp/${JOB_NAME}/late-artifacts")"
+
+main () {
+    #   .. function:: main
+    #
+    #       Creates late artifacts
+    #
+    #       :output ${ARTS_DIR}/buildresult.params: file with
+    #                       results which could be used by publisher job
+    #       :type   ${ARTS_DIR}/buildresult.params: buildresult.params
+    #
+    #       :stdin: not used
+    #       :stdout: debug info
+    #       :stderr: not used
+    #
+    echo "STEP 1/4. Clean before start"
+    # =================================
+
+    for _d in "${ARTS_DIR}" "${DEPS_DIR}" "${DESTINATION_DIR}" ; do
+        rm -rf "${_d}"
+        mkdir -p "${_d}"
+    done
 
 
-echo "STEP 1/4. Clean before start"
-# =================================
+    echo "STEP 2/4. Apply crunches"
+    # =============================
 
-for _d in "${ARTS_DIR}" "${DEPS_DIR}" "${DESTINATION_DIR}" ; do
-    rm -rf "${_d}"
-    mkdir -p "${_d}"
-done
-
-
-echo "STEP 2/4. Apply crunches"
-# =============================
-
-git checkout "${FUELMAIN_COMMIT}"
-for commit in ${fuelmain_gerrit_commit} ; do
-    git fetch https://review.openstack.org/openstack/fuel-main "${commit}" && git cherry-pick FETCH_HEAD
-done
+    git checkout "${FUELMAIN_COMMIT}"
+    for commit in ${fuelmain_gerrit_commit} ; do
+        git fetch https://review.openstack.org/openstack/fuel-main "${commit}" && git cherry-pick FETCH_HEAD
+    done
 
 
-echo "STEP 3/4. Make packages-late target"
-# ========================================
+    echo "STEP 3/4. Make packages-late target"
+    # ========================================
 
-make deep_clean
+    make deep_clean
 
-# make_args is list of additional args
-# shellcheck disable=SC2086
-make packages-late ${make_args}
+    # make_args is list of additional args
+    # shellcheck disable=SC2086
+    make packages-late ${make_args}
 
-echo "STEP 4/4. Gather results"
-# ========================================
+    echo "STEP 4/4. Gather results"
+    # ========================================
 
-# copy pkgs
-find "${BUILD_DIR}/packages/rpm/RPMS/" -type f -name '*.rpm' -exec cp -v {} "${DESTINATION_DIR}" \;
-ls "${DESTINATION_DIR}"
+    # copy pkgs
+    find "${BUILD_DIR}/packages/rpm/RPMS/" -type f -name '*.rpm' -exec cp -v {} "${DESTINATION_DIR}" \;
+    ls "${DESTINATION_DIR}"
 
-touch "${ARTS_DIR}/buildresult.params"
+    touch "${ARTS_DIR}/buildresult.params"
 
-# create artifact for publisher
-# shellcheck disable=SC2129
-echo "BUILD_HOST=$(hostname)                                                " >  "${ARTS_DIR}/buildresult.params"
-echo "PKG_PATH=${DESTINATION_DIR}                                           " >> "${ARTS_DIR}/buildresult.params"
-echo "GERRIT_CHANGE_STATUS=MERGED                                           " >> "${ARTS_DIR}/buildresult.params"
-echo "REQUEST_NUM=                                                          " >> "${ARTS_DIR}/buildresult.params"
-echo "LP_BUG=                                                               " >> "${ARTS_DIR}/buildresult.params"
-echo "IS_SECURITY=false                                                     " >> "${ARTS_DIR}/buildresult.params"
-echo "EXTRAREPO=${EXTRA_RPM_REPOS}                                          " >> "${ARTS_DIR}/buildresult.params"
-echo "REPO_TYPE=rpm                                                         " >> "${ARTS_DIR}/buildresult.params"
-echo "DIST=centos7                                                          " >> "${ARTS_DIR}/buildresult.params"
-echo "                                                                      " >> "${ARTS_DIR}/buildresult.params"
-echo "# Added for publisher                                                 " >> "${ARTS_DIR}/buildresult.params"
-echo "ORIGIN=Mirantis                                                       " >> "${ARTS_DIR}/buildresult.params"
-echo "IS_UPDATES=true                                                       " >> "${ARTS_DIR}/buildresult.params"
-echo "REPO_REQUEST_PATH_PREFIX=                                             " >> "${ARTS_DIR}/buildresult.params"
-echo "REPO_BASE_PATH=??                                                     " >> "${ARTS_DIR}/buildresult.params"
-# ubuntu section is skipped (it doesn't matter)
-# shellcheck disable=SC2129
-echo "RPM_OS_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/os              " >> "${ARTS_DIR}/buildresult.params"
-echo "RPM_PROPOSED_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/proposed  " >> "${ARTS_DIR}/buildresult.params"
-echo "RPM_UPDATES_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/updates    " >> "${ARTS_DIR}/buildresult.params"
-echo "RPM_SECURITY_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/security  " >> "${ARTS_DIR}/buildresult.params"
-echo "RPM_HOLDBACK_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/holdback  " >> "${ARTS_DIR}/buildresult.params"
-echo "REMOTE_REPO_HOST=perestroika-repo-tst.infra.mirantis.net              " >> "${ARTS_DIR}/buildresult.params"
+    cat > "${ARTS_DIR}/buildresult.params" <<EOF
+BUILD_HOST=$(hostname)
+PKG_PATH=${DESTINATION_DIR}
+GERRIT_CHANGE_STATUS=MERGED
+REQUEST_NUM=
+LP_BUG=
+IS_SECURITY=false
+EXTRAREPO=${EXTRA_RPM_REPOS}
+REPO_TYPE=rpm
+DIST=centos7
 
+# Added for publisher
+ORIGIN=Mirantis
+IS_UPDATES=true
+REPO_REQUEST_PATH_PREFIX=review/
+REPO_BASE_PATH=/home/jenkins/pubrepos
+RPM_OS_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/os
+RPM_PROPOSED_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/proposed
+RPM_UPDATES_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/updates
+RPM_SECURITY_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/security
+RPM_HOLDBACK_REPO_PATH=mos-repos/centos/mos8.0-centos7-fuel/holdback
+REMOTE_REPO_HOST=perestroika-repo-tst.infra.mirantis.net
+EOF
+}
 
+main
