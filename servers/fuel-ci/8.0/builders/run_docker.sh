c43473eed3607f4a29dcb9e01c9690f9fb048496
Anton Tcitlionok <atcitlionok@mirantis.com>
2016-05-13 18:16:21 +0300

    Styleguide fix & exit job on failed pull
    Change-Id: I56774b65a31cb4ddf5fe7ec52eb8781b89911846

--- servers/fuel-ci/8.0/builders/run_docker.sh
+++ servers/fuel-ci/8.0/builders/run_docker.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
+#
+#   :mod:`run_docker` -- Docker container runner
+#   =====================================================
+#
+#   .. module:: run_docker
+#       :platform: Unix
+#       :synopsis: Run Docker container with parameters
+#   .. vesionadded:: MOS-8.0
+#   .. vesionchanged:: MOS-8.0
+#   .. author:: Anton Tcitlionok <atcitlionok@mirantis.com>
+#
+#
+#   .. envvar::
+#       :var  DOCKER_IMAGE: Docker image name
+#       :type DOCKER_IMAGE: str
+#       :var  VOLUMES: Space-delimited key:value pairs of host directory
+#                      and mount point inside Docker container
+#       :type VOLUMES: str
+#       :var  ENVVARS: Environment variables to use inside container
+#       :type ENVVARS: str
+#       :var  MODE: Optional argument to ``docker run`` command
+#       :type MODE: str
+#       :var  WORKSPACE: Location where build is started
+#       :type WORKSPACE: path
+#       :var  JOB_NAME: Jenkins job name
+#       :type JOB_NAME: str
 
+# Container should be stopped only after job is finished,
+# so exit code is processed manually.
 set -x
 
 # Default mounts
@@ -16,17 +44,25 @@ REGISTRY="registry.fuel-infra.org/fuel-ci/fuel-ci-tests"
 CONTAINER_ID="${WORKSPACE}/container.id"
 
 CMDPARAM=;
-for i in ${VOLUMES}; do
-    CMDPARAM="${CMDPARAM} -v ${i}"
+for volume in ${VOLUMES}; do
+    CMDPARAM="${CMDPARAM} -v ${volume}"
 done
 
 docker pull "${REGISTRY}:${DOCKER_IMAGE}"
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+# ENVVARS, VOLUMES and MODE variables should not be encased in
+# double-quotes, otherwise single-quotes parameters will not be parsed
+# shellcheck disable=SC2086
 docker run -v "${JOB_HOST_PATH}:${JOB_DOCKER_PATH}" ${CMDPARAM} \
            --cidfile="${CONTAINER_ID}" \
            ${ENVVARS} -t "${REGISTRY}:${DOCKER_IMAGE}" \
-           /bin/bash -exc "${SCRIPT_PATH}"
+           /bin/bash -exc "${SCRIPT_PATH}" ${MODE}
 
 # Stop container after job finishes, preserving exit code
 exitcode=$?
 docker stop "$(cat "${CONTAINER_ID}")"
-exit $exitcode
+exit ${exitcode}
