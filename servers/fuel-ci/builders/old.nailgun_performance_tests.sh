7e5c8f177b7e7e6fbdfed7a8d3f578cb3fd28c34
Anton Tcitlionok <atcitlionok@mirantis.com>
2016-06-02 14:53:27 +0300

    Switch to checking for lastSuccessfulBuild
    This fix ports builder behaviour of 'nailgun_performance_tests' job
    for master branch to 'old.nailgun_performance_tests'
    
    Closes-Bug: #1588315
    
    Change-Id: I27149e8f076fbb6d0978516109ff83b8c1f0b674

--- servers/fuel-ci/builders/old.nailgun_performance_tests.sh
+++ servers/fuel-ci/builders/old.nailgun_performance_tests.sh
@@ -4,11 +4,11 @@ set -ex
 
 VENV="${WORKSPACE}_VENV"
 virtualenv -p python2.6 "${VENV}"
-source ${VENV}/bin/activate
+source "${VENV}/bin/activate"
 
 export TEST_NAILGUN_DB="nailgun"
 
-cd ${WORKSPACE}
+cd "${WORKSPACE}"
 
 pushd nailgun > /dev/null
 
@@ -18,12 +18,12 @@ download_artifacts() {
   return $?
 }
 
-lastCompletedBuild=$(wget -q "${JENKINS_URL}job/old.nailgun_performance_tests/lastCompletedBuild/buildNumber" -O -)
-echo "Last completed build: ${lastCompletedBuild}"
+lastSuccessfulBuild=$(wget -q "${JENKINS_URL}job/old.nailgun_performance_tests/lastSuccessfulBuild/buildNumber" -O -)
+echo "Last completed build: ${lastSuccessfulBuild}"
 
 artifactsFound=0
-for buildId in $(seq $lastCompletedBuild -1 1); do
-  download_artifacts $buildId
+for buildId in $(seq "${lastSuccessfulBuild}" -1 1); do
+  download_artifacts "${buildId}"
   if [ $? -eq 0 ]; then
     echo "Artifacts found in ${buildId}"
     artifactsFound=1
@@ -39,13 +39,13 @@ fi
 # nailgun -> nailgun/nailgun
 pushd nailgun > /dev/null
 # modify paths since by default /tmp/ is used
-sed -i 's#load_tests_base:.*#load_tests_base: "'${WORKSPACE}'/results/tests/"#' settings.yaml
-sed -i 's#last_performance_test:.*#last_performance_test: "'${WORKSPACE}'/results/last/"#' settings.yaml
-sed -i 's#load_tests_results:.*#load_tests_results: "'${WORKSPACE}'/results/results/"#' settings.yaml
-sed -i 's#last_performance_test_run:.*#last_performance_test_run: "'${WORKSPACE}'/results/last/run/"#' settings.yaml
-sed -i 's#load_previous_tests_results:.*#load_previous_tests_results: "'${WORKSPACE}'/results/previous_results.json"#' settings.yaml
+sed -i 's#load_tests_base:.*#load_tests_base: "'"${WORKSPACE}"'/results/tests/"#' settings.yaml
+sed -i 's#last_performance_test:.*#last_performance_test: "'"${WORKSPACE}"'/results/last/"#' settings.yaml
+sed -i 's#load_tests_results:.*#load_tests_results: "'"${WORKSPACE}"'/results/results/"#' settings.yaml
+sed -i 's#last_performance_test_run:.*#last_performance_test_run: "'"${WORKSPACE}"'/results/last/run/"#' settings.yaml
+sed -i 's#load_previous_tests_results:.*#load_previous_tests_results: "'"${WORKSPACE}"'/results/previous_results.json"#' settings.yaml
 popd
 
 popd
-mkdir -p ${WORKSPACE}/results
+mkdir -p "${WORKSPACE}/results"
 ./run_tests.sh -x
