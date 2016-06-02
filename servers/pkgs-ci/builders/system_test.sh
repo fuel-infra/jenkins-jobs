2055488600262c9f287593752c94b499d24f43f6
Ivan Remizov <iremizov@mirantis.com>
2016-06-02 17:08:09 +0300

    Add systest for master-to-trunk
    This patch adds additional tests (systest) for building
    master to trunk scheduled builds.
    There are also some refactoring of existing jobs:
    - functionality which never used is thrown away
    - added some documentation about scenario and using env variables
    - deprecated functionality was removed
    
    Change-Id: Ifd506ad536fb5e92a5af5078f55955ff5bd234f5

--- servers/pkgs-ci/builders/system_test.sh
+++ servers/pkgs-ci/builders/system_test.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+set -ex
 
-set -o errexit
 set -o pipefail
-set -o xtrace
 
 get_deb_snapshot() {
     # Debian repos may have format "URL DISTRO COMPONENT1 [COMPONENTN]"
     # Remove quotes and assign values to variables
-    read repo_url dist_name components <<< $(tr -d \" <<< "${@}")
+    read repo_url dist_name components <<< "$(tr -d \" <<< "${@}")"
     # Remove trailing slash
     repo_url=${repo_url%/}
     local snapshot=$(curl -fLsS "${repo_url}.target.txt" | head -1)
@@ -16,12 +15,12 @@ get_deb_snapshot() {
 
 get_rpm_snapshot() {
     # Remove quotes
-    local repo_url=$(tr -d \" <<< "${1}")
+    local repo_url="$(tr -d \" <<< "${1}")"
     # Remove trailing slash
-    repo_url=${repo_url%/}
+    repo_url="${repo_url%/}"
     # Remove architecture
-    repo_url=${repo_url%/*}
-    local snapshot=$(curl -fLsS "${repo_url}.target.txt" | head -1)
+    repo_url="${repo_url%/*}"
+    local snapshot="$(curl -fLsS "${repo_url}.target.txt" | head -1)"
     echo "${repo_url%/*}/${snapshot}/x86_64"
 }
 
@@ -31,123 +30,59 @@ join () {
     echo "$*"
 }
 
-###################### Set defaults ###############
-# When job is triggerred by Zuul, parameters for job are set by Zuul, and job
-# defaults are not applied.
-
-export EXTRA_RPM_REPOS_PRIORITY=${EXTRA_RPM_REPOS_PRIORITY:-1}
-export EXTRA_DEB_REPOS_PRIORITY=${EXTRA_DEB_REPOS_PRIORITY:-1052}
-
 ###################### Set required parameters ###############
 
-export VENV_PATH=${VENV_PATH:-${HOME}/venv-nailgun-tests-2.9}
+export VENV_PATH="{VENV_PATH:-${HOME}/venv-nailgun-tests-2.9}"
 
-ENV_NAME=${ENV_PREFIX}.${BUILD_NUMBER}.${BUILD_ID}
-ENV_NAME=${ENV_NAME:0:68}
+ENV_NAME="${ENV_PREFIX}.${BUILD_NUMBER}.${BUILD_ID}"
+ENV_NAME="${ENV_NAME:0:68}"
 
-###################### Get MIRROR HOST ###############
+###################### Set RPM Extra Repos ###############
 
-LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location || :)
-LOCATION=${LOCATION_FACT:-bud}
-UBUNTU_MIRROR_ID=${UBUNTU_MIRROR_ID:-latest}
+# RPM repository parameters if set are used for all tests
+# Append RPM_REPO_URL to EXTRA_RPM_REPOS
 
-case "${LOCATION}" in
-    srt)
-        MIRROR_HOST="http://osci-mirror-srt.srt.mirantis.net"
-        ;;
-    msk)
-        MIRROR_HOST="http://osci-mirror-msk.msk.mirantis.net"
-        ;;
-    kha)
-        MIRROR_HOST="http://osci-mirror-kha.kha.mirantis.net"
-        LOCATION="hrk"
-        ;;
-    poz|bud|bud-ext|undef)
-        MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org"
-        LOCATION="cz"
-        ;;
-    mnv|scc)
-        MIRROR_HOST="http://mirror.seed-us1.fuel-infra.org"
-        LOCATION="usa"
-        ;;
-    *)
-        MIRROR_HOST="http://mirror.fuel-infra.org"
-esac
-
-###################### Get MIRROR_UBUNTU ###############
-
-if [[ ! "${MIRROR_UBUNTU}" ]]; then
-
-    case "${UBUNTU_MIRROR_ID}" in
-        latest)
-            UBUNTU_MIRROR_URL="$(curl -fLsS ${MIRROR_HOST}/pkgs/ubuntu-latest.htm)"
-            ;;
-        *)
-            UBUNTU_MIRROR_URL="${MIRROR_HOST}/pkgs/${UBUNTU_MIRROR_ID}/"
-    esac
-
-    export MIRROR_UBUNTU="deb ${UBUNTU_MIRROR_URL} ${UBUNTU_DIST} main universe multiverse|deb ${UBUNTU_MIRROR_URL} ${UBUNTU_DIST}-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} ${UBUNTU_DIST}-security main universe multiverse|deb ${UBUNTU_MIRROR_URL} ${UBUNTU_DIST}-proposed main universe multiverse"
+# RPM_REPO_URL should be injected from publisher's artifact
+# it's non-empty if publisher published rpm binaries
+# it'empty when publisher published deb binaries
+# it's empty when job started by timer (canary) or user
+if [ -n "${RPM_REPO_URL}" ]; then
+    EXTRA_RPM_REPOS="${EXTRA_RPM_REPOS:+${EXTRA_RPM_REPOS}|}test-repo,$(get_rpm_snapshot "${RPM_REPO_URL}"),${EXTRA_RPM_REPOS_PRIORITY}"
+fi
+# EXTRA_RPM_REPOS will not be injected in case when build is trggered by zuul
+# it's empty for zuul pipelines
+# it's non-empty when job started by timer (canary)
+# it's non-empty when triggered by other jenkins job or user
+if [ -n "${EXTRA_RPM_REPOS}" ]; then
+    EXTRA_RPM_REPOS="$(tr -d \\\" <<< "${EXTRA_RPM_REPOS}")"
+    export EXTRA_RPM_REPOS
 fi
 
 ###################### Get ISO image ###############
 
-# RPM repository parameters if set are used for all tests
-# Append RPM_REPO_URL to EXTRA_RPM_REPOS
-if [ -n "${RPM_REPO_URL}" ]; then
-    EXTRA_RPM_REPOS="${EXTRA_RPM_REPOS:+${EXTRA_RPM_REPOS}|}test-repo,$(get_rpm_snapshot "${RPM_REPO_URL}"),${EXTRA_RPM_REPOS_PRIORITY}"
-fi
-
-if [ -n "${EXTRA_RPM_REPOS}" ]; then
-    EXTRA_RPM_REPOS="$(tr -d \\\" <<< ${EXTRA_RPM_REPOS})"
-    export EXTRA_RPM_REPOS
-fi
-
-if [ "${REBUILD_ISO}" = "true" ]; then
-    # Build an ISO with custom repository
-
-    read MIRROR_UBUNTU_METHOD MIRROR_UBUNTU_HOST MIRROR_UBUNTU_ROOT <<< $(awk '{match($0, "^(.+)://([^/]+)(.*)$", a); print a[1], a[2], a[3]}' <<< "${UBUNTU_MIRROR_URL}")
-
-    pushd fuel-main
-    rm -rf "/var/tmp/yum-${USER}-*"
-    make deep_clean
-    make iso \
-        USE_MIRROR="${LOCATION}" \
-        MIRROR_FUEL=$(get_rpm_snapshot "${MIRROR_HOST}/${RPM_REPO_PATH}") \
-        EXTRA_RPM_REPOS="${EXTRA_RPM_REPOS}" \
-        MIRROR_MOS_UBUNTU_METHOD="${MIRROR_UBUNTU_METHOD}" \
-        MIRROR_MOS_UBUNTU="${MIRROR_UBUNTU_HOST}" \
-        MIRROR_UBUNTU_METHOD="${MIRROR_UBUNTU_METHOD}" \
-        MIRROR_UBUNTU="${MIRROR_UBUNTU_HOST}" \
-        MIRROR_UBUNTU_ROOT="${MIRROR_UBUNTU_ROOT}" \
-        MIRROR_UBUNTU_SUITE="${UBUNTU_DIST}"
-    popd
-
-    ISO_PATH=$(find "${WORKSPACE}/fuel-main/build/artifacts/" -name "*.iso" -print)
+# MAGNET_LINK should be set by injecting stable value from devops job
+# so expecting always non-empty because if empty, then something is wrong
+: "${MAGNET_LINK?}"
+if [ -n "${MAGNET_LINK}" ]; then
+    echo "MAGNET_LINK=${MAGNET_LINK}"
+    ISO_PATH=$(seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
 else
-    if [ -z "${MAGNET_LINK}" ]; then
-        # Use last BVT-tested ISO
-        ISO_MAGNET_FILE="lastSuccessfulBuild/artifact/magnet_link.txt"
-
-        # Getting MAGNET_LINK from last built ISO and force rebuild the environment if it has successfully passed smoke test.
-        ISO_MAGNET_ART="${PRODUCT_JENKINS_URL}/job/${ISO_JOB_NAME}/${ISO_MAGNET_FILE}"
-
-        MAGNET_LINK=$(curl -kLs "${ISO_MAGNET_ART}" | awk '/^MAGNET_LINK=/ {print gensub(/^[^=]+=/,"",1)}')
-    fi
-    if [ -n "${MAGNET_LINK}" ]; then
-        echo "MAGNET_LINK=${MAGNET_LINK}"
-        ISO_PATH=$(seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
-    else
-        echo "*** ERROR: Can not get ISO image! ***"
-        exit 1
-    fi
+    echo "*** ERROR: Can not get ISO image! ***"
+    exit 1
 fi
+
 
 ###################### Set test group ###############
 
 # If job is triggerred by Zuul, job defaults are not applied and TEST_GROUP is empty
 # In this case run BVT if there is DEB_REPO_URL, or prepare_slaves_3 otherwise
+# the value itself could be set in job
+# it's empty for zuul pipelines
+# it's non-empty when job started by timer (canary)
+# it's non-empty when triggered by other jenkins job or user
 if [ -z "${TEST_GROUP}" ]; then
     if [ -n "${DEB_REPO_URL}" -o -n "${EXTRA_DEB_REPOS}" ]; then
+        # in case we have some deb
         TEST_GROUP=bvt_2
     else
         TEST_GROUP=prepare_slaves_3
@@ -170,31 +105,88 @@ fi
 
 ###################### Set Fuel update parameters ###############
 
+# Directive to update fuel's master node
+# set value depending on package type being tested
+case "${REPO_TYPE}" in
+    rpm)
+        # required for rpm
+        UPDATE_FUEL=true
+        # todo:
+        # add all dependent repos
+        # EXTRA_RPM_REPOS="${EXTRA_RPM_REPOS:+${EXTRA_RPM_REPOS}|}${EXTRA_REPOS}"
+    ;;
+    deb)
+        # not needed for deb
+        UPDATE_FUEL=false
+        # todo:
+        # add all dependent repos
+        # EXTRA_DEB_REPOS="${EXTRA_DEB_REPOS:+${EXTRA_DEB_REPOS}|}${EXTRA_REPOS}"
+    ;;
+    *)
+        # must be set from outside
+        : "${UPDATE_FUEL?}"
+    ;;
+esac
+
 if [ "${UPDATE_FUEL}" = "true" ]; then
+    # directives for fuel-qa
+    # need to update master node
     export UPDATE_MASTER=true
+    # with custom repos and packages
     export CUSTOM_ENV=true
     # Note: URL must be ended by a slash symbol!
+    # UPDATE_FUEL_MIRROR is a space-separated list of urls
+
+    # see notes for RPM_REPO_URL above:
+    # sometimes it's null, so when empty (canary), try to guess
     if [ -z "${RPM_REPO_URL}" ]; then
+        # RPM_REPO_PATH will come from publisher artifact when publisher publishes rpm
+        # MIRROR_HOST should be set by guess mirror or manually
         export UPDATE_FUEL_MIRROR=$(get_rpm_snapshot "${MIRROR_HOST}/${RPM_REPO_PATH}")/
     else
+        # when not canary builds
         export UPDATE_FUEL_MIRROR="${RPM_REPO_URL}/"
     fi
+    # todo:
+    # It looks like we need to add all rpm extra repos for updating fuel
+
     # Clear stale package cache
-    rm -rvf ${UPDATE_FUEL_PATH:-~/fuel/pkgs}
+    rm -rvf "${UPDATE_FUEL_PATH:-~/fuel/pkgs}"
 fi
 
 ###################### Run test ###############
 
 # Append DEB_REPO_URL to EXTRA_DEB_REPOS
+# DEB_REPO_URL should come from publisher's artifact
+# it's non-empty if publisher published deb binaries
+# it's non-empty when job started by timer (canary) or user
+# it'empty when publisher published rpm binaries
 if [ -n "${DEB_REPO_URL}" ]; then
+    # in case when somehow this job has DEB_REPO_URL but have no EXTRA_DEB_REPOS
+    # boundary case when job pipeline is being managed by jenkins and not zuul
+    # because for zuul EXTRA_DEB_REPOS will be empty
     if [ -z "${EXTRA_DEB_REPOS}" ]; then
-        EXTRA_DEB_REPOS="deb $(get_deb_snapshot ${MIRROR_HOST}/${DEB_REPO_PATH} ${DEB_DIST_NAME} main restricted),${EXTRA_DEB_REPOS_PRIORITY}"
+        # MIRROR_HOST is set in guess mirror or by hands
+        # DEB_REPO_PATH should come from job config injections
+        # DEB_DIST_NAME should come from job config injections
+        # EXTRA_DEB_REPOS_PRIORITY should come from job config injections
+        _deb_repo="${MIRROR_HOST}/${DEB_REPO_PATH} ${DEB_DIST_NAME} main restricted"
+        # shellcheck disable=SC2086
+        # need to pass all components as separate args
+        EXTRA_DEB_REPOS="deb $(get_deb_snapshot ${_deb_repo}),${EXTRA_DEB_REPOS_PRIORITY}"
     fi
+    # normal cases:
+    # get DEB_REPO_URL from publisher and add it to the extrarepos
+    # shellcheck disable=SC2086
     EXTRA_DEB_REPOS="${EXTRA_DEB_REPOS:+${EXTRA_DEB_REPOS}|}deb $(get_deb_snapshot ${DEB_REPO_URL}),${EXTRA_DEB_REPOS_PRIORITY}"
 fi
 
+# at this point EXTRA_DEB_REPOS could be mutated and could be not mutated
+# it's non-empty if publisher published deb binaries and steps from above is applied
+# it's non-empty for timed (canary) and manual builds
+# it could be empty only for manual trigger w/o EXTRA_DEB_REPOS
 if [ -n "${EXTRA_DEB_REPOS}" ]; then
-    EXTRA_DEB_REPOS="$(tr -d \\\" <<< ${EXTRA_DEB_REPOS})"
+    EXTRA_DEB_REPOS="$(tr -d \\\" <<< "${EXTRA_DEB_REPOS}")"
 
     # Each element must be repository description as used in sources.list (prepended by "deb")
     # 1. Split EXTRA_DEB_REPOS to individual repository description
@@ -204,8 +196,12 @@ if [ -n "${EXTRA_DEB_REPOS}" ]; then
     IFS=${OLDIFS}
 
     # 2. Check that first element is word "deb" and prepend it otherwise
+    # fixme
+    # shellcheck disable=SC2068
     for REPO_NUM in ${!DEB_REPOS[@]}; do
         REPO_ELEMENTS=( ${DEB_REPOS[${REPO_NUM}]} )
+        # fixme
+        # shellcheck disable=SC2124
         test "${REPO_ELEMENTS[0]}" != "deb" && DEB_REPOS[${REPO_NUM}]="deb ${REPO_ELEMENTS[@]}"
     done
 
@@ -220,6 +216,16 @@ if [ -n "${FUEL_QA_COMMIT}" ]; then
     git -C fuel-qa checkout "${FUEL_QA_COMMIT}"
 fi
 
+# print-out env variables for debug
+env
+
+# see defaults here: https://github.com/openstack/fuel-qa/blob/master/fuelweb_test/settings.py
 pushd fuel-qa
-sh -x "utils/jenkins/system_tests.sh" -t test -w "${WORKSPACE}/fuel-qa" -e "${ENV_NAME}" -o --group="${TEST_GROUP}" -i "${ISO_PATH}"
+    sh  -x "utils/jenkins/system_tests.sh"  \
+           -t test                          \
+           -w "${WORKSPACE}/fuel-qa"        \
+           -e "${ENV_NAME}"                 \
+           -o                               \
+           --group="${TEST_GROUP}"          \
+           -i "${ISO_PATH}"
 popd
