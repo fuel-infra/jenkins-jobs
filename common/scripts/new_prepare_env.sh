1ed7ad73a18dd236baea12f3e3643a100abe07f5
vrovachev <vrovachev@mirantis.com>
2016-06-22 18:56:37 +0300

    Fix new_prepare_env job
    Added update pip in venv
    Fixed shellcheck problems
    
    Change-Id: I7ad658b90c4c6dbb5768d411ed466a373d9e5dcf

--- common/scripts/new_prepare_env.sh
+++ common/scripts/new_prepare_env.sh
@@ -7,71 +7,80 @@ export PATH=/bin:/usr/bin:/sbin:/usr/sbin:${PATH}
 ACT=0
 
 function update_devops () {
-  ACT=1
-  VIRTUAL_ENV=/home/jenkins/qa-venv-${1}
-  BRANCH=${2}
-  REPO=${3:-fuel-qa}
+    ACT=1
+    VIRTUAL_ENV=/home/jenkins/qa-venv-${1}
+    BRANCH=${2}
+    REPO=${3:-fuel-qa}
 
-  if [[ -d "${VIRTUAL_ENV}" ]] && [[ "${FORCE_DELETE_DEVOPS}" == "true" ]]; then
+    if [[ -d "${VIRTUAL_ENV}" ]] && [[ "${FORCE_DELETE_DEVOPS}" == "true" ]]; then
     echo "Delete venv from ${VIRTUAL_ENV}"
-    rm -rf ${VIRTUAL_ENV}
-  fi
-
-  if [ -f ${VIRTUAL_ENV}/bin/activate ]; then
-    source ${VIRTUAL_ENV}/bin/activate
-    echo "Python virtual env exist"
-  else
-    rm -rf ${VIRTUAL_ENV}
-    virtualenv --no-site-packages  ${VIRTUAL_ENV}
-    source ${VIRTUAL_ENV}/bin/activate
-  fi
-
-  #
-  # fuel-devops use ~/.devops directory to store log configuration
-  # we need to delete log.yaml befeore update to get it in current
-  # version
-  #
-  test -f ~/.devops/log.yaml && rm ~/.devops/log.yaml
-
-  # Prepare requirements file
-  if [[ -n "${VENV_REQUIREMENTS}" ]]; then
-    echo "Install with custom requirements"
-    echo "${VENV_REQUIREMENTS}" >"${WORKSPACE}/venv-requirements.txt"
-  else
-    if ! curl -fsS "https://raw.githubusercontent.com/openstack/${REPO}/${BRANCH}/fuelweb_test/requirements.txt" > "${WORKSPACE}/venv-requirements.txt"; then
-      echo "Problem with downloading requirements"
-      exit 1
+    rm -rf "${VIRTUAL_ENV}"
     fi
-  fi
 
-  pip install -r "${WORKSPACE}/venv-requirements.txt" --upgrade
-  echo "=============================="
-  pip freeze
-  echo "=============================="
-  django-admin.py syncdb --settings=devops.settings --noinput
-  django-admin.py migrate devops --settings=devops.settings --noinput
-  deactivate
+    if [[ -f "${VIRTUAL_ENV}/bin/activate" ]]; then
+        echo "Python virtual env exist"
+    else
+        rm -rf "${VIRTUAL_ENV}"
+        virtualenv --no-site-packages "${VIRTUAL_ENV}"
+    fi
+    source "${VIRTUAL_ENV}/bin/activate"
+
+    #
+    # fuel-devops use ~/.devops directory to store log configuration
+    # we need to delete log.yaml befeore update to get it in current
+    # version
+    #
+    test -f ~/.devops/log.yaml && rm ~/.devops/log.yaml
+
+    # Prepare requirements file
+    if [[ -n "${VENV_REQUIREMENTS}" ]]; then
+        echo "Install with custom requirements"
+        echo "${VENV_REQUIREMENTS}" >"${WORKSPACE}/venv-requirements.txt"
+    else
+        if ! curl -fsS "https://raw.githubusercontent.com/openstack/${REPO}/${BRANCH}/fuelweb_test/requirements.txt" > "${WORKSPACE}/venv-requirements.txt"; then
+            echo "Problem with downloading requirements"
+            exit 1
+        fi
+    fi
+
+    # Upgrade pip inside virtualenv
+    pip install pip --upgrade
+
+    pip install -r "${WORKSPACE}/venv-requirements.txt" --upgrade
+    echo "=============================="
+    pip freeze
+    echo "=============================="
+    django-admin.py syncdb --settings=devops.settings --noinput
+    django-admin.py migrate devops --settings=devops.settings --noinput
+    deactivate
 
 }
 
 function download_images () {
-  ACT=1
-  TARGET_CLOUD_DIR=/home/jenkins/workspace/cloud-images
-  mkdir -p ${TARGET_CLOUD_DIR}
+    ACT=1
+    local TARGET_CLOUD_DIR=/home/jenkins/workspace/cloud-images
+    mkdir -p ${TARGET_CLOUD_DIR}
 
-  TMP_CLOUD_FEDORA=$(seedclient-wrapper -d -m "${CLOUD_FEDORA}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-  TMP_SAVANNA_IMAGE=$(seedclient-wrapper -d -m "${SAVANNA_IMAGE}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-  TMP_F17_IMAGE=$(seedclient-wrapper -d -m "${F17_IMAGE}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-  TMP_MURANO_IMAGE=$(seedclient-wrapper -d -m "${MURANO_IMAGE}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-  TMP_MURANO_IMAGE_26_02_15=$(seedclient-wrapper -d -m "${MURANO_IMAGE_26_02_15}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-
-  TMP_SAHARA_JUNO_HDP=$(seedclient-wrapper -d -m "${SAHARA_JUNO_HDP}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-  TMP_SAHARA_JUNO_VANILLA=$(seedclient-wrapper -d -m "${SAHARA_JUNO_VANILLA}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-  TMP_SAHARA_KILO_VANILLA=$(seedclient-wrapper -d -m "${SAHARA_KILO_VANILLA}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-  TMP_SAHARA_LIBERTY_VANILLA=$(seedclient-wrapper -d -m "${SAHARA_LIBERTY_VANILLA}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-
-  TMP_QA_CENTOS_COMPUTE=$(seedclient-wrapper -d -m "${QA_CENTOS_COMPUTE}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
-  TMP_QA_RHEL_COMPUTE=$(seedclient-wrapper -d -m "${QA_RHEL_COMPUTE}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}")
+    for IMAGE in "${CLOUD_FEDORA}" \
+                 "${SAVANNA_IMAGE}" \
+                 "${F17_IMAGE}" \
+                 "${MURANO_IMAGE}" \
+                 "${MURANO_IMAGE_26_02_15}" \
+                 "${SAHARA_JUNO_HDP}" \
+                 "${SAHARA_JUNO_VANILLA}" \
+                 "${SAHARA_KILO_VANILLA}" \
+                 "${SAHARA_LIBERTY_VANILLA}" \
+                 "${QA_CENTOS_COMPUTE}" \
+                 "${QA_CENTOS_CLOUD}" \
+                 "${QA_RHEL_COMPUTE}" \
+                 "${QA_OL_COMPUTE}" \
+                 "${INSTALL_PACKAGES_CENTOS_6_4}" \
+                 "${INSTALL_PACKAGES_CENTOS_7_0}" \
+                 "${INSTALL_PACKAGES_UBUNTU_12_04}" \
+                 "${INSTALL_PACKAGES_UBUNTU_14_04}";
+    do
+        seedclient.py -d -m "${IMAGE}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}"
+    done
 }
 
 # Release 6.0
