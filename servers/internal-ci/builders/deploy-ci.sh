210719c75b04f3f097b250de1e026beef6aaac8a
Pawel Brzozowski <pbrzozowski@mirantis.com>
2016-06-27 12:45:49 +0200

    added summary information
    It was really hard to get what exactly happened during job run.
    This change adds tests summary at the bottom of console log.
    
    Change-Id: Ieb49a3d0581f0c9beffe5be95508cfbdcd5f826f

--- servers/internal-ci/builders/deploy-ci.sh
+++ servers/internal-ci/builders/deploy-ci.sh
@@ -48,6 +48,8 @@ set -ex
 
 BUILD_ID="${BUILD_ID:-0}"
 WORKSPACE="${WORKSPACE:-.}"
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 main () {
     #   .. function:: main
@@ -130,21 +132,57 @@ main () {
     grep -v -w -f "${WORKSPACE}/blacklist.txt" "${WORKSPACE}/selected.txt" | \
       egrep "${INCLUDE}" | egrep -v -w "${EXCLUDE}" | sed 's/_/-/g' | \
       xargs -n1 -P"${PARALLELISM}" -I '%' bash -c "
+        # create lock file on puppet run
+        touch ${WORKSPACE}/%.running
         # generate role name
         ROLE=\$(echo % | sed 's/-/_/g' | sed 's/[0-9]*//g')
         # check if image name set in mapping file
         IMAGE=\$(awk -v role=\${ROLE} '{if (\$1 == role) { print \$2 }}' \
           ${WORKSPACE}/mapping.txt)
         # create new VM and perform first puppet run
-        ./lab-vm create % \${IMAGE} 2>&1 | (sed 's/^/%: /')
+        ./lab-vm create % \${IMAGE} 2>&1 | (sed 's/^/%: /') | \
+          tee -a ${WORKSPACE}/first_run.txt
         # stop tests when got exit code of 1, 4 or 6 on second puppet run
-        ./lab-vm exec % puppet agent --test 2>&1 | (sed 's/^/%: /')
-        if [[ '146' =~ \${PIPESTATUS} ]]; then
+        ./lab-vm exec % puppet agent --detailed-exitcodes --test 2>&1 | \
+          (sed 's/^/%: /') | tee -a ${WORKSPACE}/second_run.txt
+        # save exit code
+        STATUS=\${PIPESTATUS}
+        # remove lock as puppet run is finished
+        rm ${WORKSPACE}/%.running
+        # check if status code is 1, 4 or 6
+        if [[ '146' =~ \${STATUS} ]]; then
             exit 255
+        else
+            printf '%: ${GREEN}Success: second run deployment success!${NC}\n' | \
+              tee -a ${WORKSPACE}/successful.txt
         fi
         ./lab-vm remove %
-      "
-    exit "${?}"
+      " || RETURN="${?}"
+
+    # disable script trace from here to form clear output
+    set +x
+
+    # wait until all processes are stopped
+    while ls "${WORKSPACE}"/*.running > /dev/null 2>&1; do
+        sleep 1
+    done
+
+    # prepare and display deployment summary
+    printf '\n\n========== SUCCESSFUL NODES =========\n\n'
+    cat "${WORKSPACE}/successful.txt"
+
+    printf '\n\n========== FIRST RUN ERRORS =========\n\n'
+    grep 'Error:' "${WORKSPACE}/first_run.txt" | sort -s -k 1,1
+
+    printf '\n\n========== SECOND RUN ERRORS =========\n\n'
+    grep 'Error:' "${WORKSPACE}/second_run.txt" | sort -s -k 1,1
+
+    # exit with return code if any exists
+    if [[ -n "${RETURN}" ]]; then
+        exit "${RETURN}"
+    else
+        exit
+    fi
 }
 
 if [ "$0" == "${BASH_SOURCE}" ] ; then
