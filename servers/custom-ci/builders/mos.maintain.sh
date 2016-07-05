9126b572c6483e9ea6a049efd813d5fab2e04038
Dmitry Burmistrov <dburmistrov@mirantis.com>
2016-07-05 11:35:44 +0300

    [build][maintenance] Remove all running containers
    Something prevents sbuild containers from being destroyed and
    removed on some slaves.
    It was decided to remove all running sbuild containers before
    starting the build as there's no way to properly detect lifetime
    of the container.
    
    This patch also includes fixes for shellcheck issues.
    
    Change-Id: I4fc482c9547da9270d38537e3c408200ac03d3da
    Closes-Bug: #1598908

--- servers/custom-ci/builders/mos.maintain.sh
+++ servers/custom-ci/builders/mos.maintain.sh
@@ -10,19 +10,27 @@ main () {
     # Aborted containers cleanup
     docker ps -a | grep -F -e "Exited" -e "Dead" | cut -d ' ' -f 1 | xargs -I% docker rm %
 
+    # Clean up all build related containers
+    # FIXME: There is no way to properly detect lifetime of the container, so
+    #        remove all build containers
+    docker ps -a \
+        | egrep " (mock|s)build:latest" \
+        | cut -d ' ' -f 1 \
+        | xargs -I% docker rm -f %
+
     # Unpublished packages cleanup
     rm -rf "${HOME}/built_packages/*"
 
     # Create images
     local _images="$(docker images | grep -F -e "build" | cut -d ' ' -f 1)"
     for image in mockbuild sbuild ; do
-        [ $(echo "${_images}" | grep -Fc -e "${image}") -eq 0 ] && docker build -t "${image}" "${_dpath}/${image}/"
+        [ "$(echo "${_images}" | grep -Fc -e "${image}")" -eq 0 ] && docker build -t "${image}" "${_dpath}/${image}/"
     done
 
     # Create or update chroots
     local _rpmchroots="$(ls -1 /var/cache/docker-builder/mock/cache/)"
-    for target in $(ls -1 ${_dpath}/mockbuild/*.conf | egrep -o '[0-9]+') ; do
-        if [ $(echo "${_rpmchroots}" | grep -Fc -e "-${target}-") -eq 0 ] ; then
+    for target in $(find "${_dpath}/mockbuild/" -maxdepth 1 -name "*.conf" | egrep -o '[0-9]+') ; do
+        if [ "$(echo "${_rpmchroots}" | grep -Fc -e "-${target}-")" -eq 0 ] ; then
             env "DIST=${target}" bash "${_dpath}/create-rpm-chroot.sh"
         else
             env "DIST=${target}" bash "${_dpath}/update-rpm-chroot.sh"
@@ -30,8 +38,9 @@ main () {
     done
 
     local _debchroots="$(ls -1 /var/cache/docker-builder/sbuild/)"
-    for target in trusty ; do
-        if [ $(echo "${_debchroots}" | grep -Fc -e "${target}") -eq 0 ] ; then
+    TARGET_DISTS=(trusty)
+    for target in "${TARGET_DISTS[@]}"; do
+        if [ "$(echo "${_debchroots}" | grep -Fc -e "${target}")" -eq 0 ] ; then
             env "DIST=${target}" bash "${_dpath}/create-deb-chroot.sh"
         else
             env "DIST=${target}" bash "${_dpath}/update-deb-chroot.sh"
