086f0c9e8a8a7d7ac599069ef6b64b4b61904359
Ivan Remizov <iremizov@mirantis.com>
2016-07-14 19:19:22 +0300

    Add bvt for 9.x
    Change-Id: I63e31e33142095ef174fd2e2dc0c616035237780
    Closes-Bug: #1602248

---
+++ servers/new-product-ci/9.x/builders/freeze_deps_for_systest.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -ex
+
+# we just search for snapshots, no need to guess nearest
+# MIRROR_HOST="mirror.fuel-infra.org"
+
+# fixme: mirror.fuel-infra.org could point to brokem mirror
+MIRROR_HOST="mirror.seed-cz1.fuel-infra.org"
+
+rm -rvf snapshots.params snapshots.sh
+
+function to_uppercase() {
+    echo "$1" | awk '{print toupper($0)}'
+}
+
+function store() {
+    echo "$1=$2" >> snapshots.params
+    echo "$1=\"$2\"" >> snapshots.sh
+}
+
+
+
+# Create and store ID of this snapshot file, which will be used as PK for snapshot set
+
+# id for testrail to report test results
+__snapshot_id="snapshot #${BUILD_NUMBER?}"
+store "CUSTOM_VERSION" "${__snapshot_id}"
+
+
+# Create and store MAGNET_LINK
+
+store "MAGNET_LINK" "${MAGNET_LINK?}"
+
+
+
+# Store FUEL_QA_COMMIT
+
+store "FUEL_QA_COMMIT" "$(git -C ./fuel-qa rev-parse HEAD)"
+
+
+
+# Store snapshot for copy of Ubuntu deb repo
+
+# http://mirror.seed-cz1.fuel-infra.org/pkgs/ubuntu-2016-07-13-172538
+#                                            ^^^^^^^^^^^^^^^^^^^^^^^^
+__ubuntu_latest_repo_snaphot_url="$(\
+    curl "http://${MIRROR_HOST}/pkgs/ubuntu-latest.htm" \
+    | head -1)"
+__ubuntu_latest_repo_snaphot_id="${__ubuntu_latest_repo_snaphot_url##*/}"
+store "UBUNTU_MIRROR_ID" "${__ubuntu_latest_repo_snaphot_id}"
+
+
+
+# Store snapshot for copy of Centos rpm repo
+
+# http://mirror.fuel-infra.org/pkgs/snapshots/centos-7.2.1511-2016-05-31-083834/
+#                                             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+__centos_latest_repo_snaphot_url="$(\
+    curl "http://${MIRROR_HOST}/pkgs/centos-latest.htm" \
+    | head -1)"
+__tmp="${__centos_latest_repo_snaphot_url%/}"
+__centos_latest_repo_snaphot_id="${__tmp##*/}"
+store "CENTOS_MIRROR_ID" "${__centos_latest_repo_snaphot_id}"
+
+
+
+# Store snapshot for MOS deb repo
+
+# 9.0-2016-06-23-164100
+# ^^^^^^^^^^^^^^^^^^^^^
+__mos_latest_deb_mirror_id="$(\
+    curl "http://${MIRROR_HOST}/mos-repos/ubuntu/snapshots/9.0-latest.target.txt" \
+    | head -1)"
+store "MOS_UBUNTU_MIRROR_ID" "${__mos_latest_deb_mirror_id}"
+
+
+
+# Store snapshots for full set of MOS rpm repos
+
+# <distribution_name>-2016-07-14-082020
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+for _dn in  "os"        \
+            "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    __dt_snapshot="$(\
+        curl "http://${MIRROR_HOST}/mos-repos/centos/mos9.0-centos7/snapshots/${_dn}-latest.target.txt" \
+        | head -1)"
+    store "MOS_CENTOS_$(to_uppercase "${_dn}")_MIRROR_ID" "${__dt_snapshot}"
+done
+
+
+
+# Write down description of build
+
+echo "Description string: Snapshot ID: ${__snapshot_id}"
---
+++ servers/new-product-ci/9.x/builders/wrapped_system_tests.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+set -ex
+
+# Set statistics job-group properties for swarm tests
+
+FUEL_STATS_HOST="fuel-collect-systest.infra.mirantis.net"
+ANALYTICS_IP="fuel-stats-systest.infra.mirantis.net"
+
+export FUEL_STATS_HOST="${FUEL_STATS_HOST}"
+export ANALYTICS_IP="${ANALYTICS_IP}"
+
+
+LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location || :)
+LOCATION=${LOCATION_FACT:-bud}
+
+# fixme: move to macros
+case "${LOCATION}" in
+    # fixme: mirror.fuel-infra.org could point to brokem mirror
+    # srt)
+    #     MIRROR_HOST="osci-mirror-srt.srt.mirantis.net"
+    #     ;;
+    # msk)
+    #     MIRROR_HOST="osci-mirror-msk.msk.mirantis.net"
+    #     ;;
+    # kha)
+    #     MIRROR_HOST="osci-mirror-kha.kha.mirantis.net"
+    #     LOCATION="hrk"
+    #     ;;
+    poz|bud|bud-ext|undef)
+        MIRROR_HOST="mirror.seed-cz1.fuel-infra.org"
+        LOCATION="cz"
+        ;;
+    mnv|scc|sccext)
+        MIRROR_HOST="mirror.seed-us1.fuel-infra.org"
+        LOCATION="usa"
+        ;;
+    *)
+        # MIRROR_HOST="mirror.fuel-infra.org"
+        # fixme: mirror.fuel-infra.org could point to brokem mirror
+        MIRROR_HOST="mirror.seed-cz1.fuel-infra.org"
+
+esac
+
+
+
+if [[ ! "${MIRROR_UBUNTU}" ]]; then
+
+    case "${UBUNTU_MIRROR_ID}" in
+        latest)
+            UBUNTU_MIRROR_URL="$(curl "http://${MIRROR_HOST}ubuntu-latest.htm")"
+            ;;
+        *)
+            UBUNTU_MIRROR_URL="http://${MIRROR_HOST}/pkgs/${UBUNTU_MIRROR_ID}/"
+    esac
+
+    UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
+
+# todo: later ..
+#    ENABLE_PROPOSED="${ENABLE_PROPOSED:-true}"
+#
+#    if [ "$ENABLE_PROPOSED" = true ]; then
+#        UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+#        UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
+#    fi
+
+    export MIRROR_UBUNTU="$UBUNTU_REPOS"
+
+fi
+
+
+
+function join() {
+    local __sep="${1}"
+    local __head="${2}"
+    local __tail="${3}"
+
+    if [[ -n "${__head}" ]]; then
+        echo "${__head}${__sep}${__tail}"
+    else
+        echo "${__tail}"
+    fi
+}
+
+function to_uppercase() {
+    echo "$1" | awk '{print toupper($0)}'
+}
+
+__space=' '
+__pipe='|'
+
+
+# Adding MOS rpm repos to
+# - UPDATE_FUEL_MIRROR - will be used for master node
+# - EXTRA_RPM_REPOS - will be used for nodes in cluster
+
+for _dn in  "os"        \
+            "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    # a pointer to variable name which holds value of enable flag for this dist name
+    __enable_ptr="ENABLE_MOS_CENTOS_$(to_uppercase "${_dn}")"
+    if [[ "${!__enable_ptr}" = true ]] ; then
+        # a pointer to variable name which holds repo id
+        __repo_id_ptr="MOS_CENTOS_$(to_uppercase "${_dn}")_MIRROR_ID"
+        __repo_url="http://${MIRROR_HOST}/mos-repos/centos/mos9.0-centos7/snapshots/${!__repo_id_ptr}/x86_64"
+        __repo_name="mos-${_dn},${__repo_url}"
+        UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${__repo_url}" )"
+        EXTRA_RPM_REPOS="$(join "${__pipe}" "${EXTRA_RPM_REPOS}" "${__repo_name}" )"
+    fi
+done
+
+# UPDATE_MASTER=true in case when we have set some repos
+# otherwise there will be no reason to start updating without any repos to update from
+
+if [[ -n "${UPDATE_FUEL_MIRROR}" ]] ; then
+    UPDATE_MASTER=true
+fi
+
+# Adding MOS deb repos to
+# - EXTRA_DEB_REPOS - will be used for nodes in cluster
+
+for _dn in  "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    # a pointer to variable name which holds value of enable flag for this dist name
+    __enable_ptr="ENABLE_MOS_UBUNTU_$(to_uppercase "${_dn}")"
+    # a pointer to variable name which holds repo id
+    __repo_id_ptr="MOS_UBUNTU_MIRROR_ID"
+    __repo_url="http://${MIRROR_HOST}/mos-repos/ubuntu/snapshots/${!__repo_id_ptr}"
+    if [[ "${!__enable_ptr}" = true ]] ; then
+        __repo_name="mos-${_dn},deb ${__repo_url} mos9.0-updates main restricted"
+        EXTRA_DEB_REPOS="$(join "${__pipe}" "${EXTRA_DEB_REPOS}" "${__repo_name}")"
+    fi
+done
+
+
+export UPDATE_FUEL_MIRROR   # for fuel-qa
+export UPDATE_MASTER        # for fuel-qa
+export EXTRA_RPM_REPOS      # for fuel-qa
+export EXTRA_DEB_REPOS      # for fuel-qa
+
+rm -rf logs/*
+
+ENV_NAME="${ENV_PREFIX?}.${ENV_SUFFIX?}"
+ENV_NAME="${ENV_NAME:0:68}"
+
+# done for destroy step
+echo "export ENV_NAME=\"${ENV_NAME}\"" > "${WORKSPACE}/${DOS_ENV_NAME_PROPS_FILE:=.dos_environment_name}"
+
+# save name for destruction step
+ISO_PATH=$(seedclient-wrapper -d -m "${MAGNET_LINK?}" -v --force-set-symlink -o "${WORKSPACE?}")
+
+echo "Description string: ${TEST_GROUP?} on ${CUSTOM_VERSION?}"
+
+export MAKE_SNAPSHOT=${MAKE_SNAPSHOT:-false}
+
+env
+
+sh  -x "utils/jenkins/system_tests.sh"  \
+    -t test                             \
+    -w "${WORKSPACE}"                   \
+    -e "${ENV_NAME}"                    \
+    -o                                  \
+    --group="${TEST_GROUP}"             \
+    -i "${ISO_PATH}"
+
+# remove env if not set verbosely to keep it
+
+if [[ "${KEEP_ENV:-false}" = false ]] ; then
+    source "${VENV_PATH}/bin/activate"
+    dos.py erase "${ENV_NAME}"
+fi
