eaeefa5cfe7ac6e9acd4a13e8cc0bcc677b97dea
Ivan Remizov <iremizov@mirantis.com>
2016-07-19 14:45:40 +0300

    Add 9.x. swarm
    Change-Id: Id5e4f85eca6da28a4a14fe0d10c4eaf88b58107f

---
+++ servers/new-product-ci/9.x/builders/download_plugins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+: "${URL_PREFIX?}"
+: "${PLUGINS?}"
+
+rm -rvf  "${PLUGINS}"
+mkdir -p "${PLUGINS}"
+
+for _p in EXAMPLE_PLUGIN_PATH                   \
+          EXAMPLE_PLUGIN_V3_PATH                \
+          EXAMPLE_PLUGIN_V4_PATH                \
+          SEPARATE_SERVICE_DB_PLUGIN_PATH       \
+          SEPARATE_SERVICE_RABBIT_PLUGIN_PATH   \
+          SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH \
+          SEPARATE_SERVICE_HAPROXY_PLUGIN_PATH  \
+          SEPARATE_SERVICE_BALANCER_PLUGIN_PATH ; do
+    _plugin_path="${!_p}"
+    _plugin_name="${_plugin_path##*\/}"
+    curl -s "${URL_PREFIX}${_plugin_name}" -o "${_plugin_path}"
+done
