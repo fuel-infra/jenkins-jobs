4cead84d3c645257c8a9faf060fa71c820e22ad4
Ivan Remizov <iremizov@mirantis.com>
2016-07-22 17:08:29 +0300

    Improve cleaner_dups.py
    Currently output of script is not well-formatted
    and it's hard to find conflicting jobs using the same environment.
    This change also adds additional checks which will
    show strange environments used in job which could bring
    problems in future.
    
    Change-Id: I16e87325a828c194fed9598d838978f9505b3568
    Related-Bug: #1549224

--- common/cleaner/scripts/cleaner_dups.py
+++ common/cleaner/scripts/cleaner_dups.py
@@ -5,23 +5,74 @@
 # this could create problem with duplicated env names in libvirt
 #
 
-import os
 import sys
+from collections import defaultdict
 
-prefixes = {}
-errors = False
+prefixes = defaultdict(list)
 
 with open('jobs.txt', 'r') as jobs_all:
     for job in jobs_all:
         job_name, env_prefix, last_ts_txt = job.split(' ')
-        # check if duplicate exists
-        if env_prefix in prefixes.keys():
-            print 'Duplicated ENV_PREFIX (%s) in %s!' % (env_prefix, job_name)
-            print 'Already in %s\n' % prefixes[env_prefix]
-            errors = True
-            continue
-        # append to comparition dictionary
-        prefixes[env_prefix] = job_name
+        prefixes[env_prefix].append(job_name)
 
-if errors:
+print
+print 'Overall amount of environments {0:>3d}'.format(len(prefixes))
+print '----------------------------------'
+print
+
+# searching for duplicates.
+# it's bad idea to use sane env for different jobs
+
+duplicates = {k: v for k, v in prefixes.iteritems() if len(v) > 1}
+
+if duplicates:
+    print
+    print 'Found duplicated environments {0:>3d}'.format(len(duplicates))
+    print '---------------------------------'
+    print
+
+    for env_prefix in sorted(duplicates):
+        job_names = prefixes[env_prefix]
+        print 'env prefix:   ' + env_prefix
+        for job_name in job_names:
+            print 'used in job:  ' + job_name
+        print
+
+# searching for envs which do not match job name.
+# this will help to find possible misconfiguration before duplication occurs
+
+non_duplicated = {k: v[0] for k, v in prefixes.iteritems() if len(v) == 1}
+non_matched = {k: v for k, v in non_duplicated.iteritems() if k != v}
+
+if non_matched:
+    print
+    print 'Found non-matching environments {0:>3d}'.format(len(non_matched))
+    print '-----------------------------------'
+    print
+
+    for env_prefix, job_name in sorted(non_matched.iteritems()):
+        print 'env prefix:   ' + env_prefix
+        print 'used in job:  ' + job_name
+        print
+
+# searching for envs which have uppercase chars
+# this is bad sign too..
+
+camel_cased = {k: v for k, v in non_duplicated.iteritems() if k != k.lower()}
+
+if camel_cased:
+    print
+    print 'Found camel-cased environments {0:>3d}'.format(len(camel_cased))
+    print '----------------------------------'
+    print
+
+    for env_prefix, job_name in sorted(camel_cased.iteritems()):
+        print 'env prefix:   ' + env_prefix
+        print 'used in job:  ' + job_name
+        print
+
+if not (duplicates or non_matched or camel_cased):
+    print 'No problems detected'
+
+if duplicates:
     sys.exit(1)
