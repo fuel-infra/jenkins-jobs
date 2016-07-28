273d836c6034e4d076e269897b7505698b77c047
Ivan Remizov <iremizov@mirantis.com>
2016-07-28 14:55:11 +0300

    drop old 7.0. jobs from product ci
    Change-Id: Ie041be524672199c321aa7f470063203ebf53c0e
    Related-Bug: #1549224

--- servers/new-product-ci/7.0/builders/get_version_from_magnet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ISO_VERSION=$(echo "${MAGNET_LINK##*dn=}" | cut -d \% -f 1)
-
-echo "Description string: $ISO_VERSION"
--- servers/new-product-ci/7.0/builders/set_ubuntu_stable_mirror.sh
+++
@@ -1,3 +0,0 @@
-#osci-mirrors/fuel_master_mirror_set_stable.sh $MIRROR $STABLE_VERSION
-echo "TBD"
-exit 0
