660d935652f0e39daea552572f5400bd71c8adfb
Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
2016-08-23 15:02:31 +0300

    Add publish mos/glossary to docs.mirantis.com
       We need build glossary from mos/glossary and publish it to  
       https://docs.mirantis.com/openstack/fuel/fuel-master/glossary
    
    Change-Id: I154e3eb56aaf53c56aa182862f66f056b3a69f1f
    Related-Jira-ticket: https://mirantis.jira.com/browse/PROD-6614

---
+++ servers/new-product-ci/builders/build-mos-glossary.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+#   :mod: `build-mos-glossary.sh` -- this script  builds MOS Glossary
+#        for active branches and publishes it to ``http://docs.mirantis.com``
+#   =========================================================================
+#
+#   .. module:: build-mos-glossary.sh
+#       :platform: Unix
+#       :synopsys: this script creates MOS Glossary for a branch
+#                  in mos/glossary repository and publishes it to
+#                  docs.mirantis.com
+#   .. versionadded:: MOS-9.1
+#   .. author:: Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
+#
+#   .. envvar::
+#       :var WORKSPACE: build starter location, defaults to ``.``
+#       :var GERRIT_BRANCH: mos/glossary internal branch
+#       :var DOCS_USER: credentials used for publishing, default to
+#                       ``docs``
+#       :var DOCS_HOST: host used for publishing, default to
+#                       ``docs.fuel-infra.org``
+#       :var DOCS_ROOT: path to documentation directory (on a target node)
+#
+#   .. requirements::
+#       * valid configuration YAML file: build-mos-glossary.yaml
+#
+#   .. seealso::
+#
+#   .. warnings::
+
+set -ex
+
+echo "Description string: ${GERRIT_BRANCH}"
+
+VENV="${WORKSPACE}_VENV"
+
+virtualenv "${VENV}"
+source "${VENV}/bin/activate"
+
+    pip install tox
+    tox -e docs
+
+deactivate
+
+# Publishing
+# DOCS_USER, DOCS_HOST and DOCS_ROOT variables are injected
+# shellcheck disable=SC2029
+ssh "${DOCS_USER}@${DOCS_HOST}" "mkdir -p ${DOCS_ROOT}"
+
+# BRANCH_ID is part of GERRIT_BRANCH after last "/"
+BRANCH_ID="${GERRIT_BRANCH##*/}"
+
+DOCS_PATH="${DOCS_USER}@${DOCS_HOST}:${DOCS_ROOT}/fuel-${BRANCH_ID}/glossary/"
+
+    rsync -rv doc/build/html/ "${DOCS_PATH}"
