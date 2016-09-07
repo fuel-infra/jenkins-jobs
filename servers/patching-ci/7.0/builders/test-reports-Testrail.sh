bd73f2e52e613dd6c623ad5bfa2f598c598c2ff6
vrovachev <vrovachev@mirantis.com>
2016-09-07 09:29:42 +0300

    Add right testrail description for 8.0 and 7.0 jobs
    Added generated testrail description for swarm
    tests for 8.0 and 7.0 releases.
    Related-bug: #1618427
    
    Change-Id: I276cc252beed0c0025d0215005de1c04fffd47c7

--- servers/patching-ci/7.0/builders/test-reports-Testrail.sh
+++ servers/patching-ci/7.0/builders/test-reports-Testrail.sh
@@ -11,8 +11,13 @@ export TESTRAIL_REPORTER_PATH="fuelweb_test/testrail/report.py"
 # Prepare venv
 source /home/jenkins/qa-venv-7.0/bin/activate
 
+# export test run for testrail
+RUNNER_BUILD_NUMBER=$(curl "${JENKINS_URL}/job/${TESTS_RUNNER}/lastBuild/buildNumber")
+export TESTRAIL_DESCRIPTION="${JENKINS_URL}/job/${TESTS_RUNNER}/${RUNNER_BUILD_NUMBER}"
+
 # Report tests results from swarm (Ubuntu)
 
 export USE_UBUNTU=true
 export USE_CENTOS=false
+# shellcheck disable=SC2086
 python ${TESTRAIL_REPORTER_PATH} -v -l -j ${TESTS_RUNNER} ${OPTIONS}
