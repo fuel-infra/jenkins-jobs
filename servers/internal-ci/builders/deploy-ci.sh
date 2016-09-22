68f54576eec6a978cd98661fe407544d5310465f
Pawel Brzozowski <pbrzozowski@mirantis.com>
2016-09-22 15:56:50 +0200

    added separate working directories for prefixes
    This will allow us to have simultaneous environments with different
    prefixes.
    
    Change-Id: I067b99801f405394e86b067503b531c2c0e35dd3

--- servers/internal-ci/builders/deploy-ci.sh
+++ servers/internal-ci/builders/deploy-ci.sh
@@ -64,25 +64,28 @@ main () {
     #   .. note:: https://mirantis.jira.com/browse/PROD-1017
     #
 
+    # define base directory with prefix
+    BASE_DIR="${WORKSPACE}/${PREFIX}"
+
     echo "FORCE=1" \
-    "BASE_DIR=${WORKSPACE}" \
-    "TMP_DIR=${WORKSPACE}/virtualenv" \
+    "BASE_DIR=${BASE_DIR}" \
+    "TMP_DIR=${BASE_DIR}/virtualenv" \
     "GERRIT_REFSPEC=\"${GERRIT_REFSPEC}\"" \
     "DNS1=${DNS1}" \
     "DNS2=${DNS2}" \
     "OS_FLAVOR_NAME=${OS_FLAVOR_NAME}" \
     "OS_IMAGE_NAME=${OS_IMAGE_NAME}" \
     "OS_OPENRC_PATH=${OS_OPENRC_PATH}" \
-    "GLOBAL_PREFIX=${PREFIX}" > "${WORKSPACE}/config_local"
+    "GLOBAL_PREFIX=${PREFIX}" > "${BASE_DIR}/config_local"
 
     # prepare environment and delete all VMs
-    cd "${WORKSPACE}"
+    cd "${BASE_DIR}"
     ./tools/prepare_env.sh
     ./tools/openstack_clean_all_vms.sh
 
     # re-initialize openstack environment
     if [[ "${INITIALIZE}" == 'true' ]]; then
-        cd "${WORKSPACE}"
+        cd "${BASE_DIR}"
         # remove configuration if available
         ./tools/openstack_clean_config.sh || true
         # apply new configuration
@@ -90,7 +93,7 @@ main () {
     fi
 
     # prepare hiera data
-    cd "${WORKSPACE}/hiera"
+    cd "${BASE_DIR}/hiera"
     ./gen_ssl review.test.local
     ./gen_ssh gerrit_root
     ./gen_ssh gerrit_ssh_rsa
@@ -104,49 +107,49 @@ main () {
     ./gen_gpg perestroika_gpg
 
     # prepare base VMs
-    cd "${WORKSPACE}"
+    cd "${BASE_DIR}"
     ./lab-vm create ns1
     ./lab-vm create puppet-master
 
     # check each role
     # WORKAROUND (second pass Puppet run) https://bugs.launchpad.net/bugs/1578766
-    cd "${WORKSPACE}"
+    cd "${BASE_DIR}"
 
     # get blacklist
     ./lab-vm exec puppet-master cat /etc/puppet/blacklist.txt | \
-      grep -v '^$\|^\s*\#' > "${WORKSPACE}/blacklist.txt"
+      grep -v '^$\|^\s*\#' > "${BASE_DIR}/blacklist.txt"
 
     # generate nodes and covered roles list
     ./lab-vm exec puppet-master ls -1 /var/lib/hiera/nodes/ 2>/dev/null | \
       sed 's/.test.local.yaml//g' | sed 's/-/_/g' | \
-      tee "${WORKSPACE}/selected.txt" | sed 's/[0-9]*//g' | sort -u \
-      > "${WORKSPACE}/covered.txt"
+      tee "${BASE_DIR}/selected.txt" | sed 's/[0-9]*//g' | sort -u \
+      > "${BASE_DIR}/covered.txt"
 
     # generate roles (without roles already covered by nodes list)
     ./lab-vm exec puppet-master ls -1 /var/lib/hiera/roles/ 2>/dev/null | \
-      sed 's/.yaml//g' | grep -v -w -f "${WORKSPACE}/covered.txt" \
-      >> "${WORKSPACE}/selected.txt"
+      sed 's/.yaml//g' | grep -v -w -f "${BASE_DIR}/covered.txt" \
+      >> "${BASE_DIR}/selected.txt"
 
     # generate a file with role to image name mapping
     ./lab-vm exec puppet-master "grep -RPo '^#.*(?<=image:)[^\s]+' \
       /var/lib/hiera/roles" | awk -F '/|image:|.yaml:#' '{print $6, $8}' \
-      > "${WORKSPACE}/mapping.txt"
+      > "${BASE_DIR}/mapping.txt"
 
     # iterate all the filtered roles
-    grep -v -w -f "${WORKSPACE}/blacklist.txt" "${WORKSPACE}/selected.txt" | \
+    grep -v -w -f "${BASE_DIR}/blacklist.txt" "${BASE_DIR}/selected.txt" | \
       egrep "${INCLUDE}" | egrep -v -w "${EXCLUDE}" | sed 's/_/-/g' | \
       xargs -n1 -P"${PARALLELISM}" -I '%' bash -c "
         # generate role name
         ROLE=\$(echo % | sed 's/-/_/g' | sed 's/[0-9]*//g')
         # check if image name set in mapping file
         IMAGE=\$(awk -v role=\${ROLE} '{if (\$1 == role) { print \$2 }}' \
-          ${WORKSPACE}/mapping.txt)
+          ${BASE_DIR}/mapping.txt)
         # create new VM and perform first puppet run
         ./lab-vm create % \${IMAGE} 2>&1 | (sed 's/^/%: /') | \
-          tee -a ${WORKSPACE}/first_run.txt
+          tee -a ${BASE_DIR}/first_run.txt
         # stop tests when got exit code of 1, 4 or 6 on second puppet run
         ./lab-vm exec % puppet agent --detailed-exitcodes --test 2>&1 | \
-          (sed 's/^/%: /') | tee -a ${WORKSPACE}/second_run.txt
+          (sed 's/^/%: /') | tee -a ${BASE_DIR}/second_run.txt
         # save exit code
         STATUS=\${PIPESTATUS}
         # remove VM as it's no longer required
@@ -156,10 +159,10 @@ main () {
         # check if status code is 1, 4 or 6
         if [[ '146' =~ \${STATUS} ]]; then
             printf '${RED}Failure at %: second run deployment failed!${NC}\n' | \
-              tee -a ${WORKSPACE}/summary.txt
+              tee -a ${BASE_DIR}/summary.txt
         else
             printf '${GREEN}Success at %: second run deployment success!${NC}\n' | \
-              tee -a ${WORKSPACE}/summary.txt
+              tee -a ${BASE_DIR}/summary.txt
         fi
       "
 
@@ -168,15 +171,15 @@ main () {
 
     # prepare and display deployment summary
     printf '\n\n========== FIRST RUN ERRORS =========\n\n'
-    grep 'Error:' "${WORKSPACE}/first_run.txt" | egrep -v "${IGNORED}" | \
+    grep 'Error:' "${BASE_DIR}/first_run.txt" | egrep -v "${IGNORED}" | \
       sort -s -k 1,1
 
     printf '\n\n========== SECOND RUN ERRORS =========\n\n'
-    grep 'Error:' "${WORKSPACE}/second_run.txt" | egrep -v "${IGNORED}" | \
+    grep 'Error:' "${BASE_DIR}/second_run.txt" | egrep -v "${IGNORED}" | \
       sort -s -k 1,1
 
     printf '\n\n========== SUMMARY =========\n\n'
-    sort "${WORKSPACE}/summary.txt" -s -k 1,1
+    sort "${BASE_DIR}/summary.txt" -s -k 1,1
 
     # delete the rest of VMs
     if [[ "${KEEP}" != 'true' ]]; then
@@ -184,7 +187,7 @@ main () {
     fi
 
     # return exit code '1' if failure was registered
-    if grep -q 'Failure' "${WORKSPACE}/summary.txt"; then
+    if grep -q 'Failure' "${BASE_DIR}/summary.txt"; then
         exit 1
     else
         exit 0
