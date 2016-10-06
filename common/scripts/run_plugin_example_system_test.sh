89eab7e126738609dd21515f9acaa5b6a4a16d4a
Maksym Strukov <mstrukov@mirantis.com>
2016-10-06 13:19:20 +0300

    Fix plugin swarm job to use 9.1 instead of 9.0
    Change-Id: I4beb238adc620e276ebc8348496cd102195d603d
    Closes-Bug: #1618045

--- common/scripts/run_plugin_example_system_test.sh
+++ common/scripts/run_plugin_example_system_test.sh
@@ -75,6 +75,80 @@ echo "Description string: ${TEST_GROUP} on ${VERSION_STRING}"
 
 export MAKE_SNAPSHOT=${MAKE_SNAPSHOT:-false}
 
+function join() {
+    local __sep="${1}"
+    local __head="${2}"
+    local __tail="${3}"
+
+    if [[ -n "${__head}" ]]; then
+        echo "${__head}${__sep}${__tail}"
+    else
+        echo "${__tail}"
+    fi
+}
+
+function to_uppercase() {
+    echo "$1" | awk '{print toupper($0)}'
+}
+
+__space=' '
+__pipe='|'
+
+
+# Adding MOS rpm repos to
+# - UPDATE_FUEL_MIRROR - will be used for master node
+# - EXTRA_RPM_REPOS - will be used for nodes in cluster
+
+for _dn in  "os"        \
+            "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    # a pointer to variable name which holds value of enable flag for this dist name
+    __enable_ptr="ENABLE_MOS_CENTOS_$(to_uppercase "${_dn}")"
+    if [[ "${!__enable_ptr}" = true ]] ; then
+        # a pointer to variable name which holds repo id
+        __repo_id_ptr="MOS_CENTOS_$(to_uppercase "${_dn}")_MIRROR_ID"
+        __repo_url="http://${MIRROR_HOST}/mos-repos/centos/mos9.0-centos7/snapshots/${!__repo_id_ptr}/x86_64"
+        __repo_name="mos-${_dn},${__repo_url}"
+        UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${__repo_url}" )"
+        EXTRA_RPM_REPOS="$(join "${__pipe}" "${EXTRA_RPM_REPOS}" "${__repo_name}" )"
+    fi
+done
+
+# UPDATE_MASTER=true in case when we have set some repos
+# otherwise there will be no reason to start updating without any repos to update from
+
+if [[ -n "${UPDATE_FUEL_MIRROR}" ]] ; then
+    UPDATE_MASTER=${UPDATE_MASTER:-true}
+fi
+
+# Adding MOS deb repos to
+# - EXTRA_DEB_REPOS - will be used for nodes in cluster
+
+for _dn in  "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    # a pointer to variable name which holds value of enable flag for this dist name
+    __enable_ptr="ENABLE_MOS_UBUNTU_$(to_uppercase "${_dn}")"
+    # a pointer to variable name which holds repo id
+    __repo_id_ptr="MOS_UBUNTU_MIRROR_ID"
+    __repo_url="http://${MIRROR_HOST}/mos-repos/ubuntu/snapshots/${!__repo_id_ptr}"
+    if [[ "${!__enable_ptr}" = true ]] ; then
+        __repo_name="mos-${_dn},deb ${__repo_url} mos9.0-${_dn} main restricted"
+        EXTRA_DEB_REPOS="$(join "${__pipe}" "${EXTRA_DEB_REPOS}" "${__repo_name}")"
+    fi
+done
+
+
+export UPDATE_FUEL_MIRROR   # for fuel-qa
+export UPDATE_MASTER        # for fuel-qa
+export EXTRA_RPM_REPOS      # for fuel-qa
+export EXTRA_DEB_REPOS      # for fuel-qa
+
 ################ PLUGINS  ###############
 
 PLUGINS=plugins_data
