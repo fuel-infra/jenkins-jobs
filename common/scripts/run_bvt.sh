776d9fa88f039ed6ff62d1eca01ed5d157eab419
Dmitry Kaigarodtsev <dkaiharodsev@mirantis.com>
2016-11-21 15:21:44 +0200

    [common]unhardcode 'trusty' distro on bvt tests
    Change-Id: I684e5ce2b778a06355afe55e8bc3323760a5773d
    Related-Bug: #1643497

--- common/scripts/run_bvt.sh
+++ common/scripts/run_bvt.sh
@@ -7,6 +7,7 @@ set -ex
 LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location)
 LOCATION=${LOCATION_FACT:-bud}
 UBUNTU_MIRROR_ID=${UBUNTU_MIRROR_ID:-latest}
+DISTRO=${DISTRO:-trusty}
 
 case "${LOCATION}" in
     srt)
@@ -40,12 +41,12 @@ if [[ ! "${MIRROR_UBUNTU}" ]]; then
             UBUNTU_MIRROR_URL="${MIRROR_HOST}${UBUNTU_MIRROR_ID}/"
     esac
 
-    UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
+    UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} ${DISTRO} main universe multiverse|deb ${UBUNTU_MIRROR_URL} ${DISTRO}-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} ${DISTRO}-security main universe multiverse"
 
     ENABLE_PROPOSED="${ENABLE_PROPOSED:-true}"
 
     if [ "$ENABLE_PROPOSED" = true ]; then
-        UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+        UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} ${DISTRO}-proposed main universe multiverse"
         UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
     fi
 
