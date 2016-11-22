92679a2b6fb1265db074c79643e61e4c31e84b23
Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
2016-11-22 18:16:42 +0300

    Rework jobs for verify documentation
     Currently we have a several similar jobs for verify documentations
     This request uses one template instead several configuration files
     to create verifying jobs for documentation
    
    Change-Id: I798078144e0d66219db30479a954b6669e3ccec2

--- servers/infra-ci/verify-docs/builders/verify-mos-docs.sh
+++ servers/infra-ci/verify-docs/builders/verify-mos-docs.sh
@@ -4,8 +4,8 @@ set -ex
 
 VENV=${WORKSPACE}_VENV
 
-virtualenv --clear ${VENV}
-source ${VENV}/bin/activate || exit 1
+virtualenv --clear "${VENV}"
+source "${VENV}/bin/activate" || exit 1
 
 pip install -r requirements.txt
 
@@ -17,10 +17,11 @@ LOG_FILE=${WORKSPACE}/output/make.log
 GITHEAD=$(git rev-parse HEAD)
 
 function build {
-    make clean html pdf 2>&1 1>/dev/null | awk '!/image\.py.*rescaling/ && !/\/pdf\/pdf_.*rst/ && /WARNING/i {print $x}' | tee ${LOG_FILE}
+# shellcheck disable=SC2069
+    make clean html pdf 2>&1 1>/dev/null | awk '!/image\.py.*rescaling/ && !/\/pdf\/pdf_.*rst/ && /WARNING/i {print $x}' | tee "${LOG_FILE}"
 }
 
-mkdir -p ${WORKSPACE}/output
+mkdir -p "${WORKSPACE}/output"
 
 # First count warnings from previous commit
 git checkout HEAD~1
@@ -29,11 +30,11 @@ git checkout HEAD~1
 build
 
 # Count only Warnings for later usage
-PREV_WARNS=$(grep -ic WARNING ${LOG_FILE} || true)
-echo Number of Warnings in previous commit: ${PREV_WARNS}
+PREV_WARNS=$(grep -ic WARNING "${LOG_FILE}" || true)
+echo Number of Warnings in previous commit: "${PREV_WARNS}"
 
 # Now build with the latest commit
-git checkout ${GITHEAD}
+git checkout "${GITHEAD}"
 
 # Invoke build function
 build
@@ -43,11 +44,11 @@ if [[ ! -d _build/pdf || ! -d _build/html ]]; then
     exit 1
 fi
 
-ERRORS=$(grep -ic ERROR ${LOG_FILE} || true)
-WARNS=$(grep -ic WARNING ${LOG_FILE} || true)
+ERRORS=$(grep -ic ERROR "${LOG_FILE}" || true)
+WARNS=$(grep -ic WARNING "${LOG_FILE}" || true)
 
-echo Number of build Warnings: ${WARNS}
-echo Number of build Errors: ${ERRORS}
+echo Number of build Warnings: "${WARNS}"
+echo Number of build Errors: "${ERRORS}"
 echo "Description string: Warnings ${WARNS} Errors ${ERRORS}"
 
 # If there was at least one error or number of Warnings has increased compared to previous commit: fail the job's build
