e40d1157446421cfe097d2b3ec0590e6c4e91635
Dmitry Kaigarodtsev <dkaiharodsev@mirantis.com>
2016-11-30 09:03:52 +0200

    [custom-ci]fix MIRROR_HOST
    * fixed MIRROR_HOST URL for 10.0 bvt tests
    * fixed MIRROR_HOST URL for 11.0 bvt tests
    
    Change-Id: Ie0bdcf81726f5403bf826f723d7dbd65a904d0bc

--- servers/custom-ci/11.0/builders/run_system_test.sh
+++ servers/custom-ci/11.0/builders/run_system_test.sh
@@ -5,7 +5,7 @@ set -ex
 echo STARTED_TIME="$(date -u +'%Y-%m-%dT%H:%M:%S')" > ci_status_params.txt
 
 TEST_ISO_JOB_URL="https://product-ci.infra.mirantis.net/job/11.0.test_all/"
-MIRROR_HOST="http://mirror-pkgs.vm.mirantis.net/pkgs/snapshots/"
+MIRROR_HOST="http://mirror.fuel-infra.org/pkgs/snapshots/"
 UBUNTU_DIST=${UBUNTU_DIST:-xenial}
 
 if [[ ! "${MIRROR_UBUNTU}" ]]; then
