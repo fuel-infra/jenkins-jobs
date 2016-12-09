794f466e48a2e8fa19fc288f9dba01937d786835
Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
2016-12-09 18:01:23 +0400

    Refactoring build docs job
     Variable DOCS_HOST=docs@review.fuel-infra.org is not correct
     This request splits variable DOCS_HOST to user and host parts
     Also used one script for build MCP documentation and MOS
     documentation from 9.1 and newer branches
    
    Change-Id: I390af9c9bf7d863f5bb58851209c4ed4db684d50

--- servers/product-ci/builders/build-mos-docs.sh
+++ servers/product-ci/builders/build-mos-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #   :mod: `build-mos-docs.sh` -- this script  builds  OpenStack documentation
-#        for active branches and publishes it to ``http://docs.mirantis.com``
+#        for 9.0 and under branches and publishes it to ``http://docs.mirantis.com``
 #   =========================================================================
 #
 #   .. module:: build-mos-docs.sh
@@ -10,15 +10,16 @@
 #                  in mos/mos-docs repository and publishes it to
 #                  docs.mirantis.com
 #   .. versionadded:: MOS-8.0
-#   .. versionchanged:: MOS-9.0
+#   .. versionchanged:: MOS-9.1
 #   .. author:: Lesya Novaselskaya <onovaselskaya@mirantis.com>
 #
 #
 #   .. envvar::
 #       :var WORKSPACE: build starter location, defaults to ``.``
 #       :var GERRIT_BRANCH: Fuel QA internal branch
-#       :var DOCS_HOST: credentials used for publishing, default to
-#                       ``docs@docs.fuel-infra.org``
+#       :var DOCS_USER: username used for publishing, for example ``docs``
+#       :var DOCS_HOST: host used for publishing, for example
+#                       ``docs.fuel-infra.org``
 #       :var DOCS_ROOT: path to documentation directory (on a target node)
 #
 #   .. requirements::
@@ -37,28 +38,19 @@ VENV="${WORKSPACE}_VENV"
 virtualenv "${VENV}"
 source "${VENV}/bin/activate" || exit 1
 
-if [ "${GERRIT_BRANCH}" = "master" -o "${GERRIT_BRANCH}" = "stable/9.1" ]; then
-  pip install tox
-  tox -e publishdocs
-else
   pip install -r requirements.txt
   make clean html pdf
-fi
 
 deactivate
 
 # Publishing
-# DOCS_HOST and DOCS_ROOT variables are injected
+# DOCS_USER, DOCS_HOST and DOCS_ROOT variables are injected
 # shellcheck disable=SC2029
-ssh "${DOCS_HOST}" "mkdir -p ${DOCS_ROOT}"
+ssh "${DOCS_USER}@${DOCS_HOST}" "mkdir -p ${DOCS_ROOT}"
 
 BRANCH_ID="${GERRIT_BRANCH##*/}"
 
-DOCS_PATH="${DOCS_HOST}:${DOCS_ROOT}/fuel-${BRANCH_ID}"
+DOCS_PATH="${DOCS_USER}@${DOCS_HOST}:${DOCS_ROOT}/fuel-${BRANCH_ID}"
 
-if [ "${GERRIT_BRANCH}" = "master" -o "${GERRIT_BRANCH}" = "stable/9.1" ]; then
-  rsync -rv publish-docs/ "${DOCS_PATH}"
-else
   rsync -rv --delete --exclude pdf/ _build/html/ "${DOCS_PATH}"
   rsync -rv _build/pdf/ "${DOCS_PATH}/pdf/"
-fi
