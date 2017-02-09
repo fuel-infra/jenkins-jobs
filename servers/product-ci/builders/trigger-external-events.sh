78533436015dcb22acb6d119c73a2ddb8d89135d
Ruslan Khozinov <rkhozinov@mirantis.com>
2017-02-09 12:07:58 +0000

    [product-ci] fix external-trigger for pce ci
    PCE CI depends from the external-trigger-evens job
    that needed for downloading ISO images of Fuel 10.0
    
    Change-Id: I1e6817f60e6524c87d8914b783f9ede2a6dac284

--- servers/product-ci/builders/trigger-external-events.sh
+++ servers/product-ci/builders/trigger-external-events.sh
@@ -5,7 +5,7 @@ set -ex
 # Trigger job on TPI CI
 
 if [[ "${TRIGGER_ONLY_FOR_ISO_VERSION}" == "${ISO_VERSION}" ]]; then
-    curl -sS "http://jenkins-tpi.bud.mirantis.net:8080/job/download_iso/buildWithParameters?token=DOIT&RELEASE=${ISO_VERSION}"
+    curl -sS "http://jenkins-tpi.bud.mirantis.net:8080/job/download_iso/buildWithParameters?token=b0a59406&RELEASE=${ISO_VERSION}"
     echo "Description string: <a href=\"${REPORTED_JOB_URL}\">$BUILD</a>"
 else
     echo "Description string: Skipped for $BUILD"
