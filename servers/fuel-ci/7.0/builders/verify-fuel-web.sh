9d6c7f3165b7998ed47d6ebd4c9276a784e8f136
Anton Chevychalov <achevychalov@mirantis.com>
2017-02-14 18:56:45 +0300

    Fix setuptools to old
    As setuptools is core packages for pip it is impossible to
    upgrade them over requirements file. We have to upgrade pip to
    have new version of setuptools.
    
    Change-Id: I6cc4d8ff9d33108dc4d89dee68c240917d450bc8
    Closes-Bug: #1664619

--- servers/fuel-ci/7.0/builders/verify-fuel-web.sh
+++ servers/fuel-ci/7.0/builders/verify-fuel-web.sh
@@ -6,19 +6,20 @@ export TEST_WORKERS=4
 export PATH=$PATH:$NPM_CONFIG_PREFIX/bin
 
 VENV=${WORKSPACE}_VENV
-virtualenv -p python2.6 ${VENV}
-source ${VENV}/bin/activate
+virtualenv -p python2.6 "${VENV}"
+source "${VENV}/bin/activate"
+pip install -U pip
 
 NODE_MODULES=${VENV}/node_modules
 
-cd $WORKSPACE/nailgun
-mkdir -p ${NODE_MODULES}
-ln -s ${NODE_MODULES} node_modules
+cd "$WORKSPACE/nailgun"
+mkdir -p "${NODE_MODULES}"
+ln -s "${NODE_MODULES}" node_modules
 npm install
 
 export TEST_NAILGUN_DB=nailgun
 
-cd $WORKSPACE
+cd "$WORKSPACE"
 ./run_tests.sh --with-xunit
 
 deactivate
