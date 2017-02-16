1e8d35027b5d0ac099641f1bd023943a6df13470
Alexander Evseev <aevseev@mirantis.com>
2017-02-16 11:42:58 +0300

    [product-ci] Get more parameters by test timers
    Get more parameters by test timer jobs to use for runner jobs.
    
    Change-Id: I79304d1e672f8d0a053dd7c5d94aa61a58ad5c48

--- servers/product-ci/builders/test.timer.sh
+++ servers/product-ci/builders/test.timer.sh
@@ -2,15 +2,8 @@
 
 set -ex
 
-JENKINS_TEST_BUILD_URL="${JENKINS_URL}job/${TEST_JOB}/lastSuccessfulBuild/"
-ISO_BUILD_URL="$(curl -fsS ${JENKINS_TEST_BUILD_URL}artifact/iso_build_url.txt \
-        | awk -F '[ =]' '{print $NF}')"
+ISO_BUILD_URL=$(awk -F '[ =]' '{print $2}' iso_build_url.txt)
+ISO_VERSION=${ISO_BUILD_URL%/*}
+ISO_VERSION=${ISO_VERSION##*/}
 
-echo "Description string: ISO <a href=\"$ISO_BUILD_URL\">${ISO_BUILD_URL##*[a-z]/}</a>"
-
-curl -O "${JENKINS_TEST_BUILD_URL}artifact/ubuntu_mirror_id.txt"
-curl -O "${ISO_BUILD_URL}artifact/artifacts/magnet_link.txt"
-
-cat magnet_link.txt > links.txt
-
-cat ubuntu_mirror_id.txt >> links.txt
+echo "Description string: ISO <a href=\"$ISO_BUILD_URL\">#${ISO_VERSION}</a>"
