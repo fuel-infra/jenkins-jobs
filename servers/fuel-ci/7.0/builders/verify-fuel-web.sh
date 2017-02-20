fbbcab683d0053640461fe25a7e0fe613544f17d
Anton Chevychalov <achevychalov@mirantis.com>
2017-02-20 18:49:27 +0300

    Revert "workaround for request library issue"
    That revert 6e7a68691ed409d07fbe2ba41b7b8fd6702afe39.
    
    Workaround for https://github.com/kennethreitz/requests/issues/3670
    is not working
    
    Change-Id: I9e9cdcdd97cc060396390a5648325c3c91c834d7
    Related-Bug: #1664619

--- servers/fuel-ci/7.0/builders/verify-fuel-web.sh
+++ servers/fuel-ci/7.0/builders/verify-fuel-web.sh
@@ -9,11 +9,7 @@ VENV=${WORKSPACE}_VENV
 virtualenv -p python2.6 "${VENV}"
 source "${VENV}/bin/activate"
 
-# Workaround for https://github.com/kennethreitz/requests/issues/3670
-pip install --upgrade requests
-
 # Upgrade too old core packages which can't be upgrade with requirements file
-pip install -U pip
 pip install -U setuptools
 
 NODE_MODULES=${VENV}/node_modules
