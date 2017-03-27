3ccbba7d50c8f0173a893c4bc67abc71073799ff
Kirill Mashchenko <kmashchenko@mirantis.com>
2017-03-27 13:47:33 +0300

    [fuel-ci]Run restore user permissions before and after dockerized job
    Change-Id: I814cc1d9f0871cceb2a999ce3371e61164e42309

--- servers/fuel-ci/dockerized_tests/builders/run_docker.sh
+++ servers/fuel-ci/dockerized_tests/builders/run_docker.sh
@@ -38,6 +38,12 @@ ENVVARS="${ENVVARS:-}"
 WORKSPACE="${WORKSPACE:-${PWD}}"
 JOB_NAME="${JOB_NAME:-debug_docker_script}"
 
+restore_user_permissions() {
+    # restore user permissions on workspace
+    # shellcheck disable=SC2086
+    docker run --rm -v ${WORKSPACE}:/workspace -t ubuntu /bin/bash -c "chown -R $(id -u):$(id -g) /workspace"
+}
+
 LATEST_IMAGE=$(curl -ksSL "${REGISTRY_PROTOCOL}://${REGISTRY}/v2/${IMAGE}/tags/list"|jq -r '.tags[]'|grep -v -e latest -e 2017|sort -n|tail -n1)
 
 CMD_VOLUMES="-v ${WORKSPACE}:/opt/jenkins/${JOB_NAME}"
@@ -45,9 +51,9 @@ for volume in ${VOLUMES}; do
     CMD_VOLUMES+=" -v ${volume}"
 done
 bash -exc "docker pull ${REGISTRY}/${IMAGE}:${LATEST_IMAGE}"
-bash -exc "docker run --rm ${CMD_VOLUMES} ${ENVVARS} -t ${REGISTRY}/${IMAGE}:${LATEST_IMAGE} /bin/bash -exc '${SCRIPT_PATH} ${SCRIPT_ARGS}'"
-EXIT_CODE=${?}
-# restore permissions of workspace
-# shellcheck disable=SC2086
-docker run --rm -v ${WORKSPACE}:/workspace -t ubuntu /bin/bash -c "chown -R $(id -u):$(id -g) /workspace"
+restore_user_permissions
+EXIT_CODE=0
+# shellcheck disable=2086
+docker run --rm ${CMD_VOLUMES} ${ENVVARS} -t ${REGISTRY}/${IMAGE}:${LATEST_IMAGE} /bin/bash -exc "${SCRIPT_PATH} ${SCRIPT_ARGS}" || EXIT_CODE=1
+restore_user_permissions
 exit ${EXIT_CODE}
