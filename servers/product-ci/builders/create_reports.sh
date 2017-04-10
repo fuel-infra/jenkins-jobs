ff04e09e61c90612fc829a18c11856b0b6e71845
Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
2017-04-10 20:03:54 +0400

    Add ability to scan packages from mcp repositories
    Related-PROD: https://mirantis.jira.com/browse/PROD-10178
    
    Change-Id: Ic9ba2c56a111c3f05010d4c9540a0771e9d2bb5d

--- servers/product-ci/builders/create_reports.sh
+++ servers/product-ci/builders/create_reports.sh
@@ -5,7 +5,15 @@ set -ex
 
 # Change '|||' to ',' as separate in license_compliance file compliance csv format
 # Combine and upload report
-for DISTR in centos ubuntu; do
-    sed -e 's/|||/,/g' "license_mos_${RELEASE}${UPDATES_SUFFIX}_${DISTR}" > "license_mos_${RELEASE}${UPDATES_SUFFIX}_${DISTR}.csv"
-    python license-compliance/extract.py "${DISTR}${RELEASE}${UPDATES_SUFFIX}.csv" "license_mos_${RELEASE}${UPDATES_SUFFIX}_${DISTR}.csv" "enc_lic_${DISTR}_output_${RELEASE}${UPDATES_SUFFIX}.csv" "${KEYPATH}"
+case ${PROJECT_NAME} in
+    "mos")
+     DISTRS="ubuntu centos"
+     ;;
+     "mcp")
+     DISTRS="ubuntu"
+     ;;
+esac
+for DISTR in ${DISTRS}; do
+    sed -e 's/|||/,/g' "license_${PROJECT_NAME}_${RELEASE}${UPDATES_SUFFIX}_${DISTR}" > "license_${PROJECT_NAME}_${RELEASE}${UPDATES_SUFFIX}_${DISTR}.csv"
+    python license-compliance/extract.py "${DISTR}${RELEASE}${UPDATES_SUFFIX}.csv" "license_${PROJECT_NAME}_${RELEASE}${UPDATES_SUFFIX}_${DISTR}.csv" "enc_lic_${DISTR}_output_${RELEASE}${UPDATES_SUFFIX}.csv" "${KEYPATH}"
 done
