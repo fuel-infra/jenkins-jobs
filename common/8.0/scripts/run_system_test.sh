800e12afcf992a8b6e6a2abd3803043bd3ad6dd8
Vladimir Jigulin <vjigulin@mirantis.com>
2017-07-21 15:04:05 +0400

    [patching-ci][8.0] Improve repository calculation related code
    Improved flexibility of code that define repos for systest and tempest jobs.
    New macroses for repos parameters and code.
    Integration with 8.0.snapshot.params job.
    
    Change-Id: I825eab0dc70bac516c8ebe946b3ade1ca3822340

--- common/8.0/scripts/run_system_test.sh
+++ common/8.0/scripts/run_system_test.sh
@@ -2,124 +2,9 @@
 
 set -ex
 
-###################### Get MIRROR HOST ###############
-
-LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location)
-LOCATION=${LOCATION_FACT:-bud}
-CENTOS_UPDATE_HOST="http://pkg-updates.fuel-infra.org/centos7/"
-
-case "${LOCATION}" in
-    mnv|scc)
-        MIRROR_HOST="http://us.mirror.fuel-infra.org/"
-        ;;
-    *)
-        MIRROR_HOST="http://eu.mirror.fuel-infra.org/"
-esac
-
-######## Install Upstream CentOS 7 repositories ########
-
-if [[ ! -z ${CENTOS_MIRROR_ID} ]]; then
-    for _repo in "os"      \
-                 "extras"  \
-                 "updates"
-    do
-       _url="${MIRROR_HOST}pkgs/snapshots/${CENTOS_MIRROR_ID}/${_repo}/x86_64"
-       UPDATE_FUEL_MIRROR="${_url} ${UPDATE_FUEL_MIRROR}"
-    done
-    export UPDATE_FUEL_MIRROR
-    export UPDATE_MASTER=true
-fi
-
-###################### Get MIRROR_UBUNTU ###############
-
-if [[ ! "${MIRROR_UBUNTU}" ]]; then
-
-    case "${UBUNTU_MIRROR_ID}" in
-        latest)
-            UBUNTU_MIRROR_URL="${MIRROR_HOST}pkgs/snapshots/ubuntu-latest/"
-            ;;
-        *)
-            UBUNTU_MIRROR_URL="${MIRROR_HOST}pkgs/snapshots/${UBUNTU_MIRROR_ID}/"
-    esac
-
-    UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
-
-    if [ "$ENABLE_UBUNTU_PROPOSED" = true ]; then
-        UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
-        UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
-    fi
-
-    export MIRROR_UBUNTU="$UBUNTU_REPOS"
-
-fi
-
-###################### Set extra DEB and RPM repos ####
-
-if [[ -n "${RPM_LATEST}" ]]; then
-    RPM_MIRROR="${MIRROR_HOST}mos-repos/centos/mos8.0-centos7-fuel/snapshots/"
-    if [[ "${ENABLE_MOS_PROPOSED}" == "true" ]]; then
-        RPM_PROPOSED="mos-proposed,${RPM_MIRROR}proposed-${RPM_LATEST}/x86_64"
-        EXTRA_RPM_REPOS+="${RPM_PROPOSED}"
-        UPDATE_FUEL_MIRROR+=" ${RPM_MIRROR}proposed-${RPM_LATEST}/x86_64"
-    fi
-    if [[ "${ENABLE_UPDATES}" == "true" ]]; then
-        RPM_UPDATES="mos-updates,${RPM_MIRROR}updates-${RPM_LATEST}/x86_64"
-        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
-            EXTRA_RPM_REPOS+="|"
-            UPDATE_FUEL_MIRROR+=" "
-        fi
-        EXTRA_RPM_REPOS+="${RPM_UPDATES}"
-        UPDATE_FUEL_MIRROR+="${RPM_MIRROR}updates-${RPM_LATEST}/x86_64"
-    fi
-    if [[ "${ENABLE_SECURITY}" == "true" ]]; then
-        RPM_SECURITY="mos-security,${RPM_MIRROR}security-${RPM_LATEST}/x86_64"
-        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
-            EXTRA_RPM_REPOS+="|"
-            UPDATE_FUEL_MIRROR+=" "
-        fi
-        EXTRA_RPM_REPOS+="${RPM_SECURITY}"
-        UPDATE_FUEL_MIRROR+="${RPM_MIRROR}security-${RPM_LATEST}/x86_64"
-    fi
-    if [[ "${ENABLE_UPDATE_CENTOS}" == "true" ]]; then
-        RPM_UPDATE_CENTOS="centos-security,${CENTOS_UPDATE_HOST}"
-        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
-            EXTRA_RPM_REPOS+="|"
-            UPDATE_FUEL_MIRROR+=" "
-        fi
-        EXTRA_RPM_REPOS+="${RPM_UPDATE_CENTOS}"
-        UPDATE_FUEL_MIRROR+="${CENTOS_UPDATE_HOST}"
-    fi
-    export EXTRA_RPM_REPOS
-    export UPDATE_FUEL_MIRROR
-    export UPDATE_MASTER=true
-fi
-
-if [[ -n "${DEB_LATEST}" ]]; then
-    DEB_MIRROR="${MIRROR_HOST}mos-repos/ubuntu/snapshots/8.0-${DEB_LATEST}"
-    if [[ "${ENABLE_MOS_PROPOSED}" == "true" ]]; then
-        DEB_PROPOSED="mos-proposed,deb ${DEB_MIRROR} mos8.0-proposed main restricted"
-        EXTRA_DEB_REPOS+="${DEB_PROPOSED}"
-    fi
-    if [[ "${ENABLE_UPDATES}" == "true" ]]; then
-        DEB_UPDATES="mos-updates,deb ${DEB_MIRROR} mos8.0-updates main restricted"
-        if [[ -n "${EXTRA_DEB_REPOS}" ]]; then
-            EXTRA_DEB_REPOS+="|"
-        fi
-        EXTRA_DEB_REPOS+="${DEB_UPDATES}"
-    fi
-    if [[ "${ENABLE_SECURITY}" == "true" ]]; then
-        DEB_SECURITY="mos-security,deb ${DEB_MIRROR} mos8.0-security main restricted"
-        if [[ -n "${EXTRA_DEB_REPOS}" ]]; then
-            EXTRA_DEB_REPOS+="|"
-        fi
-        EXTRA_DEB_REPOS+="${DEB_SECURITY}"
-    fi
-    export EXTRA_DEB_REPOS
-fi
-
 rm -rf logs/*
 
-ENV_NAME=$ENV_PREFIX.$BUILD_NUMBER.$BUILD_ID
+ENV_NAME="${ENV_PREFIX}.${BUILD_NUMBER}.${BUILD_ID}"
 ENV_NAME=${ENV_NAME:0:68}
 echo "export ENV_NAME=\"${ENV_NAME}\"" > "${WORKSPACE}/${DOS_ENV_NAME_PROPS_FILE:=.dos_environment_name}"
 
@@ -128,4 +13,4 @@ ISO_PATH=$(seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "
 VERSION_STRING=$(basename "${ISO_PATH}" | cut -d '-' -f 2-3)
 echo "Description string: $TEST_GROUP on $VERSION_STRING"
 
-sh -x "utils/jenkins/system_tests.sh" -t test -w "$WORKSPACE" -e "$ENV_NAME" -o --group="$TEST_GROUP" -i "$ISO_PATH"
+sh -x "utils/jenkins/system_tests.sh" -w "${WORKSPACE}" -e "${ENV_NAME}" -o --group="${TEST_GROUP}" -i "${ISO_PATH}"
---
+++ common/8.0/scripts/systest_repos_parsing.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+set -ex
+
+# Fetch params from snapshot job
+if [[ -n "${SNAPSHOT_PARAMS_ID}" ]]; then
+    source <(curl -s "https://patching-ci.infra.mirantis.net/job/8.0.snapshot.params/${SNAPSHOT_PARAMS_ID}/artifact/snapshots.sh")
+fi
+
+###################### Get MIRROR HOST ###############
+LOCATION_FACT=$(facter --external-dir /etc/facter/facts.d/ location)
+LOCATION=${LOCATION_FACT:-bud}
+CENTOS_SECURITY_PROPOSED=${CENTOS_SECURITY_PROPOSED:-"http://pkg-updates.fuel-infra.org/centos7/"}
+
+case "${LOCATION}" in
+    mnv|scc)
+        MIRROR_HOST="http://us.mirror.fuel-infra.org/"
+        ;;
+    *)
+        MIRROR_HOST="http://eu.mirror.fuel-infra.org/"
+esac
+
+function join() {
+    local __sep="${1}"
+    local __head="${2}"
+    local __tail="${3}"
+
+    if [[ -n "${__head}" ]]; then
+        echo "${__head}${__sep}${__tail}"
+    else
+        echo "${__tail}"
+    fi
+}
+
+function to_uppercase() {
+    echo "$1" | awk '{print toupper($0)}'
+}
+
+__space=' '
+__pipe='|'
+
+# Adding CentOS rpm repos to
+# - UPDATE_FUEL_MIRROR - will replace the default upstream repositories in cluster
+if [[ ! -z ${CENTOS_MIRROR_ID} ]]; then
+    for _repo in "os"      \
+                 "extras"  \
+                 "updates"; do
+        __repo_url="${MIRROR_HOST}pkgs/snapshots/${CENTOS_MIRROR_ID}/${_repo}/x86_64"
+        UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${__repo_url}" )"
+    done
+fi
+
+# Adding Ubuntu deb repos to
+# - MIRROR_UBUNTU - will be used for nodes in cluster
+UBUNTU_MIRROR_URL="${MIRROR_HOST}/pkgs/snapshots/${UBUNTU_MIRROR_ID}/"
+for __dn in "trusty"         \
+            "trusty-updates" \
+            "trusty-security"; do
+    __repo="deb ${UBUNTU_MIRROR_URL} ${__dn} main universe multiverse"
+    MIRROR_UBUNTU="$(join "${__pipe}" "${MIRROR_UBUNTU}" "${__repo}")"
+done
+if [ "${ENABLE_UBUNTU_MIRROR_PROPOSED}" = true ]; then
+    __repo="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+    MIRROR_UBUNTU="$(join "${__pipe}" "${MIRROR_UBUNTU}" "${__repo}")"
+fi
+
+# Adding MOS rpm repos to
+# - UPDATE_FUEL_MIRROR - will be used for master node
+# - EXTRA_RPM_REPOS - will be used for nodes in cluster
+for _dn in  "os"        \
+            "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    # a pointer to variable name which holds value of enable flag for this dist name
+    __enable_ptr="ENABLE_MOS_CENTOS_$(to_uppercase "${_dn}")"
+    if [[ "${!__enable_ptr}" = true ]] ; then
+        # a pointer to variable name which holds repo id
+        __repo_id_ptr="MOS_CENTOS_$(to_uppercase "${_dn}")_MIRROR_ID"
+        __repo_url="${MIRROR_HOST}mos-repos/centos/mos8.0-centos7-fuel/snapshots/${!__repo_id_ptr}/x86_64"
+        __repo_name="mos-${_dn},${__repo_url}"
+        UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${__repo_url}" )"
+        EXTRA_RPM_REPOS="$(join "${__pipe}" "${EXTRA_RPM_REPOS}" "${__repo_name}" )"
+    fi
+done
+# Add separate mos rpm security repo
+if [[ "${ENABLE_MOS_CENTOS_SECURITY_PROPOSED}" = true ]] ; then
+    __repo_name="centos-security,${CENTOS_SECURITY_PROPOSED}"
+    UPDATE_FUEL_MIRROR="$(join "${__space}" "${UPDATE_FUEL_MIRROR}" "${CENTOS_SECURITY_PROPOSED}" )"
+    EXTRA_RPM_REPOS="$(join "${__pipe}" "${EXTRA_RPM_REPOS}" "${__repo_name}" )"
+fi
+
+# UPDATE_MASTER=true in case when we have set some repos
+# otherwise there will be no reason to start updating without any repos to update from
+if [[ -n "${UPDATE_FUEL_MIRROR}" ]] ; then
+    UPDATE_MASTER=${UPDATE_MASTER:-true}
+fi
+
+# Adding MOS deb repos to
+# - EXTRA_DEB_REPOS - will be used for nodes in cluster
+for _dn in  "proposed"  \
+            "updates"   \
+            "holdback"  \
+            "hotfix"    \
+            "security"  ; do
+    # a pointer to variable name which holds value of enable flag for this dist name
+    __enable_ptr="ENABLE_MOS_UBUNTU_$(to_uppercase "${_dn}")"
+    # a pointer to variable name which holds repo id
+    __repo_id_ptr="MOS_UBUNTU_MIRROR_ID"
+    __repo_url="${MIRROR_HOST}mos-repos/ubuntu/snapshots/${!__repo_id_ptr}"
+    if [[ "${!__enable_ptr}" = true ]] ; then
+        __repo_name="mos-${_dn},deb ${__repo_url} mos8.0-${_dn} main restricted"
+        EXTRA_DEB_REPOS="$(join "${__pipe}" "${EXTRA_DEB_REPOS}" "${__repo_name}")"
+    fi
+done
+
+echo "<============> REPOS PARSING RESULTS <============>"
+echo "UPDATE_MASTER: ${UPDATE_MASTER}"
+echo "UPDATE_FUEL_MIRROR: ${UPDATE_FUEL_MIRROR}"
+echo "MIRROR_UBUNTU: ${MIRROR_UBUNTU}"
+echo "EXTRA_RPM_REPOS: ${EXTRA_RPM_REPOS}"
+echo "EXTRA_DEB_REPOS: ${EXTRA_DEB_REPOS}"
+echo "<=================================================>"
+
+cat > systest_repos.jenkins-injectfile <<EOF
+UPDATE_MASTER="${UPDATE_MASTER}"
+UPDATE_FUEL_MIRROR="${UPDATE_FUEL_MIRROR}"
+MIRROR_UBUNTU="${MIRROR_UBUNTU}"
+EXTRA_RPM_REPOS="${EXTRA_RPM_REPOS}"
+EXTRA_DEB_REPOS="${EXTRA_DEB_REPOS}"
+EOF
