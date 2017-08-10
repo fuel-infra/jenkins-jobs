1fea707784038466af5078090cf7e72a350a2625
Vladimir Jigulin <vjigulin@mirantis.com>
2017-08-10 14:16:58 +0400

    [patching-ci][mcp][tempest] Use templates. Add runner.
    Tempest jobs now use templates for flexibiltity.
    Created runner job for mcp tempest.
    Added test-group parameter for mcp tempest.
    Added timeout for tempest jobs.
    
    Change-Id: Id81c599adbd4b737125207053ea9f291d759c8f2

--- servers/patching-ci/mcp/builders/mcp.virtual.tempest.sh
+++ servers/patching-ci/mcp/builders/mcp.virtual.tempest.sh
@@ -8,18 +8,13 @@ export IMAGE_PATH1604=/home/jenkins/images/xenial-server-cloudimg-amd64-disk1.im
 export SHUTDOWN_ENV_ON_TEARDOWN
 export REPOSITORY_SUITE
 export ENV_NAME
-
+export TEST_GROUP
 export PYTHONIOENCODING=UTF-8
 
-LC_ALL=en_US.UTF-8  py.test -vvv -s -k test_tcp_install_run_rally
-
-REPORT_FILE=$(find "$(pwd)" -name "report_*.xml")
-
-if [[ -n "${REPORT_PREFIX}" ]]; then
-    mkdir -p "${REPORT_PREFIX}"
-    cp -f "${REPORT_FILE?}" "${REPORT_PREFIX}"
-fi
-
-mv "${REPORT_FILE?}" "${WORKSPACE}/verification.xml"
+# Run tests
+LC_ALL=en_US.UTF-8 py.test -vvv -s -p no:django -p no:ipdb -k "${TEST_GROUP}"
 
 deactivate
+
+REPORT_FILE=$(find "$(pwd)" -name "report_*.xml")
+mv "${REPORT_FILE?}" "${WORKSPACE}/verification.xml"
