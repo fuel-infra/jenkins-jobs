e34ee270cd6c0374b541727f1075ef79d6e63245
Vladimir Jigulin <vjigulin@mirantis.com>
2017-08-11 10:41:56 +0400

    [patching-ci][mcp] Create new report job for mcp tempest
    Because we have many differences with maintenance reporter
    
    Change-Id: I8808962357e5dbb07e1ab9539548480ee074b54f

---
+++ servers/patching-ci/mcp/builders/mcp.xunit-testrail-reporter.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -ex
+
+if [ ! -f "${REPORT_FILE?}" ]; then
+    echo "Can't find \"${REPORT_FILE}\" report file"
+    exit 1
+fi
+
+ARGS=()
+if [ -n "${TESTRAIL_NAME_TEMPLATE}" ]; then
+    ARGS+=(--testrail-name-template "${TESTRAIL_NAME_TEMPLATE}")
+fi
+
+if [ -n "${XUNIT_NAME_TEMPLATE}" ]; then
+    ARGS+=(--xunit-name-template "${XUNIT_NAME_TEMPLATE}")
+fi
+
+if [ -z "${TEST_BUILD_URL}" ]; then
+    TEST_BUILD_URL="${BUILD_URL}"
+fi
+
+if [ "${SEND_SKIPPED}" == true ]; then
+    ARGS+=(--send-skipped)
+fi
+
+if [ ! -f report-venv/bin/activate ]; then
+    rm -rf report-venv
+    virtualenv report-venv
+    source report-venv/bin/activate
+    pip install -U pip
+else
+    source report-venv/bin/activate
+fi
+
+pip install -U xunit2testrail
+
+report -v --testrail-plan-name "${TESTRAIL_PLAN_NAME?}" \
+          --env-description "${ENV_DESCRIPTION?}"\
+          --testrail-user "${TESTRAIL_USER?}" \
+          --testrail-password "${TESTRAIL_PASSWORD?}" \
+          --testrail-project "${TESTRAIL_PROJECT?}" \
+          --testrail-milestone "${TESTRAIL_MILESTONE?}" \
+          --testrail-suite "${TESTRAIL_SUITE?}" \
+          --test-results-link "${TEST_BUILD_URL}"\
+          "${ARGS[@]}" \
+          "${REPORT_FILE?}"
+
+deactivate
