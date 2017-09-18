4d5e1d4777f9fdf945aec8281acbcbc56dc78b85
Dmitry Teselkin <dteselkin@mirantis.com>
2017-09-18 19:13:50 +0300

    Update cve_tracking job
    * Improve bootloader script
    * Add job parameter
    
    Change-Id: I2ae20e31977398e342f0e2eafda8d866b534058b
    Related-Prod: PROD-PROD-14681

--- servers/pkgs-ci/builders/cve_tracking.sh
+++ servers/pkgs-ci/builders/cve_tracking.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-WORKDIR=/workspace
-export WORKDIR
+set -o errexit
+set -o xtrace
 
-apt-get install -y git
+WORKDIR=/workspace
+BRANCH=${BRANCH:-master}
+
+URL=https://172.18.170.22:8443/raw/cve-tracker-config.git/${BRANCH}/start.sh
+START_SH=${WORKDIR}/start.sh
+
+apt install -y \
+  git \
+  python3-dev \
+  python3-pip
+
+mkdir -p "${WORKDIR}/html"
+mkdir -p "${WORKDIR}/persistent"
+
+rm -f ${START_SH} || true
+curl -k "${URL}" -o "${START_SH}"
 
 cd ${WORKDIR}
-git clone git://172.18.170.22/cve-tracker-config.git
-rm -f ${WORKDIR}/start.sh
-cp ${WORKDIR}/cve-tracker-config/start.sh ${WORKDIR}
-${WORKDIR}/start.sh once
+bash -o xtrace -o errexit "${START_SH}" once
 
