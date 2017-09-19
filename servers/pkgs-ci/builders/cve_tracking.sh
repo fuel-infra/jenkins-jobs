5eb1c30169cc3202c52304b99aae1203ab21c92a
Dmitry Teselkin <dteselkin@mirantis.com>
2017-09-19 17:25:17 +0300

    Implement simple lock
    Change-Id: I4e13e7ab37007105c7b611eae150f5021aa4fdda
    Related-Prod: PROD-14681

--- servers/pkgs-ci/builders/cve_tracking.sh
+++ servers/pkgs-ci/builders/cve_tracking.sh
@@ -3,11 +3,39 @@
 set -o errexit
 set -o xtrace
 
+trap on_exit EXIT
+
+die() {
+  cat << EOF
+
+========================================
+$@
+========================================
+EOF
+  exit 1
+}
+
+on_exit() {
+  if ${DROPLOCK}; then
+    sudo rm -f "${LOCKFILE}" || true
+  fi
+  exit
+}
+
 WORKDIR=/workspace
 BRANCH=${BRANCH:-master}
 
+LOCKFILE=/var/run/cve-tracker.lock
+DROPLOCK=false
+
 URL=https://172.18.170.22:8443/raw/cve-tracker-config.git/${BRANCH}/start.sh
 START_SH=${WORKDIR}/start.sh
+
+if [[ -f "${LOCKFILE}" ]]; then
+  die "Amother process already running, lockfile '${LOCKFILE}' found"
+fi
+DROPLOCK=true
+sudo touch "${LOCKFILE}"
 
 sudo apt install -y \
   git \
@@ -16,7 +44,7 @@ sudo apt install -y \
 
 sudo mkdir -p "${WORKDIR}"
 
-sudo chown -R "${USER}" "${WORKDIR}"
+sudo chown "${USER}" "${WORKDIR}"
 sudo chown -R "${USER}" /var/www/html
 
 mkdir -p "${WORKDIR}/html"
