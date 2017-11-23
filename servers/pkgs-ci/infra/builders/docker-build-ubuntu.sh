db18c3d7c810498bf1c796b1ea418705417c4035
Alexander Evseev <aevseev@mirantis.com>
2017-11-23 12:32:33 +0100

    [infra] Explicitly add DB packages to docker image
    Due to updates to upstream's bindep.txt mongodb was removed from testing
    dependencies for new OpenStack releases but is required for old releases.
    
    Add database packages to install explicitly.
    
    Change-Id: I4b82327aca63a5e03be91e44ebfa89c8b6d7c7cd

--- servers/pkgs-ci/infra/builders/docker-build-ubuntu.sh
+++ servers/pkgs-ci/infra/builders/docker-build-ubuntu.sh
@@ -45,7 +45,7 @@ RUN \
   apt-get update ; \
   apt-get install lsb-release apt-transport-https ca-certificates curl git openssh-client python python3 python-yaml \
     sudo bridge-utils iproute2 iptables iputils-ping net-tools strace qemu-utils keepalived sasl2-bin genisoimage \
-    python-bashate ; \
+    python-bashate libmysqlclient-dev mysql-client mysql-server mongodb libpq-dev postgresql postgresql-client ; \
   \
   python2 /tmp/get-pip.py ; \
   python3 /tmp/get-pip.py ; \
